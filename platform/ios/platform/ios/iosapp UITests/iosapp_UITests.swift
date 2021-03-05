a5a1809bdead9aa78760026d4ee9f8c72938d7a9
RobLabs.com <roblabs@users.noreply.github.com>
2021-03-04 16:15:21 -0800

    Save a PNG screenshot to the UITest results

--- platform/ios/platform/ios/iosapp UITests/iosapp_UITests.swift
+++ platform/ios/platform/ios/iosapp UITests/iosapp_UITests.swift
@@ -26,6 +26,7 @@ class iosapp_UITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        add(screenshot())
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
