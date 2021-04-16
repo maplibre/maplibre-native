254c3ec859008ddff9d0eba5c9ec4fd788bd0378
RobLabs.com <roblabs@users.noreply.github.com>
2021-04-15 19:48:47 -0700

    Add test app `iosapp-swiftui` for testing with Swift UI

---
+++ platform/ios/platform/ios/SwiftUI/iosapp_swiftui.swift
@@ -0,0 +1,16 @@
+//
+//  iosapp_swiftui.swift
+//
+//  Copyright © 2021 MapLibre Contributors. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iosapp_swiftui: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
