d9d9103f3013fe5d9cba105482eb8350aa1af013
Tadej Novak <tadej@tano.si>
2022-07-01 20:41:04 +0200

    Fix geojson warning

--- platform/qt/src/utils/geojson.cpp
+++ platform/qt/src/utils/geojson.cpp
@@ -80,7 +80,7 @@ mbgl::Value asPropertyValue(const QVariant &value) {
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     switch (value.typeId()) {
 #else
-    switch (value.type()) {
+    switch (static_cast<QMetaType::Type>(value.type())) {
 #endif
     case QMetaType::UnknownType:
         return mbgl::NullValue {};
@@ -108,7 +108,7 @@ mbgl::FeatureIdentifier asFeatureIdentifier(const QVariant &id) {
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     switch (id.typeId()) {
 #else
-    switch (id.type()) {
+    switch (static_cast<QMetaType::Type>(id.type())) {
 #endif
     case QMetaType::UnknownType:
         return {};
