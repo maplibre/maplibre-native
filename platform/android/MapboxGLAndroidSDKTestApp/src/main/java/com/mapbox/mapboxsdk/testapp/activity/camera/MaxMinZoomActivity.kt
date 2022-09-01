570f6333a752c48f30f799e5130ff6f2190055e0
Birk Skyum <birk.skyum@pm.me>
2022-09-02 01:25:58 +0200

    Lint

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/MaxMinZoomActivity.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/MaxMinZoomActivity.kt
@@ -11,71 +11,71 @@ import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.testapp.R
 import timber.log.Timber
 
-/**
- * Test activity showcasing using maximum and minimum zoom levels to restrict camera movement.
- */
+/** Test activity showcasing using maximum and minimum zoom levels to restrict camera movement. */
 class MaxMinZoomActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mapView: MapView
-    private lateinit var mapboxMap: MapboxMap
-    private val clickListener = OnMapClickListener {
-        if (mapboxMap != null) {
-            mapboxMap!!.setStyle(Style.Builder().fromUri(Style.getPredefinedStyle("Outdoor")))
-        }
-        true
+  private lateinit var mapView: MapView
+  private lateinit var mapboxMap: MapboxMap
+  private val clickListener = OnMapClickListener {
+    if (mapboxMap != null) {
+      mapboxMap!!.setStyle(Style.Builder().fromUri(Style.getPredefinedStyle("Outdoor")))
     }
+    true
+  }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maxmin_zoom)
-        mapView = findViewById<MapView>(R.id.mapView)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
-        mapView.addOnDidFinishLoadingStyleListener(OnDidFinishLoadingStyleListener { Timber.d("Style Loaded") })
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_maxmin_zoom)
+    mapView = findViewById<MapView>(R.id.mapView)
+    mapView.onCreate(savedInstanceState)
+    mapView.getMapAsync(this)
+    mapView.addOnDidFinishLoadingStyleListener(
+        OnDidFinishLoadingStyleListener { Timber.d("Style Loaded") }
+    )
+  }
 
-    override fun onMapReady(map: MapboxMap) {
-        mapboxMap = map
-        mapboxMap!!.setStyle(Style.getPredefinedStyle("Streets"))
-        mapboxMap!!.setMinZoomPreference(3.0)
-        mapboxMap!!.setMaxZoomPreference(5.0)
-        mapboxMap!!.addOnMapClickListener(clickListener)
-    }
+  override fun onMapReady(map: MapboxMap) {
+    mapboxMap = map
+    mapboxMap!!.setStyle(Style.getPredefinedStyle("Streets"))
+    mapboxMap!!.setMinZoomPreference(3.0)
+    mapboxMap!!.setMaxZoomPreference(5.0)
+    mapboxMap!!.addOnMapClickListener(clickListener)
+  }
 
-    override fun onStart() {
-        super.onStart()
-        mapView!!.onStart()
-    }
+  override fun onStart() {
+    super.onStart()
+    mapView!!.onStart()
+  }
 
-    override fun onResume() {
-        super.onResume()
-        mapView!!.onResume()
-    }
+  override fun onResume() {
+    super.onResume()
+    mapView!!.onResume()
+  }
 
-    override fun onPause() {
-        super.onPause()
-        mapView!!.onPause()
-    }
+  override fun onPause() {
+    super.onPause()
+    mapView!!.onPause()
+  }
 
-    override fun onStop() {
-        super.onStop()
-        mapView!!.onStop()
-    }
+  override fun onStop() {
+    super.onStop()
+    mapView!!.onStop()
+  }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView!!.onSaveInstanceState(outState)
-    }
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    mapView!!.onSaveInstanceState(outState)
+  }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        if (mapboxMap != null) {
-            mapboxMap!!.removeOnMapClickListener(clickListener)
-        }
-        mapView!!.onDestroy()
+  override fun onDestroy() {
+    super.onDestroy()
+    if (mapboxMap != null) {
+      mapboxMap!!.removeOnMapClickListener(clickListener)
     }
+    mapView!!.onDestroy()
+  }
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView!!.onLowMemory()
-    }
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView!!.onLowMemory()
+  }
 }
