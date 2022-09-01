6354ead73b8f5d4f3b1c167cd134773ad75467df
Birk Skyum <birk.skyum@pm.me>
2022-09-02 01:43:23 +0200

    Lint indentation

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/ManualZoomActivity.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/ManualZoomActivity.kt
@@ -18,87 +18,92 @@ import com.mapbox.mapboxsdk.testapp.R
  * This includes zoomIn, zoomOut, zoomTo, zoomBy (center and custom focal point).
  */
 class ManualZoomActivity : AppCompatActivity() {
-  private lateinit var mapboxMap: MapboxMap
-  private lateinit var mapView: MapView
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_manual_zoom)
-    mapView = findViewById<View>(R.id.mapView) as MapView
-    mapView!!.onCreate(savedInstanceState)
-    mapView!!.getMapAsync { mapboxMap: MapboxMap ->
-      this@ManualZoomActivity.mapboxMap = mapboxMap
-      mapboxMap.setStyle(Style.Builder().fromUri(Style.getPredefinedStyle("Satellite Hybrid")))
-      val uiSettings = this@ManualZoomActivity.mapboxMap!!.uiSettings
-      uiSettings.setAllGesturesEnabled(false)
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var mapView: MapView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual_zoom)
+        mapView = findViewById<View>(R.id.mapView) as MapView
+        mapView!!.onCreate(savedInstanceState)
+        mapView!!.getMapAsync { mapboxMap: MapboxMap ->
+            this@ManualZoomActivity.mapboxMap = mapboxMap
+            mapboxMap.setStyle(
+                    Style.Builder().fromUri(Style.getPredefinedStyle("Satellite Hybrid"))
+            )
+            val uiSettings = this@ManualZoomActivity.mapboxMap!!.uiSettings
+            uiSettings.setAllGesturesEnabled(false)
+        }
     }
-  }
 
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    menuInflater.inflate(R.menu.menu_zoom, menu)
-    return true
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    return when (item.itemId) {
-      R.id.action_zoom_in -> {
-        mapboxMap!!.animateCamera(CameraUpdateFactory.zoomIn())
-        true
-      }
-      R.id.action_zoom_out -> {
-        mapboxMap!!.animateCamera(CameraUpdateFactory.zoomOut())
-        true
-      }
-      R.id.action_zoom_by -> {
-        mapboxMap!!.animateCamera(CameraUpdateFactory.zoomBy(2.0))
-        true
-      }
-      R.id.action_zoom_to -> {
-        mapboxMap!!.animateCamera(CameraUpdateFactory.zoomTo(2.0))
-        true
-      }
-      R.id.action_zoom_to_point -> {
-        val view = window.decorView
-        mapboxMap!!.animateCamera(
-            CameraUpdateFactory.zoomBy(1.0, Point(view.measuredWidth / 4, view.measuredHeight / 4))
-        )
-        true
-      }
-      else -> super.onOptionsItemSelected(item)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_zoom, menu)
+        return true
     }
-  }
 
-  override fun onStart() {
-    super.onStart()
-    mapView!!.onStart()
-  }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_zoom_in -> {
+                mapboxMap!!.animateCamera(CameraUpdateFactory.zoomIn())
+                true
+            }
+            R.id.action_zoom_out -> {
+                mapboxMap!!.animateCamera(CameraUpdateFactory.zoomOut())
+                true
+            }
+            R.id.action_zoom_by -> {
+                mapboxMap!!.animateCamera(CameraUpdateFactory.zoomBy(2.0))
+                true
+            }
+            R.id.action_zoom_to -> {
+                mapboxMap!!.animateCamera(CameraUpdateFactory.zoomTo(2.0))
+                true
+            }
+            R.id.action_zoom_to_point -> {
+                val view = window.decorView
+                mapboxMap!!.animateCamera(
+                        CameraUpdateFactory.zoomBy(
+                                1.0,
+                                Point(view.measuredWidth / 4, view.measuredHeight / 4)
+                        )
+                )
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
-  override fun onResume() {
-    super.onResume()
-    mapView!!.onResume()
-  }
+    override fun onStart() {
+        super.onStart()
+        mapView!!.onStart()
+    }
 
-  override fun onPause() {
-    super.onPause()
-    mapView!!.onPause()
-  }
+    override fun onResume() {
+        super.onResume()
+        mapView!!.onResume()
+    }
 
-  override fun onStop() {
-    super.onStop()
-    mapView!!.onStop()
-  }
+    override fun onPause() {
+        super.onPause()
+        mapView!!.onPause()
+    }
 
-  override fun onSaveInstanceState(outState: Bundle) {
-    super.onSaveInstanceState(outState)
-    mapView!!.onSaveInstanceState(outState)
-  }
+    override fun onStop() {
+        super.onStop()
+        mapView!!.onStop()
+    }
 
-  override fun onDestroy() {
-    super.onDestroy()
-    mapView!!.onDestroy()
-  }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView!!.onSaveInstanceState(outState)
+    }
 
-  override fun onLowMemory() {
-    super.onLowMemory()
-    mapView!!.onLowMemory()
-  }
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView!!.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView!!.onLowMemory()
+    }
 }
