8ee0d96a0d243573fbca5ed5af1cf71e45c7c732
Birk Skyum <birk.skyum@pm.me>
2022-09-02 01:25:58 +0200

    Lint

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/CameraAnimationTypeActivity.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/camera/CameraAnimationTypeActivity.kt
@@ -17,146 +17,133 @@ import com.mapbox.mapboxsdk.testapp.R
 import timber.log.Timber
 
 /**
- * Test activity showcasing the Camera API and listen to camera updates by animating the camera above London.
- *
+ * Test activity showcasing the Camera API and listen to camera updates by animating the camera
+ * above London.
  *
  * Shows how to use animate, ease and move camera update factory methods.
- *
  */
 class CameraAnimationTypeActivity : AppCompatActivity(), OnMapReadyCallback {
-    private val callback: CancelableCallback = object : CancelableCallback {
+  private val callback: CancelableCallback =
+      object : CancelableCallback {
         override fun onCancel() {
-            Timber.i("Duration onCancel Callback called.")
-            Toast.makeText(
-                this@CameraAnimationTypeActivity.applicationContext,
-                "Ease onCancel Callback called.",
-                Toast.LENGTH_LONG
-            ).show()
+          Timber.i("Duration onCancel Callback called.")
+          Toast.makeText(
+                  this@CameraAnimationTypeActivity.applicationContext,
+                  "Ease onCancel Callback called.",
+                  Toast.LENGTH_LONG
+              )
+              .show()
         }
 
         override fun onFinish() {
-            Timber.i("Duration onFinish Callback called.")
-            Toast.makeText(
-                this@CameraAnimationTypeActivity.applicationContext,
-                "Ease onFinish Callback called.",
-                Toast.LENGTH_LONG
-            ).show()
+          Timber.i("Duration onFinish Callback called.")
+          Toast.makeText(
+                  this@CameraAnimationTypeActivity.applicationContext,
+                  "Ease onFinish Callback called.",
+                  Toast.LENGTH_LONG
+              )
+              .show()
         }
+      }
+  private lateinit var mapboxMap: MapboxMap
+  private lateinit var mapView: MapView
+  private var cameraState = false
+  private val cameraIdleListener = OnCameraIdleListener {
+    if (mapboxMap != null) {
+      Timber.w(mapboxMap!!.cameraPosition.toString())
     }
-    private lateinit var mapboxMap: MapboxMap
-    private lateinit var mapView: MapView
-    private var cameraState = false
-    private val cameraIdleListener = OnCameraIdleListener {
-        if (mapboxMap != null) {
-            Timber.w(mapboxMap!!.cameraPosition.toString())
-        }
-    }
+  }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera_animation_types)
-        mapView = findViewById(R.id.mapView)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_camera_animation_types)
+    mapView = findViewById(R.id.mapView)
+    mapView.onCreate(savedInstanceState)
+    mapView.getMapAsync(this)
+  }
 
-    override fun onMapReady(map: MapboxMap) {
-        mapboxMap = map
-        mapboxMap!!.setStyle(Style.Builder().fromUri(Style.getPredefinedStyle("Streets")))
-        mapboxMap!!.uiSettings.isAttributionEnabled = false
-        mapboxMap!!.uiSettings.isLogoEnabled = false
-        mapboxMap!!.addOnCameraIdleListener(cameraIdleListener)
+  override fun onMapReady(map: MapboxMap) {
+    mapboxMap = map
+    mapboxMap!!.setStyle(Style.Builder().fromUri(Style.getPredefinedStyle("Streets")))
+    mapboxMap!!.uiSettings.isAttributionEnabled = false
+    mapboxMap!!.uiSettings.isLogoEnabled = false
+    mapboxMap!!.addOnCameraIdleListener(cameraIdleListener)
 
-        // handle move button clicks
-        val moveButton = findViewById<View>(R.id.cameraMoveButton)
-        moveButton?.setOnClickListener { view: View? ->
-            val cameraPosition = CameraPosition.Builder()
-                .target(nextLatLng)
-                .zoom(14.0)
-                .tilt(30.0)
-                .tilt(0.0)
-                .build()
-            mapboxMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
-        }
-
-        // handle ease button clicks
-        val easeButton = findViewById<View>(R.id.cameraEaseButton)
-        easeButton?.setOnClickListener { view: View? ->
-            val cameraPosition = CameraPosition.Builder()
-                .target(nextLatLng)
-                .zoom(15.0)
-                .bearing(180.0)
-                .tilt(30.0)
-                .build()
-            mapboxMap!!.easeCamera(
-                CameraUpdateFactory.newCameraPosition(cameraPosition),
-                7500,
-                callback
-            )
-        }
-
-        // handle animate button clicks
-        val animateButton = findViewById<View>(R.id.cameraAnimateButton)
-        animateButton?.setOnClickListener { view: View? ->
-            val cameraPosition = CameraPosition.Builder()
-                .target(nextLatLng)
-                .bearing(270.0)
-                .tilt(20.0)
-                .build()
-            mapboxMap!!.animateCamera(
-                CameraUpdateFactory.newCameraPosition(cameraPosition),
-                7500,
-                callback
-            )
-        }
+    // handle move button clicks
+    val moveButton = findViewById<View>(R.id.cameraMoveButton)
+    moveButton?.setOnClickListener { view: View? ->
+      val cameraPosition =
+          CameraPosition.Builder().target(nextLatLng).zoom(14.0).tilt(30.0).tilt(0.0).build()
+      mapboxMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 
-    private val nextLatLng: LatLng
-        private get() {
-            cameraState = !cameraState
-            return if (cameraState) LAT_LNG_TOWER_BRIDGE else LAT_LNG_LONDON_EYE
-        }
-
-    override fun onStart() {
-        super.onStart()
-        mapView!!.onStart()
+    // handle ease button clicks
+    val easeButton = findViewById<View>(R.id.cameraEaseButton)
+    easeButton?.setOnClickListener { view: View? ->
+      val cameraPosition =
+          CameraPosition.Builder().target(nextLatLng).zoom(15.0).bearing(180.0).tilt(30.0).build()
+      mapboxMap!!.easeCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 7500, callback)
     }
 
-    override fun onResume() {
-        super.onResume()
-        mapView!!.onResume()
+    // handle animate button clicks
+    val animateButton = findViewById<View>(R.id.cameraAnimateButton)
+    animateButton?.setOnClickListener { view: View? ->
+      val cameraPosition =
+          CameraPosition.Builder().target(nextLatLng).bearing(270.0).tilt(20.0).build()
+      mapboxMap!!.animateCamera(
+          CameraUpdateFactory.newCameraPosition(cameraPosition),
+          7500,
+          callback
+      )
+    }
+  }
+
+  private val nextLatLng: LatLng
+    private get() {
+      cameraState = !cameraState
+      return if (cameraState) LAT_LNG_TOWER_BRIDGE else LAT_LNG_LONDON_EYE
     }
 
-    override fun onPause() {
-        super.onPause()
-        mapView!!.onPause()
-    }
+  override fun onStart() {
+    super.onStart()
+    mapView!!.onStart()
+  }
 
-    override fun onStop() {
-        super.onStop()
-        mapView!!.onStop()
-    }
+  override fun onResume() {
+    super.onResume()
+    mapView!!.onResume()
+  }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView!!.onSaveInstanceState(outState)
-    }
+  override fun onPause() {
+    super.onPause()
+    mapView!!.onPause()
+  }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        if (mapboxMap != null) {
-            mapboxMap!!.removeOnCameraIdleListener(cameraIdleListener)
-        }
-        mapView!!.onDestroy()
-    }
+  override fun onStop() {
+    super.onStop()
+    mapView!!.onStop()
+  }
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView!!.onLowMemory()
-    }
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    mapView!!.onSaveInstanceState(outState)
+  }
 
-    companion object {
-        private val LAT_LNG_LONDON_EYE = LatLng(51.50325, -0.11968)
-        private val LAT_LNG_TOWER_BRIDGE = LatLng(51.50550, -0.07520)
+  override fun onDestroy() {
+    super.onDestroy()
+    if (mapboxMap != null) {
+      mapboxMap!!.removeOnCameraIdleListener(cameraIdleListener)
     }
+    mapView!!.onDestroy()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView!!.onLowMemory()
+  }
+
+  companion object {
+    private val LAT_LNG_LONDON_EYE = LatLng(51.50325, -0.11968)
+    private val LAT_LNG_TOWER_BRIDGE = LatLng(51.50550, -0.07520)
+  }
 }
