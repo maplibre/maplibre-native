49d4dc736a1706b51e6a94548069a9d944055654
Birk Skyum <birk.skyum@pm.me>
2022-09-04 01:54:55 +0200

    Refactor HeatmapStyleLayerTests

--- platform/ios/platform/darwin/test/MGLHeatmapStyleLayerTests.mm
+++ platform/ios/platform/darwin/test/MGLHeatmapStyleLayerTests.mm
@@ -64,10 +64,12 @@
                               @"heatmapIntensity should round-trip constant value expressions.");
 
         constantExpression = [NSExpression expressionWithFormat:@"1"];
-#if TARGET_OS_IPHONE
-        XCTExpectFailure(@"Awaiting unit test refactoring for https://github.com/maplibre/maplibre-gl-native/issues/331");
-#endif
-        NSExpression *functionExpression = [NSExpression expressionWithFormat:@"mgl_step:from:stops:($zoomLevel, %@, %@)", constantExpression, @{@18: constantExpression}];
+        NSExpression *stops = [NSExpression expressionForConstantValue:@{@18: constantExpression}];
+
+        NSExpression *functionExpression = [NSExpression mgl_expressionForSteppingExpression:NSExpression.zoomLevelVariableExpression
+            fromExpression:constantExpression
+            stops:stops];
+
         layer.heatmapIntensity = functionExpression;
 
         {
@@ -79,7 +81,6 @@
 
         XCTAssertEqual(rawLayer->getHeatmapIntensity(), propertyValue,
                        @"Setting heatmapIntensity to a camera expression should update heatmap-intensity.");
-        XCTExpectFailure(@"Awaiting unit test refactoring for https://github.com/maplibre/maplibre-gl-native/issues/331");
         XCTAssertEqualObjects(layer.heatmapIntensity, functionExpression,
                               @"heatmapIntensity should round-trip camera expressions.");
 
