2fe0f2e1897b4475e2c7176a6c55f4be6dc005cd
Birk Skyum <birk.skyum@pm.me>
2022-09-08 18:29:27 +0200

    Port info window tests

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/infowindow/DynamicInfoWindowAdapterActivity.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/infowindow/DynamicInfoWindowAdapterActivity.kt
@@ -1,159 +1,147 @@
-package com.mapbox.mapboxsdk.testapp.activity.infowindow;
+package com.mapbox.mapboxsdk.testapp.activity.infowindow
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
-
-import com.mapbox.mapboxsdk.annotations.InfoWindow;
-import com.mapbox.mapboxsdk.annotations.Marker;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.testapp.R;
-import com.mapbox.mapboxsdk.testapp.utils.IconUtils;
-
-import java.util.Locale;
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.mapbox.mapboxsdk.annotations.Marker
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.MapboxMap.InfoWindowAdapter
+import com.mapbox.mapboxsdk.maps.MapboxMap.OnMapClickListener
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.testapp.R
+import com.mapbox.mapboxsdk.testapp.utils.IconUtils
+import java.util.*
 
 /**
  * Test activity showcasing how to dynamically update InfoWindow when Using an MapboxMap.InfoWindowAdapter.
  */
-public class DynamicInfoWindowAdapterActivity extends AppCompatActivity implements OnMapReadyCallback {
+class DynamicInfoWindowAdapterActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var mapView: MapView
+    private var marker: Marker? = null
+    private val mapClickListener = OnMapClickListener { point ->
+        if (marker == null) {
+            return@OnMapClickListener false
+        }
 
-  private static final LatLng PARIS = new LatLng(48.864716, 2.349014);
+        // Distance from click to marker
+        val distanceKm = marker!!.position.distanceTo(point) / 1000
 
-  private MapboxMap mapboxMap;
-  private MapView mapView;
-  private Marker marker;
+        // Get the info window
+        val infoWindow = marker!!.infoWindow
 
-  private MapboxMap.OnMapClickListener mapClickListener = new MapboxMap.OnMapClickListener() {
-    @Override
-    public boolean onMapClick(@NonNull LatLng point) {
-      if (marker == null) {
-        return false;
-      }
-
-      // Distance from click to marker
-      double distanceKm = marker.getPosition().distanceTo(point) / 1000;
-
-      // Get the info window
-      final InfoWindow infoWindow = marker.getInfoWindow();
-
-      // Get the view from the info window
-      if (infoWindow != null && infoWindow.getView() != null) {
-        // Set the new text on the text view in the info window
-        TextView textView = (TextView) infoWindow.getView();
-        textView.setText(String.format(Locale.getDefault(), "%.2fkm", distanceKm));
-        // Update the info window position (as the text length changes)
-        textView.post(infoWindow::update);
-      }
-      return true;
+        // Get the view from the info window
+        if (infoWindow != null && infoWindow.view != null) {
+            // Set the new text on the text view in the info window
+            val textView = infoWindow.view as TextView?
+            textView!!.text = String.format(Locale.getDefault(), "%.2fkm", distanceKm)
+            // Update the info window position (as the text length changes)
+            textView.post { infoWindow.update() }
+        }
+        true
     }
-  };
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_infowindow_adapter);
-
-    mapView = findViewById(R.id.mapView);
-    mapView.onCreate(savedInstanceState);
-    mapView.getMapAsync(this);
-  }
-
-  @Override
-  public void onMapReady(@NonNull MapboxMap map) {
-    mapboxMap = map;
-
-    map.setStyle(Style.getPredefinedStyle("Streets"));
-
-    // Add info window adapter
-    addCustomInfoWindowAdapter(mapboxMap);
-
-    // Keep info windows open on click
-    mapboxMap.getUiSettings().setDeselectMarkersOnTap(false);
-
-    // Add a marker
-    marker = addMarker(mapboxMap);
-    mapboxMap.selectMarker(marker);
-
-    // On map click, change the info window contents
-    mapboxMap.addOnMapClickListener(mapClickListener);
-
-    // Focus on Paris
-    mapboxMap.animateCamera(CameraUpdateFactory.newLatLng(PARIS));
-  }
-
-  private Marker addMarker(MapboxMap mapboxMap) {
-    return mapboxMap.addMarker(
-      new MarkerOptions()
-        .position(PARIS)
-        .icon(IconUtils.drawableToIcon(this, R.drawable.ic_location_city,
-          ResourcesCompat.getColor(getResources(), R.color.mapbox_blue, getTheme()))
-        ));
-  }
-
-  private void addCustomInfoWindowAdapter(final MapboxMap mapboxMap) {
-    final int padding = (int) getResources().getDimension(R.dimen.attr_margin);
-    mapboxMap.setInfoWindowAdapter(marker -> {
-      TextView textView = new TextView(DynamicInfoWindowAdapterActivity.this);
-      textView.setText(marker.getTitle());
-      textView.setBackgroundColor(Color.WHITE);
-      textView.setText(R.string.action_calculate_distance);
-      textView.setPadding(padding, padding, padding, padding);
-      return textView;
-    });
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-    mapView.onStart();
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    mapView.onResume();
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-    mapView.onPause();
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    mapView.onStop();
-  }
-
-  @Override
-  protected void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    mapView.onSaveInstanceState(outState);
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    if (mapboxMap != null) {
-      mapboxMap.removeOnMapClickListener(mapClickListener);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_infowindow_adapter)
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
-    mapView.onDestroy();
-  }
 
-  @Override
-  public void onLowMemory() {
-    super.onLowMemory();
-    mapView.onLowMemory();
-  }
+    override fun onMapReady(map: MapboxMap) {
+        mapboxMap = map
+        map.setStyle(Style.getPredefinedStyle("Streets"))
+
+        // Add info window adapter
+        addCustomInfoWindowAdapter(mapboxMap!!)
+
+        // Keep info windows open on click
+        mapboxMap!!.uiSettings.isDeselectMarkersOnTap = false
+
+        // Add a marker
+        marker = addMarker(mapboxMap!!)
+        mapboxMap!!.selectMarker(marker!!)
+
+        // On map click, change the info window contents
+        mapboxMap!!.addOnMapClickListener(mapClickListener)
+
+        // Focus on Paris
+        mapboxMap!!.animateCamera(CameraUpdateFactory.newLatLng(PARIS))
+    }
+
+    private fun addMarker(mapboxMap: MapboxMap): Marker {
+        return mapboxMap.addMarker(
+            MarkerOptions()
+                .position(PARIS)
+                .icon(
+                    IconUtils.drawableToIcon(
+                        this,
+                        R.drawable.ic_location_city,
+                        ResourcesCompat.getColor(resources, R.color.mapbox_blue, theme)
+                    )
+                )
+        )
+    }
+
+    private fun addCustomInfoWindowAdapter(mapboxMap: MapboxMap) {
+        val padding = resources.getDimension(R.dimen.attr_margin).toInt()
+        mapboxMap.infoWindowAdapter = InfoWindowAdapter { marker: Marker ->
+            val textView = TextView(this@DynamicInfoWindowAdapterActivity)
+            textView.text = marker.title
+            textView.setBackgroundColor(Color.WHITE)
+            textView.setText(R.string.action_calculate_distance)
+            textView.setPadding(padding, padding, padding, padding)
+            textView
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView!!.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView!!.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView!!.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView!!.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView!!.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mapboxMap != null) {
+            mapboxMap!!.removeOnMapClickListener(mapClickListener)
+        }
+        mapView!!.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView!!.onLowMemory()
+    }
+
+    companion object {
+        private val PARIS = LatLng(48.864716, 2.349014)
+    }
 }
--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/infowindow/InfoWindowActivity.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/infowindow/InfoWindowActivity.kt
@@ -1,200 +1,197 @@
-package com.mapbox.mapboxsdk.testapp.activity.infowindow;
+package com.mapbox.mapboxsdk.testapp.activity.infowindow
 
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.mapbox.mapboxsdk.annotations.Marker;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.testapp.R;
-
-import java.text.DecimalFormat;
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.annotations.Marker
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.MapboxMap.OnInfoWindowClickListener
+import com.mapbox.mapboxsdk.maps.MapboxMap.OnInfoWindowCloseListener
+import com.mapbox.mapboxsdk.maps.MapboxMap.OnInfoWindowLongClickListener
+import com.mapbox.mapboxsdk.maps.MapboxMap.OnMapLongClickListener
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.testapp.R
+import java.text.DecimalFormat
 
 /**
  * Test activity showcasing using the InfoWindow API above Washington D.C.
- * <p>
+ *
+ *
  * Allows to test mulitple concurrently open InfoWindows.
- * </p>
+ *
  */
-public class InfoWindowActivity extends AppCompatActivity
-  implements OnMapReadyCallback, MapboxMap.OnInfoWindowCloseListener, MapboxMap.OnInfoWindowClickListener,
-  MapboxMap.OnInfoWindowLongClickListener {
+class InfoWindowActivity :
+    AppCompatActivity(),
+    OnMapReadyCallback,
+    OnInfoWindowCloseListener,
+    OnInfoWindowClickListener,
+    OnInfoWindowLongClickListener {
+    private var mapboxMap: MapboxMap? = null
+    private var mapView: MapView? = null
+    private var customMarker: Marker? = null
+    private val mapLongClickListener = OnMapLongClickListener { point ->
+        if (customMarker != null) {
+            // Remove previous added marker
+            mapboxMap!!.removeAnnotation(customMarker!!)
+            customMarker = null
+        }
 
-  private MapboxMap mapboxMap;
-  private MapView mapView;
-  private Marker customMarker;
-
-  private MapboxMap.OnMapLongClickListener mapLongClickListener = new MapboxMap.OnMapLongClickListener() {
-    @Override
-    public boolean onMapLongClick(@NonNull LatLng point) {
-      if (customMarker != null) {
-        // Remove previous added marker
-        mapboxMap.removeAnnotation(customMarker);
-        customMarker = null;
-      }
-
-      // Add marker on long click location with default marker image
-      customMarker = mapboxMap.addMarker(new MarkerOptions()
-        .title("Custom Marker")
-        .snippet(new DecimalFormat("#.#####").format(point.getLatitude()) + ", "
-          + new DecimalFormat("#.#####").format(point.getLongitude()))
-        .position(point));
-      return true;
+        // Add marker on long click location with default marker image
+        customMarker = mapboxMap!!.addMarker(
+            MarkerOptions()
+                .title("Custom Marker")
+                .snippet(
+                    DecimalFormat("#.#####").format(point.latitude) + ", " +
+                        DecimalFormat("#.#####").format(point.longitude)
+                )
+                .position(point)
+        )
+        true
     }
-  };
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_infowindow);
-
-    mapView = (MapView) findViewById(R.id.mapView);
-    mapView.onCreate(savedInstanceState);
-    mapView.getMapAsync(this);
-  }
-
-  @Override
-  public void onMapReady(@NonNull MapboxMap mapboxMap) {
-    this.mapboxMap = mapboxMap;
-    mapboxMap.setStyle(Style.getPredefinedStyle("Streets"), style -> {
-      addMarkers();
-      addInfoWindowListeners();
-    });
-  }
-
-  private void addMarkers() {
-    mapboxMap.addMarker(new MarkerOptions()
-      .title("Intersection")
-      .snippet("H St NW with 15th St NW")
-      .position(new LatLng(38.9002073, -77.03364419)));
-
-    mapboxMap.addMarker(new MarkerOptions().title("Intersection")
-      .snippet("E St NW with 17th St NW")
-      .position(new LatLng(38.8954236, -77.0394623)));
-
-    mapboxMap.addMarker(new MarkerOptions().title("The Ellipse").position(new LatLng(38.89393, -77.03654)));
-
-    mapboxMap.addMarker(new MarkerOptions().position(new LatLng(38.89596, -77.03434)));
-
-    mapboxMap.addMarker(new MarkerOptions().snippet("Lafayette Square").position(new LatLng(38.89949, -77.03656)));
-
-    Marker marker = mapboxMap.addMarker(new MarkerOptions()
-      .title("White House")
-      .snippet("The official residence and principal workplace of the President of the United States, "
-        + "located at 1600 Pennsylvania Avenue NW in Washington, D.C. It has been the residence of every"
-        + "U.S. president since John Adams in 1800.")
-      .position(new LatLng(38.897705003219784, -77.03655168667463)));
-
-    // open InfoWindow at startup
-    mapboxMap.selectMarker(marker);
-  }
-
-  private void addInfoWindowListeners() {
-    mapboxMap.setOnInfoWindowCloseListener(this);
-    mapboxMap.addOnMapLongClickListener(mapLongClickListener);
-    mapboxMap.setOnInfoWindowClickListener(this);
-    mapboxMap.setOnInfoWindowLongClickListener(this);
-  }
-
-  private void toggleConcurrentInfoWindow(boolean allowConcurrentInfoWindow) {
-    mapboxMap.deselectMarkers();
-    mapboxMap.setAllowConcurrentMultipleOpenInfoWindows(allowConcurrentInfoWindow);
-  }
-
-  private void toggleDeselectMarkersOnTap(boolean deselectMarkersOnTap) {
-    mapboxMap.getUiSettings().setDeselectMarkersOnTap(deselectMarkersOnTap);
-  }
-
-  @Override
-  public boolean onInfoWindowClick(@NonNull Marker marker) {
-    Toast.makeText(getApplicationContext(), "OnClick: " + marker.getTitle(), Toast.LENGTH_LONG).show();
-    // returning true will leave the info window open
-    return false;
-  }
-
-  @Override
-  public void onInfoWindowClose(Marker marker) {
-    Toast.makeText(getApplicationContext(), "OnClose: " + marker.getTitle(), Toast.LENGTH_LONG).show();
-  }
-
-  @Override
-  public void onInfoWindowLongClick(Marker marker) {
-    Toast.makeText(getApplicationContext(), "OnLongClick: " + marker.getTitle(), Toast.LENGTH_LONG).show();
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-    mapView.onStart();
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    mapView.onResume();
-  }
-
-  @Override
-  protected void onPause() {
-    super.onPause();
-    mapView.onPause();
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    mapView.onStop();
-  }
-
-  @Override
-  protected void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-    mapView.onSaveInstanceState(outState);
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    if (mapboxMap != null) {
-      mapboxMap.removeOnMapLongClickListener(mapLongClickListener);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_infowindow)
+        mapView = findViewById<View>(R.id.mapView) as MapView
+        mapView!!.onCreate(savedInstanceState)
+        mapView!!.getMapAsync(this)
     }
-    mapView.onDestroy();
-  }
 
-  @Override
-  public void onLowMemory() {
-    super.onLowMemory();
-    mapView.onLowMemory();
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_infowindow, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.action_toggle_concurrent_infowindow:
-        toggleConcurrentInfoWindow(!item.isChecked());
-        item.setChecked(!item.isChecked());
-        return true;
-      case R.id.action_toggle_deselect_markers_on_tap:
-        toggleDeselectMarkersOnTap(!item.isChecked());
-        item.setChecked(!item.isChecked());
-        return true;
-      default:
-        return super.onOptionsItemSelected(item);
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.getPredefinedStyle("Streets")) { style: Style? ->
+            addMarkers()
+            addInfoWindowListeners()
+        }
     }
-  }
+
+    private fun addMarkers() {
+        mapboxMap!!.addMarker(
+            MarkerOptions()
+                .title("Intersection")
+                .snippet("H St NW with 15th St NW")
+                .position(LatLng(38.9002073, -77.03364419))
+        )
+        mapboxMap!!.addMarker(
+            MarkerOptions().title("Intersection")
+                .snippet("E St NW with 17th St NW")
+                .position(LatLng(38.8954236, -77.0394623))
+        )
+        mapboxMap!!.addMarker(
+            MarkerOptions().title("The Ellipse").position(LatLng(38.89393, -77.03654))
+        )
+        mapboxMap!!.addMarker(MarkerOptions().position(LatLng(38.89596, -77.03434)))
+        mapboxMap!!.addMarker(
+            MarkerOptions().snippet("Lafayette Square").position(LatLng(38.89949, -77.03656))
+        )
+        val marker = mapboxMap!!.addMarker(
+            MarkerOptions()
+                .title("White House")
+                .snippet(
+                    "The official residence and principal workplace of the President of the United States, " +
+                        "located at 1600 Pennsylvania Avenue NW in Washington, D.C. It has been the residence of every" +
+                        "U.S. president since John Adams in 1800."
+                )
+                .position(LatLng(38.897705003219784, -77.03655168667463))
+        )
+
+        // open InfoWindow at startup
+        mapboxMap!!.selectMarker(marker)
+    }
+
+    private fun addInfoWindowListeners() {
+        mapboxMap!!.onInfoWindowCloseListener = this
+        mapboxMap!!.addOnMapLongClickListener(mapLongClickListener)
+        mapboxMap!!.onInfoWindowClickListener = this
+        mapboxMap!!.onInfoWindowLongClickListener = this
+    }
+
+    private fun toggleConcurrentInfoWindow(allowConcurrentInfoWindow: Boolean) {
+        mapboxMap!!.deselectMarkers()
+        mapboxMap!!.isAllowConcurrentMultipleOpenInfoWindows = allowConcurrentInfoWindow
+    }
+
+    private fun toggleDeselectMarkersOnTap(deselectMarkersOnTap: Boolean) {
+        mapboxMap!!.uiSettings.isDeselectMarkersOnTap = deselectMarkersOnTap
+    }
+
+    override fun onInfoWindowClick(marker: Marker): Boolean {
+        Toast.makeText(applicationContext, "OnClick: " + marker.title, Toast.LENGTH_LONG).show()
+        // returning true will leave the info window open
+        return false
+    }
+
+    override fun onInfoWindowClose(marker: Marker) {
+        Toast.makeText(applicationContext, "OnClose: " + marker.title, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onInfoWindowLongClick(marker: Marker) {
+        Toast.makeText(applicationContext, "OnLongClick: " + marker.title, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView!!.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView!!.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView!!.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView!!.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView!!.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mapboxMap != null) {
+            mapboxMap!!.removeOnMapLongClickListener(mapLongClickListener)
+        }
+        mapView!!.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView!!.onLowMemory()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_infowindow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_toggle_concurrent_infowindow -> {
+                toggleConcurrentInfoWindow(!item.isChecked)
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_toggle_deselect_markers_on_tap -> {
+                toggleDeselectMarkersOnTap(!item.isChecked)
+                item.isChecked = !item.isChecked
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
