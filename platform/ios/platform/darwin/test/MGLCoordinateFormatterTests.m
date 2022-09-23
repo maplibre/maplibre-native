ac5322cb4af1bba76115ae8c970511b760074b3d
Birk Skyum <birk.skyum@pm.me>
2022-09-23 09:42:49 +0200

    change condition

--- platform/ios/platform/darwin/test/MGLCoordinateFormatterTests.m
+++ platform/ios/platform/darwin/test/MGLCoordinateFormatterTests.m
@@ -24,7 +24,7 @@
     coordinate = CLLocationCoordinate2DMake(38.9131982, -77.0325453144239);
     XCTAssertEqualObjects([shortFormatter stringFromCoordinate:coordinate], @"38°54′48″N, 77°1′57″W");
     XCTAssertEqualObjects([mediumFormatter stringFromCoordinate:coordinate], @"38°54′48″ north, 77°1′57″ west");
-    #ifdef TARGET_OS_MAC
+    #if TARGET_OS_MAC
     XCTAssertEqualObjects([longFormatter stringFromCoordinate:coordinate], @"38 degrees, 54 minutes, and 48 seconds north by 77 degrees, 1 minute, and 57 seconds west");
     #elif TARGET_OS_IPHONE
     XCTAssertEqualObjects([longFormatter stringFromCoordinate:coordinate], @"38 degree(s), 54 minute(s), and 48 second(s) north by 77 degree(s), 1 minute(s), and 57 second(s) west");
@@ -37,7 +37,7 @@
     coordinate = CLLocationCoordinate2DMake(38.9131982, -77.0325453144239);
     XCTAssertEqualObjects([shortFormatter stringFromCoordinate:coordinate], @"38°55′N, 77°2′W");
     XCTAssertEqualObjects([mediumFormatter stringFromCoordinate:coordinate], @"38°55′ north, 77°2′ west");
-    #ifdef TARGET_OS_MAC
+    #if TARGET_OS_MAC
     XCTAssertEqualObjects([longFormatter stringFromCoordinate:coordinate], @"38 degrees and 55 minutes north by 77 degrees and 2 minutes west");
     #elif TARGET_OS_IPHONE
     XCTAssertEqualObjects([longFormatter stringFromCoordinate:coordinate], @"38 degree(s) and 55 minute(s) north by 77 degree(s) and 2 minute(s) west");
@@ -52,7 +52,7 @@
     XCTAssertEqualObjects([shortFormatter stringFromCoordinate:coordinate], @"39°N, 77°W");
     XCTAssertEqualObjects([mediumFormatter stringFromCoordinate:coordinate], @"39° north, 77° west");
     
-    #ifdef TARGET_OS_MAC
+    #if TARGET_OS_MAC
     XCTAssertEqualObjects([longFormatter stringFromCoordinate:coordinate], @"39 degrees north by 77 degrees west");
     #elif TARGET_OS_IPHONE
     XCTAssertEqualObjects([longFormatter stringFromCoordinate:coordinate], @"39 degree(s) north by 77 degree(s) west");
