95362c55593e0be28b58bf26ea835bd3b8c32564
Birk Skyum <birk.skyum@pm.me>
2022-12-17 22:39:58 +0100

    Migrate android test app models to kotlin

---
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/model/customlayer/ExampleCustomLayer.kt
@@ -0,0 +1,17 @@
+package com.mapbox.mapboxsdk.testapp.model.customlayer
+
+import androidx.annotation.Keep
+
+@Keep
+object ExampleCustomLayer {
+    init {
+        System.loadLibrary("example-custom-layer")
+    }
+
+    external fun createContext(): Long
+    external fun setColor(red: Float, green: Float, blue: Float, alpha: Float)
+    var InitializeFunction: Long = 0
+    var RenderFunction: Long = 0
+    var ContextLostFunction: Long = 0
+    var DeinitializeFunction: Long = 0
+}
