c1a887a91368582f98af9dd1b4d70736c31ddb9c
Bart Louwers <bart@emeel.net>
2023-01-18 02:07:14 +0100

    initial container Nix-based Android CI container

---
+++ platform/android/docker/flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "My Android project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+    android.url = "github:tadfisher/android-nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, devshell, flake-utils, android }:
+    {
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) android-sdk android-studio;
+      };
+    }
+    //
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            devshell.overlay
+            self.overlay
+          ];
+        };
+        jdk = pkgs.jdk;
+        android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+          # Useful packages for building and testing.
+          build-tools-30-0-3
+          cmdline-tools-latest
+          emulator
+          platform-tools
+          platforms-android-30
+
+          # Other useful packages for a development environment.
+          # sources-android-30
+          # system-images-android-30-google-apis-x86
+          # system-images-android-30-google-apis-playstore-x86
+        ]);
+        paths = with pkgs; [
+          cmake
+          ninja
+          bash
+          nodejs
+          gnumake
+          coreutils
+          gnugrep
+          gnused
+          git
+          gradle
+        ];
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "maplibre-gl-native-android-builder";
+          created = "now";
+          extraCommands = ''mkdir tmp
+					chmod 1777 tmp'';
+          config = {
+            Env = let androidHome = "${android-sdk}/share/android-sdk"; in
+              [
+                ''JAVA_HOME=${jdk.home}''
+                ''ANDROID_SDK_ROOT=${androidHome}''
+                ''ANDROID_HOME=${androidHome}''
+                ''GRADLE_OPTS=-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/30.0.3/aapt2''
+              ];
+          };
+          contents = paths ++ [ pkgs.dockerTools.usrBinEnv ];
+        };
+      in
+      {
+        packages = {
+
+          /*   android-studio = pkgs.androidStudioPackages.stable; */
+          /*   # android-studio = pkgs.androidStudioPackages.beta; */
+          /*   # android-studio = pkgs.androidStudioPackages.preview; */
+          /*   # android-studio = pkgs.androidStudioPackage.canary; */
+          /*   inherit (pkgs) cmake nodejs gnumake coreutils gnugrep gnused git; */
+          docker = dockerImage;
+        };
+
+        defaultPackage = dockerImage;
+
+        devShell = import ./devshell.nix { inherit pkgs; };
+      }
+    );
+}
+
