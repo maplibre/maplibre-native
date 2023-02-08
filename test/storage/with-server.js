ea591b8c59c7b71ec7f0e81e12c8b134c4f31731
Bart Louwers <bart@emeel.net>
2023-02-08 11:01:31 +0100

    add logging to with-server.js

--- test/storage/with-server.js
+++ test/storage/with-server.js
@@ -12,18 +12,19 @@ const serverScript = process.argv[2];
 const prog = process.argv[3];
 const args = process.argv.slice(4);
 
-const server = spawn('node', [serverScript])
+
+const sameStdio = {	stdio: [process.stdin, process.stdout, process.stderr]};
+
+const server = spawn('node', [serverScript], sameStdio);
 
 server.stdout.on('data', (data) => {
 	if (data.toString() === "OK") {
-		const test = spawn(prog, args, {
-			stdio: [process.stdin, process.stdout, process.stderr]
-		});
-
+		const test = spawn(prog, args, sameStdio);
 
 		test.on('close', (code) => {
+			console.error(`Sub process exited with code '${code}', killing server`);
 			server.kill();
 			process.exit(code);
-		})
+		});
 	}
 });
