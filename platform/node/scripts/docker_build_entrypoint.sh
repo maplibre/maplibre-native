207b59a39960640d2a9fcc8ae1acc8f0b8da4d11
acalcutt <acalcutt@techidiots.net>
2023-02-15 00:47:11 -0500

    check disk space

--- platform/node/scripts/docker_build_entrypoint.sh
+++ platform/node/scripts/docker_build_entrypoint.sh
@@ -5,11 +5,15 @@ cat /etc/os-release
 uname -m
 
 cd /usr/src/app
+df -h
 npm ci --ignore-scripts
+df -h
 ccache --clear --set-config cache_dir=~/.ccache
 cmake . -B build -G Ninja  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER=gcc-10 -DCMAKE_CXX_COMPILER=g++-10
+df -h
 cmake --build build -j $(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null)
 cp -R ./lib/ /data/lib/
+df -h
 
 xvfb-run --auto-servernum ./build/mbgl-render-test-runner --manifestPath metrics/macos-xcode11-release-style.json
 xvfb-run --auto-servernum npm test
