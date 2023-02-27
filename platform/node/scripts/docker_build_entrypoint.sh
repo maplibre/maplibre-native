ea91ffc6b05bdedbf4b54395d6bd18375feb7433
acalcutt <acalcutt@techidiots.net>
2023-02-27 18:56:37 -0500

    put back docker process changed in https://github.com/maplibre/maplibre-gl-native/pull/804

--- platform/node/scripts/docker_build_entrypoint.sh
+++ platform/node/scripts/docker_build_entrypoint.sh
@@ -4,12 +4,13 @@ node -v
 cat /etc/os-release
 uname -m
 
-#build lib files
-cd /data/
+#Build lib files
+cd /usr/src/app/
 npm ci --ignore-scripts
 ccache --clear --set-config cache_dir=~/.ccache
 cmake . -B build -G Ninja  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER=gcc-10 -DCMAKE_CXX_COMPILER=g++-10
 cmake --build build -j $(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null)
+cp -R ./lib/ /data/lib/
 
 #Test
 xvfb-run --auto-servernum ./build/mbgl-render-test-runner --manifestPath metrics/linux-gcc8-release-style.json
