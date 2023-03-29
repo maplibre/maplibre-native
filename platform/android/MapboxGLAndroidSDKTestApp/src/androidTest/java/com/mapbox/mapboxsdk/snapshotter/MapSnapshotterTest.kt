dfd3715cf5e44017e53b62f2e09a98e975a188c5
Bart Louwers <bart@emeel.net>
2023-03-29 12:54:34 +0200

    Reproduce error with test

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/snapshotter/MapSnapshotterTest.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/snapshotter/MapSnapshotterTest.kt
@@ -7,6 +7,9 @@ import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.style.layers.BackgroundLayer
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.mapboxsdk.style.layers.RasterLayer
+import com.mapbox.mapboxsdk.style.sources.RasterSource
+import com.mapbox.mapboxsdk.style.sources.Source
 import com.mapbox.mapboxsdk.testapp.activity.FeatureOverviewActivity
 import org.junit.Assert
 import org.junit.Assert.assertNotNull
@@ -16,6 +19,17 @@ import org.junit.runner.RunWith
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
+
+fun commonOptions(): MapSnapshotter.Options  {
+    return MapSnapshotter.Options(512, 512)
+        .withPixelRatio(1.0f)
+        .withCameraPosition(
+            CameraPosition.Builder()
+                .zoom(12.0)
+                .target(LatLng(51.145495, 5.742234))
+                .build()
+        )
+}
 
 /**
  * Integration test that validates if a snapshotter creation
@@ -27,38 +41,51 @@ class MapSnapshotterTest {
     @JvmField
     var rule = ActivityTestRule(FeatureOverviewActivity::class.java)
 
-    private val countDownLatch = CountDownLatch(1)
+    private val optionsVariants: Array<() -> MapSnapshotter.Options> = arrayOf(
+        {
+            val options = commonOptions()
+            val bg = BackgroundLayer("rand_tint")
+            bg.setProperties(PropertyFactory.backgroundColor("rgba(255,128,0,0.7)"))
+            val styleBuilder = Style.Builder().fromUri(Style.getPredefinedStyle("Satellite Hybrid"))
+                .withLayerAbove(bg, "country-label")
+            options.withStyleBuilder(styleBuilder)
+        },
+        {
+            val options = commonOptions()
+            val styleBuilder = Style.Builder().fromJson(
+                rule.activity.assets.open("demotiles.json").bufferedReader().readText()
+            )
+            val source: Source =
+                RasterSource("my-raster-source", "maptiler://sources/satellite", 512)
+            styleBuilder.withLayerAbove(
+                RasterLayer("satellite-layer", "my-raster-source"),
+                "country_1"
+            )
+            styleBuilder.withSource(source)
+            options.withStyleBuilder(styleBuilder)
+        }
+    )
+    private val countDownLatch = CountDownLatch(optionsVariants.size)
 
     @Test
     fun mapSnapshotter() {
-        var mapSnapshotter: MapSnapshotter?
-        rule.activity.runOnUiThread {
-            val bg = BackgroundLayer("rand_tint")
-            bg.setProperties(PropertyFactory.backgroundColor("rgba(255,128,0,0.7)"))
-            val options = MapSnapshotter.Options(512, 512)
-                .withPixelRatio(1.0f)
-                .withStyleBuilder(
-                    Style.Builder().fromUri(Style.getPredefinedStyle("Satellite Hybrid"))
-                        .withLayerAbove(bg, "country-label")
+        for (options in optionsVariants) {
+            var mapSnapshotter: MapSnapshotter?
+            rule.activity.runOnUiThread {
+                mapSnapshotter = MapSnapshotter(rule.activity, options())
+                mapSnapshotter!!.start(
+                    {
+                        assertNotNull(it)
+                        assertNotNull(it.bitmap)
+                        countDownLatch.countDown()
+                    },
+                    {
+                        Assert.fail(it)
+                    }
                 )
-                .withCameraPosition(
-                    CameraPosition.Builder()
-                        .zoom(12.0)
-                        .target(LatLng(51.145495, 5.742234))
-                        .build()
-                )
-            mapSnapshotter = MapSnapshotter(rule.activity, options)
-            mapSnapshotter!!.start(
-                {
-                    assertNotNull(it)
-                    assertNotNull(it.bitmap)
-                    countDownLatch.countDown()
-                },
-                {
-                    Assert.fail(it)
-                }
-            )
+            }
         }
+
         if (!countDownLatch.await(30, TimeUnit.SECONDS)) {
             throw TimeoutException()
         }
