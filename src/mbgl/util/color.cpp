bd2a8affbbbb25fa9797d6fbd3f9b1a07923b229
Bart Louwers <bart@emeel.net>
2023-04-29 15:22:53 +0200

    fix other messed up file

--- src/mbgl/util/color.cpp
+++ src/mbgl/util/color.cpp
@@ -27,33 +27,32 @@ std::array<double, 4> Color::toArray() const {
     if (a == 0) {
         return {{0, 0, 0, 0}};
     } else {
-        return {
-            {
-                r * 255 / a,
-                g * 255 / a,
-                b * 255 / a,
-                floor(a * 100 + .5) / 100 // round to 2 decimal places}
-            };
-        }
+        return {{
+            r * 255 / a,
+            g * 255 / a,
+            b * 255 / a,
+            floor(a * 100 + .5) / 100 // round to 2 decimal places
+        }};
     }
+}
 
-    mbgl::Value Color::toObject() const {
-        return mapbox::base::ValueObject{
-            {"r", static_cast<double>(r)},
-            {"g", static_cast<double>(g)},
-            {"b", static_cast<double>(b)},
-            {"a", static_cast<double>(a)}};
-    }
+mbgl::Value Color::toObject() const {
+    return mapbox::base::ValueObject{
+        {"r", static_cast<double>(r)},
+        {"g", static_cast<double>(g)},
+        {"b", static_cast<double>(b)},
+        {"a", static_cast<double>(a)}};
+}
 
-    mbgl::Value Color::serialize() const {
-        std::array<double, 4> array = toArray();
-        return std::vector<mbgl::Value>{
-            std::string("rgba"),
-            array[0],
-            array[1],
-            array[2],
-            array[3],
-        };
-    }
+mbgl::Value Color::serialize() const {
+    std::array<double, 4> array = toArray();
+    return std::vector<mbgl::Value>{
+        std::string("rgba"),
+        array[0],
+        array[1],
+        array[2],
+        array[3],
+    };
+}
 
 } // namespace mbgl
