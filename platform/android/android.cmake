d28c756edeb3b28ae0840d56a171ab8baf20f042
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2023-05-03 12:02:43 +0200

    Get rid of mapbox-gl-native-curl

--- platform/android/android.cmake
+++ platform/android/android.cmake
@@ -139,24 +139,11 @@ target_link_libraries(
         -Wl,--no-whole-archive
 )
 
-if(ANDROID_NATIVE_API_LEVEL VERSION_LESS 24)
-    target_sources(
-        mbgl-test-runner
-        PRIVATE ${PROJECT_SOURCE_DIR}/platform/android/src/test/http_file_source_test_stub.cpp
-    )
-else()
-    target_sources(
-        mbgl-test-runner
-        PRIVATE ${PROJECT_SOURCE_DIR}/platform/default/src/mbgl/storage/http_file_source.cpp
-    )
 
-    include(${PROJECT_SOURCE_DIR}/vendor/curl.cmake)
-
-    target_link_libraries(
-        mbgl-test-runner
-        PRIVATE mbgl-vendor-curl
-    )
-endif()
+target_sources(
+    mbgl-test-runner
+    PRIVATE ${PROJECT_SOURCE_DIR}/platform/android/MapboxGLAndroidSDK/src/cpp/http_file_source.cpp
+)
 
 add_custom_command(
     TARGET mbgl-test-runner PRE_BUILD
