408a1e39e23431fe09a5a9064d6dcc13abd1926a
Tim Sylvester <tim@wetdogweather.com>
2023-05-05 09:59:37 -0700

    Drawable Renderer (#1570), Move drawable management back to render layers, start on fill layer

--- src/mbgl/renderer/layers/render_fill_layer.hpp
+++ src/mbgl/renderer/layers/render_fill_layer.hpp
@@ -18,6 +18,11 @@ public:
     explicit RenderFillLayer(Immutable<style::FillLayer::Impl>);
     ~RenderFillLayer() override;
 
+    void layerRemoved(UniqueChangeRequestVec&) override;
+
+    /// Generate any changes needed by the layer
+    void update(int32_t layerIndex, gfx::ShaderRegistry&, gfx::Context&, const TransformState&, UniqueChangeRequestVec&) override;
+
 private:
     void transition(const TransitionParameters&) override;
     void evaluate(const PropertyEvaluationParameters&) override;
