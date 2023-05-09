a5b0ca773c8eaa1b717dd949f10e32963e6aac4d
Bart Louwers <bart@emeel.net>
2023-05-09 17:09:17 +0200

    Use dynamic library for ProvisionedApp

--- src/mbgl/util/version.cpp
+++ src/mbgl/util/version.cpp
@@ -3,7 +3,11 @@
 namespace mbgl {
 namespace version {
 
+#ifdef MBGL_VERSION_REV
 const char* revision = MBGL_VERSION_REV;
+#else
+const char* revision = "NOT SET";
+#endif
 
 } // namespace version
 } // namespace mbgl
