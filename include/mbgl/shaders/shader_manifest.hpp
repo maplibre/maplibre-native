ac2741e40500e1de9b5809b93ca2e313a0f926b1
mwilsnd <53413200+mwilsnd@users.noreply.github.com>
2023-05-25 15:59:40 -0500

    Drop author & timestamp from generated shader code (#1178)

--- include/mbgl/shaders/shader_manifest.hpp
+++ include/mbgl/shaders/shader_manifest.hpp
@@ -1,6 +1,4 @@
 // Generated code, do not modify this file!
-// Generated on 2023-05-23T18:10:10.798Z by timsylvester using shaders/generate_shader_code.js
-
 #pragma once
 #include <mbgl/shaders/shader_source.hpp>
 
