785257f542eebe70c14955249aa9cc0cf41dfb03
mwilsnd <mwilsnd@gmail.com>
2023-05-25 11:35:43 -0500

    ASAN: use-after-free, renderLayers

--- src/mbgl/renderer/renderer.cpp
+++ src/mbgl/renderer/renderer.cpp
@@ -39,7 +39,8 @@ void Renderer::render(const std::shared_ptr<UpdateParameters>& updateParameters)
         if (impl->staticData && impl->staticData->shaders) {
             auto& context = impl->backend.getContext();
             auto& shaders = *impl->staticData->shaders;
-            impl->orchestrator.updateLayers(shaders, context, state, updateParameters);
+            impl->orchestrator.updateLayers(
+                shaders, context, state, updateParameters, renderTree->getLayerRenderItems());
         }
 
         impl->render(*renderTree);
