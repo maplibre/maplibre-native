4e5b05f6129b65f626a4b0765b2f17b4d5fc9aaa
mwilsnd <mwilsnd@gmail.com>
2023-05-31 10:05:12 -0500

    Drawable Renderer (#1570), PremultipliedImage is RGBA only

--- src/mbgl/gl/upload_pass.cpp
+++ src/mbgl/gl/upload_pass.cpp
@@ -213,7 +213,7 @@ gfx::AttributeBindingArray UploadPass::buildAttributeBindings(
 std::shared_ptr<gfx::Texture2D> UploadPass::createTexture2D(const PremultipliedImage& image) {
     auto tex = std::make_shared<gl::Texture2D>(commandEncoder.context);
     tex->setSize(image.size)
-        .setFormat(image.channels == 4 ? gfx::TexturePixelType::RGBA : gfx::TexturePixelType::Alpha,
+        .setFormat(gfx::TexturePixelType::RGBA,
                    gfx::TextureChannelDataType::UnsignedByte)
         .create();
     tex->upload(image, *this);
