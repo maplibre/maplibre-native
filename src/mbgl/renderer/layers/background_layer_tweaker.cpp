d2267c10ed5c54d04064538a06f15619751a72e2
Tim Sylvester <tim@wetdogweather.com>
2023-06-01 07:44:41 -0700

    tweaks

--- src/mbgl/renderer/layers/background_layer_tweaker.cpp
+++ src/mbgl/renderer/layers/background_layer_tweaker.cpp
@@ -68,13 +68,17 @@ void BackgroundLayerTweaker::execute(LayerGroup& layerGroup, const RenderTree&, 
     if (!patternShader) {
         patternShader = parameters.context.getGenericShader(parameters.shaders, "BackgroundPatternShader");
     }
+    const auto& curShader = hasPattern ? patternShader : shader;
+    if (!curShader) {
+        return;
+    }
 
     layerGroup.observeDrawables([&](gfx::Drawable& drawable) {
         assert(drawable.getTileID());
         const UnwrappedTileID tileID = drawable.getTileID()->toUnwrapped();
         const auto matrix = parameters.matrixForTile(tileID);
 
-        drawable.setShader(hasPattern ? patternShader : shader);
+        drawable.setShader(curShader);
 
         BackgroundDrawableUBO drawableUBO;
         drawableUBO.matrix = util::cast<float>(matrix);
@@ -83,6 +87,8 @@ void BackgroundLayerTweaker::execute(LayerGroup& layerGroup, const RenderTree&, 
 
         gfx::UniformBufferPtr layerUniformBuffer;
         if (hasPattern) {
+            // TODO: add when raster is merged
+            // curShader->getSamplerLocation("u_image");
             drawable.setTexture(parameters.patternAtlas.texture(), 0);
 
             // from BackgroundPatternProgram::layoutUniformValues
@@ -92,27 +98,33 @@ void BackgroundLayerTweaker::execute(LayerGroup& layerGroup, const RenderTree&, 
                 tileSizeAtNearestZoom * (tileID.canonical.x + tileID.wrap * state.zoomScale(tileID.canonical.z)));
             const int32_t pixelY = tileSizeAtNearestZoom * tileID.canonical.y;
             const Size atlasSize = parameters.patternAtlas.getPixelSize();
+            const auto pixToTile = tileID.pixelsToTileUnits(1.0f, state.getIntegerZoom());
 
-            BackgroundPatternLayerUBO layerUBO;
-            layerUBO.pattern_tl_a = util::cast<float>(imagePosA->tl());
-            layerUBO.pattern_br_a = util::cast<float>(imagePosA->br());
-            layerUBO.pattern_tl_b = util::cast<float>(imagePosB->tl());
-            layerUBO.pattern_br_b = util::cast<float>(imagePosB->br());
-            layerUBO.texsize = {static_cast<float>(atlasSize.width), static_cast<float>(atlasSize.height)};
-            layerUBO.pattern_size_a = imagePosA->displaySize();
-            layerUBO.pattern_size_b = imagePosB->displaySize();
-            layerUBO.pixel_coord_upper = {static_cast<float>(pixelX >> 16), static_cast<float>(pixelY >> 16)};
-            layerUBO.pixel_coord_lower = {static_cast<float>(pixelX & 0xFFFF), static_cast<float>(pixelY & 0xFFFF)};
-            layerUBO.tile_units_to_pixels = 1.0f / tileID.pixelsToTileUnits(1.0f, state.getIntegerZoom());
-            layerUBO.scale_a = crossfade.fromScale;
-            layerUBO.scale_b = crossfade.toScale;
-            layerUBO.mix = crossfade.t;
-            layerUBO.opacity = evaluated.get<BackgroundOpacity>();
+            const BackgroundPatternLayerUBO layerUBO = {
+                /* .pattern_tl_a = */         util::cast<float>(imagePosA->tl()),
+                /* .pattern_br_a = */         util::cast<float>(imagePosA->br()),
+                /* .pattern_tl_b = */         util::cast<float>(imagePosB->tl()),
+                /* .pattern_br_b = */         util::cast<float>(imagePosB->br()),
+                /* .texsize = */              {static_cast<float>(atlasSize.width), static_cast<float>(atlasSize.height)},
+                /* .pattern_size_a = */       imagePosA->displaySize(),
+                /* .pattern_size_b = */       imagePosB->displaySize(),
+                /* .pixel_coord_upper = */    {static_cast<float>(pixelX >> 16), static_cast<float>(pixelY >> 16)},
+                /* .pixel_coord_lower = */    {static_cast<float>(pixelX & 0xFFFF), static_cast<float>(pixelY & 0xFFFF)},
+                /* .tile_units_to_pixels = */ (pixToTile != 0) ? 1.0f / pixToTile : 0.0f,
+                /* .scale_a = */              crossfade.fromScale,
+                /* .scale_b = */              crossfade.toScale,
+                /* .mix = */                  crossfade.t,
+                /* .opacity = */              evaluated.get<BackgroundOpacity>(),
+                /* .pad = */                  { 0 },
+            };
+            // TODO: Update UBOs in-place
             layerUniformBuffer = parameters.context.createUniformBuffer(&layerUBO, sizeof(layerUBO));
         } else {
-            BackgroundLayerUBO layerUBO;
-            layerUBO.color = evaluated.get<BackgroundColor>();
-            layerUBO.opacity = evaluated.get<BackgroundOpacity>();
+            const BackgroundLayerUBO layerUBO = {
+                /* .color = */   evaluated.get<BackgroundColor>(),
+                /* .opacity = */ evaluated.get<BackgroundOpacity>(),
+                /* .pad = */ { 0, 0, 0 },
+            };
             layerUniformBuffer = parameters.context.createUniformBuffer(&layerUBO, sizeof(layerUBO));
         }
         drawable.mutableUniformBuffers().addOrReplace("BackgroundLayerUBO", std::move(layerUniformBuffer));
