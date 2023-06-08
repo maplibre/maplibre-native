89b3c364216c46df69beaafffc097ad69467c6c0
Birk Skyum <birk.skyum@pm.me>
2023-06-08 16:23:50 +0200

    Move LocationIndicator to Kotlin

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/org/maplibre/android/location/LocationIndicatorLayerTest.kt
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/org/maplibre/android/location/LocationIndicatorLayerTest.kt
@@ -1,4 +1,5 @@
 // This file is generated. Edit scripts/generate-style-code.js, then run `make style-code`.
+package com.mapbox.mapboxsdk.location
 
 package org.maplibre.android.location;
 
@@ -34,8 +35,9 @@ import java.util.List;
 /**
  * Basic smoke tests for LocationIndicatorLayer
  */
-@RunWith(AndroidJUnit4ClassRunner.class)
-public class LocationIndicatorLayerTest extends BaseLayerTest {
+@RunWith(AndroidJUnit4ClassRunner::class)
+class LocationIndicatorLayerTest : BaseLayerTest() {
+    private var layer: LocationIndicatorLayer? = null
 
   private LocationIndicatorLayer layer;
   private final List<Point> pointsList = new ArrayList<Point>() {
@@ -52,300 +54,316 @@ public class LocationIndicatorLayerTest extends BaseLayerTest {
     }
   };
 
-  @Before
-  @UiThreadTest
-  public void beforeTest(){
-    super.before();
-    layer = new LocationIndicatorLayer("my-layer");
-    setupLayer(layer);
-  }
+    @Test
+    @UiThreadTest
+    fun testSetVisibility() {
+        Timber.i("Visibility")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testSetVisibility() {
-    Timber.i("Visibility");
-    assertNotNull(layer);
+        // Get initial
+        Assert.assertEquals(layer!!.visibility.getValue(), Property.VISIBLE)
 
-    // Get initial
-    assertEquals(layer.getVisibility().getValue(), VISIBLE);
+        // Set
+        layer!!.setProperties(LocationPropertyFactory.visibility(Property.NONE))
+        Assert.assertEquals(layer!!.visibility.getValue(), Property.NONE)
+    }
 
-    // Set
-    layer.setProperties(visibility(NONE));
-    assertEquals(layer.getVisibility().getValue(), NONE);
-  }
+    @Test
+    @UiThreadTest
+    fun testTopImageAsConstant() {
+        Timber.i("top-image")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.topImage.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testTopImageAsConstant() {
-    Timber.i("top-image");
-    assertNotNull(layer);
-    assertNull(layer.getTopImage().getValue());
+        // Set and Get
+        val propertyValue = "undefined"
+        layer!!.setProperties(LocationPropertyFactory.topImage(propertyValue))
+        Assert.assertEquals(layer!!.topImage.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    String propertyValue = "undefined";
-    layer.setProperties(topImage(propertyValue));
-    assertEquals(layer.getTopImage().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testBearingImageAsConstant() {
+        Timber.i("bearing-image")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.bearingImage.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testBearingImageAsConstant() {
-    Timber.i("bearing-image");
-    assertNotNull(layer);
-    assertNull(layer.getBearingImage().getValue());
+        // Set and Get
+        val propertyValue = "undefined"
+        layer!!.setProperties(LocationPropertyFactory.bearingImage(propertyValue))
+        Assert.assertEquals(layer!!.bearingImage.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    String propertyValue = "undefined";
-    layer.setProperties(bearingImage(propertyValue));
-    assertEquals(layer.getBearingImage().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testShadowImageAsConstant() {
+        Timber.i("shadow-image")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.shadowImage.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testShadowImageAsConstant() {
-    Timber.i("shadow-image");
-    assertNotNull(layer);
-    assertNull(layer.getShadowImage().getValue());
+        // Set and Get
+        val propertyValue = "undefined"
+        layer!!.setProperties(LocationPropertyFactory.shadowImage(propertyValue))
+        Assert.assertEquals(layer!!.shadowImage.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    String propertyValue = "undefined";
-    layer.setProperties(shadowImage(propertyValue));
-    assertEquals(layer.getShadowImage().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testPerspectiveCompensationAsConstant() {
+        Timber.i("perspective-compensation")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.perspectiveCompensation.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testPerspectiveCompensationAsConstant() {
-    Timber.i("perspective-compensation");
-    assertNotNull(layer);
-    assertNull(layer.getPerspectiveCompensation().getValue());
+        // Set and Get
+        val propertyValue = 0.3f
+        layer!!.setProperties(LocationPropertyFactory.perspectiveCompensation(propertyValue))
+        Assert.assertEquals(layer!!.perspectiveCompensation.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Float propertyValue = 0.3f;
-    layer.setProperties(perspectiveCompensation(propertyValue));
-    assertEquals(layer.getPerspectiveCompensation().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testImageTiltDisplacementAsConstant() {
+        Timber.i("image-tilt-displacement")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.imageTiltDisplacement.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testImageTiltDisplacementAsConstant() {
-    Timber.i("image-tilt-displacement");
-    assertNotNull(layer);
-    assertNull(layer.getImageTiltDisplacement().getValue());
+        // Set and Get
+        val propertyValue = 0.3f
+        layer!!.setProperties(LocationPropertyFactory.imageTiltDisplacement(propertyValue))
+        Assert.assertEquals(layer!!.imageTiltDisplacement.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Float propertyValue = 0.3f;
-    layer.setProperties(imageTiltDisplacement(propertyValue));
-    assertEquals(layer.getImageTiltDisplacement().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testBearingAsConstant() {
+        Timber.i("bearing")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.bearing.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testBearingAsConstant() {
-    Timber.i("bearing");
-    assertNotNull(layer);
-    assertNull(layer.getBearing().getValue());
+        // Set and Get
+        val propertyValue = 0.3
+        layer!!.setProperties(LocationPropertyFactory.bearing(propertyValue))
+        Assert.assertEquals(layer!!.bearing.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Double propertyValue = 0.3;
-    layer.setProperties(bearing(propertyValue));
-    assertEquals(layer.getBearing().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testLocationTransition() {
+        Timber.i("locationTransitionOptions")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testLocationTransition() {
-    Timber.i("locationTransitionOptions");
-    assertNotNull(layer);
+        // Set and Get
+        val options = TransitionOptions(300, 100)
+        layer!!.locationTransition = options
+        Assert.assertEquals(layer!!.locationTransition, options)
+    }
 
-    // Set and Get
-    TransitionOptions options = new TransitionOptions(300, 100);
-    layer.setLocationTransition(options);
-    assertEquals(layer.getLocationTransition(), options);
-  }
+    @Test
+    @UiThreadTest
+    fun testLocationAsConstant() {
+        Timber.i("location")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.location.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testLocationAsConstant() {
-    Timber.i("location");
-    assertNotNull(layer);
-    assertNull(layer.getLocation().getValue());
+        // Set and Get
+        val propertyValue = arrayOf(0.0, 0.0, 0.0)
+        layer!!.setProperties(LocationPropertyFactory.location(propertyValue))
+        Assert.assertEquals(layer!!.location.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Double[] propertyValue = new Double[] {0.0, 0.0, 0.0};
-    layer.setProperties(location(propertyValue));
-    assertEquals(layer.getLocation().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusTransition() {
+        Timber.i("accuracy-radiusTransitionOptions")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusTransition() {
-    Timber.i("accuracy-radiusTransitionOptions");
-    assertNotNull(layer);
+        // Set and Get
+        val options = TransitionOptions(300, 100)
+        layer!!.accuracyRadiusTransition = options
+        Assert.assertEquals(layer!!.accuracyRadiusTransition, options)
+    }
 
-    // Set and Get
-    TransitionOptions options = new TransitionOptions(300, 100);
-    layer.setAccuracyRadiusTransition(options);
-    assertEquals(layer.getAccuracyRadiusTransition(), options);
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusAsConstant() {
+        Timber.i("accuracy-radius")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.accuracyRadius.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusAsConstant() {
-    Timber.i("accuracy-radius");
-    assertNotNull(layer);
-    assertNull(layer.getAccuracyRadius().getValue());
+        // Set and Get
+        val propertyValue = 0.3f
+        layer!!.setProperties(LocationPropertyFactory.accuracyRadius(propertyValue))
+        Assert.assertEquals(layer!!.accuracyRadius.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Float propertyValue = 0.3f;
-    layer.setProperties(accuracyRadius(propertyValue));
-    assertEquals(layer.getAccuracyRadius().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testTopImageSizeTransition() {
+        Timber.i("top-image-sizeTransitionOptions")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testTopImageSizeTransition() {
-    Timber.i("top-image-sizeTransitionOptions");
-    assertNotNull(layer);
+        // Set and Get
+        val options = TransitionOptions(300, 100)
+        layer!!.topImageSizeTransition = options
+        Assert.assertEquals(layer!!.topImageSizeTransition, options)
+    }
 
-    // Set and Get
-    TransitionOptions options = new TransitionOptions(300, 100);
-    layer.setTopImageSizeTransition(options);
-    assertEquals(layer.getTopImageSizeTransition(), options);
-  }
+    @Test
+    @UiThreadTest
+    fun testTopImageSizeAsConstant() {
+        Timber.i("top-image-size")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.topImageSize.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testTopImageSizeAsConstant() {
-    Timber.i("top-image-size");
-    assertNotNull(layer);
-    assertNull(layer.getTopImageSize().getValue());
+        // Set and Get
+        val propertyValue = 0.3f
+        layer!!.setProperties(LocationPropertyFactory.topImageSize(propertyValue))
+        Assert.assertEquals(layer!!.topImageSize.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Float propertyValue = 0.3f;
-    layer.setProperties(topImageSize(propertyValue));
-    assertEquals(layer.getTopImageSize().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testBearingImageSizeTransition() {
+        Timber.i("bearing-image-sizeTransitionOptions")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testBearingImageSizeTransition() {
-    Timber.i("bearing-image-sizeTransitionOptions");
-    assertNotNull(layer);
+        // Set and Get
+        val options = TransitionOptions(300, 100)
+        layer!!.bearingImageSizeTransition = options
+        Assert.assertEquals(layer!!.bearingImageSizeTransition, options)
+    }
 
-    // Set and Get
-    TransitionOptions options = new TransitionOptions(300, 100);
-    layer.setBearingImageSizeTransition(options);
-    assertEquals(layer.getBearingImageSizeTransition(), options);
-  }
+    @Test
+    @UiThreadTest
+    fun testBearingImageSizeAsConstant() {
+        Timber.i("bearing-image-size")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.bearingImageSize.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testBearingImageSizeAsConstant() {
-    Timber.i("bearing-image-size");
-    assertNotNull(layer);
-    assertNull(layer.getBearingImageSize().getValue());
+        // Set and Get
+        val propertyValue = 0.3f
+        layer!!.setProperties(LocationPropertyFactory.bearingImageSize(propertyValue))
+        Assert.assertEquals(layer!!.bearingImageSize.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Float propertyValue = 0.3f;
-    layer.setProperties(bearingImageSize(propertyValue));
-    assertEquals(layer.getBearingImageSize().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testShadowImageSizeTransition() {
+        Timber.i("shadow-image-sizeTransitionOptions")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testShadowImageSizeTransition() {
-    Timber.i("shadow-image-sizeTransitionOptions");
-    assertNotNull(layer);
+        // Set and Get
+        val options = TransitionOptions(300, 100)
+        layer!!.shadowImageSizeTransition = options
+        Assert.assertEquals(layer!!.shadowImageSizeTransition, options)
+    }
 
-    // Set and Get
-    TransitionOptions options = new TransitionOptions(300, 100);
-    layer.setShadowImageSizeTransition(options);
-    assertEquals(layer.getShadowImageSizeTransition(), options);
-  }
+    @Test
+    @UiThreadTest
+    fun testShadowImageSizeAsConstant() {
+        Timber.i("shadow-image-size")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.shadowImageSize.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testShadowImageSizeAsConstant() {
-    Timber.i("shadow-image-size");
-    assertNotNull(layer);
-    assertNull(layer.getShadowImageSize().getValue());
+        // Set and Get
+        val propertyValue = 0.3f
+        layer!!.setProperties(LocationPropertyFactory.shadowImageSize(propertyValue))
+        Assert.assertEquals(layer!!.shadowImageSize.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    Float propertyValue = 0.3f;
-    layer.setProperties(shadowImageSize(propertyValue));
-    assertEquals(layer.getShadowImageSize().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusColorTransition() {
+        Timber.i("accuracy-radius-colorTransitionOptions")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusColorTransition() {
-    Timber.i("accuracy-radius-colorTransitionOptions");
-    assertNotNull(layer);
+        // Set and Get
+        val options = TransitionOptions(300, 100)
+        layer!!.accuracyRadiusColorTransition = options
+        Assert.assertEquals(layer!!.accuracyRadiusColorTransition, options)
+    }
 
-    // Set and Get
-    TransitionOptions options = new TransitionOptions(300, 100);
-    layer.setAccuracyRadiusColorTransition(options);
-    assertEquals(layer.getAccuracyRadiusColorTransition(), options);
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusColorAsConstant() {
+        Timber.i("accuracy-radius-color")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.accuracyRadiusColor.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusColorAsConstant() {
-    Timber.i("accuracy-radius-color");
-    assertNotNull(layer);
-    assertNull(layer.getAccuracyRadiusColor().getValue());
+        // Set and Get
+        val propertyValue = "rgba(255,128,0,0.7)"
+        layer!!.setProperties(LocationPropertyFactory.accuracyRadiusColor(propertyValue))
+        Assert.assertEquals(layer!!.accuracyRadiusColor.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    String propertyValue = "rgba(255,128,0,0.7)";
-    layer.setProperties(accuracyRadiusColor(propertyValue));
-    assertEquals(layer.getAccuracyRadiusColor().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusColorAsIntConstant() {
+        Timber.i("accuracy-radius-color")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusColorAsIntConstant() {
-    Timber.i("accuracy-radius-color");
-    assertNotNull(layer);
+        // Set and Get
+        layer!!.setProperties(
+            LocationPropertyFactory.accuracyRadiusColor(
+                Color.argb(
+                    127,
+                    255,
+                    127,
+                    0
+                )
+            )
+        )
+        Assert.assertEquals(
+            layer!!.accuracyRadiusColorAsInt.toLong(),
+            Color.argb(127, 255, 127, 0).toLong()
+        )
+    }
 
-    // Set and Get
-    layer.setProperties(accuracyRadiusColor(Color.argb(127, 255, 127, 0)));
-    assertEquals(layer.getAccuracyRadiusColorAsInt(), Color.argb(127, 255, 127, 0));
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusBorderColorTransition() {
+        Timber.i("accuracy-radius-border-colorTransitionOptions")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusBorderColorTransition() {
-    Timber.i("accuracy-radius-border-colorTransitionOptions");
-    assertNotNull(layer);
+        // Set and Get
+        val options = TransitionOptions(300, 100)
+        layer!!.accuracyRadiusBorderColorTransition = options
+        Assert.assertEquals(layer!!.accuracyRadiusBorderColorTransition, options)
+    }
 
-    // Set and Get
-    TransitionOptions options = new TransitionOptions(300, 100);
-    layer.setAccuracyRadiusBorderColorTransition(options);
-    assertEquals(layer.getAccuracyRadiusBorderColorTransition(), options);
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusBorderColorAsConstant() {
+        Timber.i("accuracy-radius-border-color")
+        Assert.assertNotNull(layer)
+        Assert.assertNull(layer!!.accuracyRadiusBorderColor.getValue())
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusBorderColorAsConstant() {
-    Timber.i("accuracy-radius-border-color");
-    assertNotNull(layer);
-    assertNull(layer.getAccuracyRadiusBorderColor().getValue());
+        // Set and Get
+        val propertyValue = "rgba(255,128,0,0.7)"
+        layer!!.setProperties(LocationPropertyFactory.accuracyRadiusBorderColor(propertyValue))
+        Assert.assertEquals(layer!!.accuracyRadiusBorderColor.getValue(), propertyValue)
+    }
 
-    // Set and Get
-    String propertyValue = "rgba(255,128,0,0.7)";
-    layer.setProperties(accuracyRadiusBorderColor(propertyValue));
-    assertEquals(layer.getAccuracyRadiusBorderColor().getValue(), propertyValue);
-  }
+    @Test
+    @UiThreadTest
+    fun testAccuracyRadiusBorderColorAsIntConstant() {
+        Timber.i("accuracy-radius-border-color")
+        Assert.assertNotNull(layer)
 
-  @Test
-  @UiThreadTest
-  public void testAccuracyRadiusBorderColorAsIntConstant() {
-    Timber.i("accuracy-radius-border-color");
-    assertNotNull(layer);
-
-    // Set and Get
-    layer.setProperties(accuracyRadiusBorderColor(Color.argb(127, 255, 127, 0)));
-    assertEquals(layer.getAccuracyRadiusBorderColorAsInt(), Color.argb(127, 255, 127, 0));
-  }
+        // Set and Get
+        layer!!.setProperties(
+            LocationPropertyFactory.accuracyRadiusBorderColor(
+                Color.argb(
+                    127,
+                    255,
+                    127,
+                    0
+                )
+            )
+        )
+        Assert.assertEquals(
+            layer!!.accuracyRadiusBorderColorAsInt.toLong(),
+            Color.argb(127, 255, 127, 0).toLong()
+        )
+    }
 }
