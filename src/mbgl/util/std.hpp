3666c6577fad66b5d93ff80d129e7e50846f8c85
Tim Sylvester <tim@wetdogweather.com>
2023-06-09 07:23:17 -0700

    Topic/fill pass (#1225)

--- src/mbgl/util/std.hpp
+++ src/mbgl/util/std.hpp
@@ -8,7 +8,7 @@ namespace mbgl {
 namespace util {
 
 template <typename Container, typename ForwardIterator, typename Predicate>
-void erase_if(Container &container, ForwardIterator it, Predicate pred) {
+void erase_if(Container& container, ForwardIterator it, Predicate pred) {
     while (it != container.end()) {
         if (pred(*it)) {
             it = container.erase(it);
@@ -19,8 +19,16 @@ void erase_if(Container &container, ForwardIterator it, Predicate pred) {
 }
 
 template <typename Container, typename Predicate>
-void erase_if(Container &container, Predicate pred) {
+void erase_if(Container& container, Predicate pred) {
     erase_if(container, container.begin(), pred);
+}
+
+template <typename T, std::size_t N, std::size_t M>
+auto concat(const std::array<T, N>& a1, const std::array<T, M>& a2) {
+    std::array<T, N + M> result;
+    std::copy(a1.cbegin(), a1.cend(), result.begin());
+    std::copy(a2.cbegin(), a2.cend(), result.begin() + N);
+    return result;
 }
 
 } // namespace util
