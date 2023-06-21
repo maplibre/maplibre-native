71c3b28f7c2e893ff4d94f31466d7f814abf94b2
Tim Sylvester <tim@wetdogweather.com>
2023-06-21 07:13:34 -0700

    Eliminate special color attribute handling (#1266)

--- include/mbgl/shaders/gl/shader_group_gl.hpp
+++ include/mbgl/shaders/gl/shader_group_gl.hpp
@@ -16,11 +16,12 @@ public:
           programParameters(programParameters_){};
     ~ShaderGroupGL() noexcept override = default;
 
-    const std::shared_ptr<gfx::Shader> getOrCreateShader(
-        gfx::Context& context, const std::vector<std::string>& propertiesAsUniforms) override {
-        const auto& name = shaders::ShaderSource<ShaderID, gfx::Backend::Type::OpenGL>::name;
-        const auto& vert = shaders::ShaderSource<ShaderID, gfx::Backend::Type::OpenGL>::vertex;
-        const auto& frag = shaders::ShaderSource<ShaderID, gfx::Backend::Type::OpenGL>::fragment;
+    gfx::ShaderPtr getOrCreateShader(gfx::Context& context,
+                                     const std::vector<std::string>& propertiesAsUniforms,
+                                     std::string_view firstAttribName) override {
+        constexpr auto& name = shaders::ShaderSource<ShaderID, gfx::Backend::Type::OpenGL>::name;
+        constexpr auto& vert = shaders::ShaderSource<ShaderID, gfx::Backend::Type::OpenGL>::vertex;
+        constexpr auto& frag = shaders::ShaderSource<ShaderID, gfx::Backend::Type::OpenGL>::fragment;
 
         uint32_t key = 0;
         std::string additionalDefines;
@@ -33,12 +34,13 @@ public:
             additionalDefines += propertiesAsUniforms[i];
             additionalDefines += "\n";
         }
-        std::string shaderName = std::string(name) + "#" + std::to_string(key);
+        const std::string shaderName = std::string(name) + "#" + std::to_string(key);
         auto shader = get<gl::ShaderProgramGL>(shaderName);
         if (!shader) {
+            auto& glContext = static_cast<gl::Context&>(context);
             shader = ShaderProgramGL::create(
-                static_cast<gl::Context&>(context), programParameters, shaderName, vert, frag, additionalDefines);
-            if (!registerShader(shader, shaderName)) {
+                glContext, programParameters, shaderName, firstAttribName, vert, frag, additionalDefines);
+            if (!shader || !registerShader(shader, shaderName)) {
                 throw std::runtime_error("Failed to register " + shaderName + " with shader group!");
             }
         }
