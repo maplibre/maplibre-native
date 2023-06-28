9374765cb873306606cbd2679958e8d1e2fe9a0f
Bart Louwers <bart@emeel.net>
2023-06-28 16:41:21 +0200

    Add target for iOS render test to Bazel (#1277)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- WORKSPACE
+++ WORKSPACE
@@ -48,3 +48,12 @@ load(
 )
 
 xcodeproj_rules_dependencies()
+
+load(
+    "@build_bazel_rules_apple//apple:apple.bzl",
+    "provisioning_profile_repository",
+)
+
+provisioning_profile_repository(
+    name = "local_provisioning_profiles",
+)
--- render-test/runner.cpp
+++ render-test/runner.cpp
@@ -814,7 +814,14 @@ void TestRunner::run(TestMetadata& metadata) {
                     auto it = std::find_if(symbols.begin(), symbols.end(), [isIcon, box](const auto& a) {
                         return a.isIcon == isIcon && a.location.min == box.min && a.location.max == box.max;
                     });
+
+// TODO: remove usage of std::codecvt_utf8
+// https://github.com/maplibre/maplibre-native/issues/1269
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
                     static std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> cv;
+#pragma clang diagnostic pop
+
                     if (it == symbols.end()) {
                         symbols.push_back({tileId, box, placed, isIcon});
                         return;
