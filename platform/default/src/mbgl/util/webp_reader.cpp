b77b31ed7d2eab58b316cd50b9374f28941a1d1e
mwilsnd <mwilsnd@gmail.com>
2023-07-07 22:04:48 -0500

    Reduce copy, premultiply image data

--- platform/default/src/mbgl/util/webp_reader.cpp
+++ platform/default/src/mbgl/util/webp_reader.cpp
@@ -1,4 +1,7 @@
 #include <mbgl/util/image.hpp>
+#include <mbgl/util/logging.hpp>
+#include <mbgl/util/premultiply.hpp>
+
 #include <webp/types.h>
 #include <webp/decode.h>
 
@@ -6,15 +9,18 @@ namespace mbgl {
 
 PremultipliedImage decodeWEBP(const uint8_t* data, size_t size) {
     int32_t width, height;
-    auto bytes = WebPDecodeRGBA(data, size, &width, &height);
-    if (!bytes) {
+    if (!WebPGetInfo(data, size, &width, &height)) {
+        Log::Warning(Event::Image, "Failed to decode WebP image header!");
         return {};
     }
 
-    const auto rawSize = 4 * width * height; // RGBA = 4
-    auto img = PremultipliedImage({static_cast<uint32_t>(width), static_cast<uint32_t>(height)}, bytes, rawSize);
-    WebPFree(bytes);
-    return img;
+    auto img = UnassociatedImage({static_cast<uint32_t>(width), static_cast<uint32_t>(height)});
+    if (!WebPDecodeRGBAInto(data, size, img.data.get(), img.bytes(), static_cast<int32_t>(img.stride()))) {
+        Log::Warning(Event::Image, "Failed to decode WebP image contents!");
+        return {};
+    }
+
+    return util::premultiply(std::move(img));
 }
 
 } // namespace mbgl
