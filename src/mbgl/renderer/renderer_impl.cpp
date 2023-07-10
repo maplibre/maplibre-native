0aed086d3bd5e80ab8dc96744ec2059f4e8aad8b
mwilsnd <mwilsnd@gmail.com>
2023-07-10 15:24:29 -0500

    Render tree maybe unsued

--- src/mbgl/renderer/renderer_impl.cpp
+++ src/mbgl/renderer/renderer_impl.cpp
@@ -62,7 +62,7 @@ void Renderer::Impl::setObserver(RendererObserver* observer_) {
     observer = observer_ ? observer_ : &nullObserver();
 }
 
-void Renderer::Impl::preRender(const RenderTree& renderTree,
+void Renderer::Impl::preRender([[maybe_unused]] const RenderTree& renderTree,
                                [[maybe_unused]] const std::shared_ptr<UpdateParameters>& updateParameters) {
     [[maybe_unused]] auto& context = backend.getContext();
     // Blocks execution until the renderable is available.
