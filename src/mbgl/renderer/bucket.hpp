964ce7879c6c0fdc0cc3ecac4a231e1122acd710
mwilsnd <mwilsnd@gmail.com>
2023-07-13 15:32:47 -0500

    Drawable Renderer (#1570), Add guards around bucket IDs

--- src/mbgl/renderer/bucket.hpp
+++ src/mbgl/renderer/bucket.hpp
@@ -4,7 +4,10 @@
 #include <mbgl/style/image_impl.hpp>
 #include <mbgl/renderer/image_atlas.hpp>
 #include <mbgl/style/layer_impl.hpp>
+
+#if MLN_DRAWABLE_RENDERER
 #include <mbgl/util/identity.hpp>
+#endif
 
 #include <atomic>
 
@@ -67,13 +70,17 @@ public:
     virtual void updateVertices(
         const Placement&, bool /*updateOpacities*/, const TransformState&, const RenderTile&, std::set<uint32_t>&) {}
 
+#if MLN_DRAWABLE_RENDERER
     const util::SimpleIdentity& getID() const { return bucketID; }
+#endif
 
 protected:
     Bucket() = default;
     std::atomic<bool> uploaded{false};
 
+#if MLN_DRAWABLE_RENDERER
     util::SimpleIdentity bucketID;
+#endif
 };
 
 } // namespace mbgl
