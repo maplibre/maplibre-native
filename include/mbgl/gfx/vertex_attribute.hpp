783a1cb3fc8dabaccbcd10887ec8b67b93f999a8
Stefan Karschti <stefan.karschti@gmail.com>
2023-07-24 23:03:40 +0300

    Revert "comments. refactor observe* to visit*"
    This reverts commit 861da79315e4c4924ec475382ed4c95c4bea9482.

--- include/mbgl/gfx/vertex_attribute.hpp
+++ include/mbgl/gfx/vertex_attribute.hpp
@@ -15,13 +15,8 @@
 
 namespace mbgl {
 
-/// A 2 by 2 matrix
 using mat2 = std::array<double, 2 * 2>;
-
-/// A 3 by 3 matrix
 using mat3 = std::array<double, 3 * 3>;
-
-/// A 4 by 4 matrix
 using mat4 = std::array<double, 4 * 4>;
 
 namespace gfx {
@@ -31,44 +26,20 @@ class VertexVectorBase;
 
 class VertexAttribute {
 public:
-    /// type for vector of 2 floats
     using float2 = std::array<float, 2>;
-
-    /// type for vector of 3 floats
     using float3 = std::array<float, 3>;
-
-    /// type for vector of 3 floats
     using float4 = std::array<float, 4>;
-
-    /// type for 2 by 2 matrix of floats
     using matf2 = std::array<float, 2 * 2>;
-
-    /// type for 3 by 3 matrix of floats
     using matf3 = std::array<float, 3 * 3>;
-
-    /// type for 4 by 4 matrix of floats
     using matf4 = std::array<float, 4 * 4>;
-
-    /// type for vector of 2 integers
     using int2 = std::array<std::int32_t, 2>;
-
-    /// type for vector of 3 integers
     using int3 = std::array<std::int32_t, 3>;
-
-    /// type for vector of 4 integers
     using int4 = std::array<std::int32_t, 4>;
-
-    /// type for vector of 8 unsigned short integers
     using ushort8 = std::array<std::uint16_t, 8>;
 
     using ElementType =
         std::variant<std::int32_t, ushort8, int2, int3, int4, float, float2, float3, float4, matf3, matf4>;
 
-    /// @brief  Constructor for vertex attribute
-    /// @param index_ the index of the vertex attribute
-    /// @param dataType_ the vertex attribute data type
-    /// @param count_ element count
-    /// @param stride_ the vertex attribute stride
     VertexAttribute(int index_, AttributeDataType dataType_, std::size_t count_, std::size_t stride_)
         : index(index_),
           stride((int)stride_),
@@ -96,35 +67,18 @@ public:
 public:
     virtual ~VertexAttribute() = default;
 
-    /// @brief Get the index of the vertex attribute
     int getIndex() const { return index; }
-
-    /// @brief Set the index of the vertex attribute
     void setIndex(int value) { index = value; }
 
-    /// @brief Get the stride of the vertex attribute
     std::size_t getStride() const { return stride; }
 
-    /// @brief Get the count of vertex attribute items
     std::size_t getCount() const;
-
-    /// @brief Get the data type of the vertex attribute
     AttributeDataType getDataType() const { return dataType; }
 
-    /// @brief Get a vertex attribute item
-    /// @param index index of the item
-    /// @return ElementType reference to the item
     const ElementType& get(std::size_t i) const { return items[i]; }
 
-    /// @brief Reserves space for a number of items
-    /// @param count Number of items to reserve memory for
     void reserve(std::size_t count) { items.reserve(count); }
 
-    /// @brief Set the value of an item
-    /// @tparam T item data type
-    /// @param i index of the item
-    /// @param value value to set
-    /// @return ElementType reference to item
     template <typename T>
     const ElementType& set(std::size_t i, T value) {
         if (items.size() < i + 1) {
@@ -144,11 +98,6 @@ public:
         }
         return items[i] = value;
     }
-
-    /// @brief Set the value of an item
-    /// @param i index of the item
-    /// @param value value to set
-    /// @return ElementType reference to item
     const ElementType& setVariant(std::size_t i, const ElementType& value) {
         if (items.size() < i + 1) {
             items.resize(std::max(items.size(), i + 1));
@@ -157,7 +106,6 @@ public:
         return items[i] = value;
     }
 
-    /// @brief Clear the items
     void clear() {
         if (!items.empty()) {
             setDirty();
@@ -165,16 +113,12 @@ public:
         items.clear();
     }
 
-    /// @brief Get dirty state
     bool isDirty() const { return dirty; }
-
-    /// @brief Set dirty state
     void setDirty(bool value = true) { dirty = value; }
 
     template <std::size_t I = 0, typename... Tp>
     inline typename std::enable_if<I == sizeof...(Tp), void>::type set(std::size_t, std::tuple<Tp...>, std::size_t) {}
 
-    /// Set item value
     template <std::size_t I = 0, typename... Tp>
         inline typename std::enable_if <
         I<sizeof...(Tp), void>::type set(std::size_t i, std::tuple<Tp...> tuple, std::size_t tupleIndex) {
@@ -185,31 +129,14 @@ public:
         }
     }
 
-    /// Get the vertex attribute's raw data
     std::vector<std::uint8_t>& getRawData() { return rawData; }
     const std::vector<std::uint8_t>& getRawData() const { return rawData; }
 
-    /// Get the vertex attribute's shared raw data
     const std::shared_ptr<VertexVectorBase>& getSharedRawData() const { return sharedRawData; }
-
-    /// Get the vertex attribute's shared data type
     AttributeDataType getSharedType() const { return sharedType; }
-
-    /// Get the vertex attribute's shared data offset
     uint32_t getSharedOffset() const { return sharedOffset; }
-
-    /// Get the vertex attribute's shared vertex offset
     uint32_t getSharedVertexOffset() const { return sharedVertexOffset; }
-
-    /// Get the vertex attribute's shared data stride
     uint32_t getSharedStride() const { return sharedStride; }
-
-    /// @brief Set the vertex attribute's shared data
-    /// @param data shared pointer to vertex vector
-    /// @param offset Offset into the source vertex layout structure
-    /// @param vertexOffset Offset into the source vertex array
-    /// @param stride Stride of the source vertex structure
-    /// @param type Type of the attribute
     void setSharedRawData(std::shared_ptr<VertexVectorBase> data,
                           uint32_t offset,
                           uint32_t vertexOffset,
@@ -221,8 +148,6 @@ public:
         sharedVertexOffset = vertexOffset;
         sharedStride = stride_;
     }
-
-    /// @brief Clears the shared data
     void resetSharedRawData() { sharedRawData.reset(); }
 
     /// Convert from the odd partially-normalized color component array produced by `Color::toArray` into normalized
@@ -322,20 +247,17 @@ public:
             attrs.begin(), attrs.end(), [](const auto& kv) { return kv.second && kv.second->isDirty(); });
     }
 
-    /// Clear the collection
     void clear();
 
     /// Do something with each attribute
-    void visitAttributes(const std::function<void(const std::string&, VertexAttribute&)>& f) {
+    void observeAttributes(const std::function<void(const std::string&, VertexAttribute&)>& f) {
         std::for_each(attrs.begin(), attrs.end(), [&](const auto& kv) {
             if (kv.second) {
                 f(kv.first, *kv.second);
             }
         });
     }
-
-    /// Do something with each attribute
-    void visitAttributes(const std::function<void(const std::string&, const VertexAttribute&)>& f) const {
+    void observeAttributes(const std::function<void(const std::string&, const VertexAttribute&)>& f) const {
         std::for_each(attrs.begin(), attrs.end(), [&](const auto& kv) {
             if (kv.second) {
                 f(kv.first, *kv.second);
@@ -351,7 +273,6 @@ public:
     VertexAttributeArray& operator=(VertexAttributeArray&&);
     VertexAttributeArray& operator=(const VertexAttributeArray&);
 
-    /// Clone the collection
     virtual std::unique_ptr<VertexAttributeArray> clone() const {
         auto newAttrs = std::make_unique<VertexAttributeArray>();
         newAttrs->copy(*this);
