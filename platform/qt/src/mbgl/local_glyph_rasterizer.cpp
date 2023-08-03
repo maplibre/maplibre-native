44050162562115b5aa725b5e07859cdf7ac33963
alanchen <alanchenboy@gmail.com>
2023-08-03 15:32:28 +0800

    fix qt build

--- platform/qt/src/mbgl/local_glyph_rasterizer.cpp
+++ platform/qt/src/mbgl/local_glyph_rasterizer.cpp
@@ -40,7 +40,7 @@ LocalGlyphRasterizer::LocalGlyphRasterizer(const std::optional<std::string>& fon
 LocalGlyphRasterizer::~LocalGlyphRasterizer() {}
 
 bool LocalGlyphRasterizer::canRasterizeGlyph(const FontStack&, GlyphID glyphID) {
-    return impl->isConfigured() && impl->metrics->inFont(glyphID) &&
+    return impl->isConfigured() && impl->metrics->inFont(glyphID.complex.code) &&
            util::i18n::allowsFixedWidthGlyphGeneration(glyphID);
 }
 
@@ -54,9 +54,9 @@ Glyph LocalGlyphRasterizer::rasterizeGlyph(const FontStack&, GlyphID glyphID) {
     }
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 11, 0)
-    glyph.metrics.width = impl->metrics->horizontalAdvance(glyphID);
+    glyph.metrics.width = impl->metrics->horizontalAdvance(glyphID.complex.code);
 #else
-    glyph.metrics.width = impl->metrics->width(glyphID);
+    glyph.metrics.width = impl->metrics->width(glyphID.complex.code);
 #endif
     glyph.metrics.height = impl->metrics->height();
     glyph.metrics.left = 3;
@@ -71,7 +71,7 @@ Glyph LocalGlyphRasterizer::rasterizeGlyph(const FontStack&, GlyphID glyphID) {
     painter.setFont(impl->font);
     painter.setRenderHints(QPainter::TextAntialiasing);
     // Render at constant baseline, to align with glyphs that are rendered by node-fontnik.
-    painter.drawText(QPointF(0, 20), QString(QChar(glyphID)));
+    painter.drawText(QPointF(0, 20), QString(QChar(glyphID.complex.code)));
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
     auto img = std::make_unique<uint8_t[]>(image.sizeInBytes());
