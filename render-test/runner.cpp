4990b0075d45092191de2604e8453d742259fb41
alanchen <alanchenboy@gmail.com>
2023-08-18 18:03:29 +0800

    fix cmake build errors

--- render-test/runner.cpp
+++ render-test/runner.cpp
@@ -817,10 +817,8 @@ void TestRunner::run(TestMetadata& metadata) {
 
 // TODO: remove usage of std::codecvt_utf8
 // https://github.com/maplibre/maplibre-native/issues/1269
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+// NOLINTNEXTLINE
                     static std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> cv;
-#pragma clang diagnostic pop
 
                     if (it == symbols.end()) {
                         symbols.push_back({tileId, box, placed, isIcon});
