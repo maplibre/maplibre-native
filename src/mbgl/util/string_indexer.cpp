b33cf91009d61d6c08690f561c81b3dba2933f2d
Stefan Karschti <stefan.karschti@gmail.com>
2023-09-15 09:19:26 +0300

    string indexer reader lock in get()

--- src/mbgl/util/string_indexer.cpp
+++ src/mbgl/util/string_indexer.cpp
@@ -9,18 +9,34 @@ const std::string empty;
 }
 
 StringIdentity StringIndexer::get(const std::string& string) {
-    std::unique_lock<std::shared_mutex> writerLock(instance().sharedMutex);
+    {
+        std::shared_lock<std::shared_mutex> readerLock(instance().sharedMutex);
+        
+        auto& stringToIdentity = instance().stringToIdentity;
+        auto& identityToString = instance().identityToString;
+        assert(stringToIdentity.size() == identityToString.size());
+        
+        if (auto it = stringToIdentity.find(string); it != stringToIdentity.end()) {
+            return it->second;
+        }
+    }
+    
+    {
+        std::unique_lock<std::shared_mutex> writerLock(instance().sharedMutex);
 
-    auto& stringToIdentity = instance().stringToIdentity;
-    auto& identityToString = instance().identityToString;
-    assert(stringToIdentity.size() == identityToString.size());
-
-    if (auto it = stringToIdentity.find(string); it != stringToIdentity.end()) {
-        return it->second;
-    } else {
+        auto& stringToIdentity = instance().stringToIdentity;
+        auto& identityToString = instance().identityToString;
+        assert(stringToIdentity.size() == identityToString.size());
+        
         StringIdentity id = identityToString.size();
-        identityToString.push_back(string);
-        stringToIdentity[string] = id;
+        auto result = stringToIdentity.insert({string, id});
+        if(result.second) {
+            // this writer made the insert
+            identityToString.push_back(string);
+        } else {
+            // another writer inserted into the map
+            id = result.first->second;
+        }
         return id;
     }
 }
