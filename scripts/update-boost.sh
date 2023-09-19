2693c05037f998a57a8921f8c8a6789be6f89758
Bart Louwers <bart@emeel.net>
2023-09-19 15:02:08 +0200

    Add missing Boost headers

--- scripts/update-boost.sh
+++ scripts/update-boost.sh
@@ -5,7 +5,7 @@ set -eu
 # Extract boost modules that we need
 rm -rf vendor/boost/include
 mkdir -p vendor/boost/include
-bcp --boost="$BOOST" --scan $(find {src,include,test,platform,bin} -name "*.cpp" -o -name "*.hpp") vendor/boost/include
+bcp --boost="$BOOST" --scan $(find {src,include,test,platform,bin,render-test} -name "*.cpp" -o -name "*.hpp") vendor/boost/include
 
 pushd vendor/boost
 VERSION=$(sed -n 's/^#define BOOST_LIB_VERSION "\([^"]*\)"$/\1/p' include/boost/version.hpp)
