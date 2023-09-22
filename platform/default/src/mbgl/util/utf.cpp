db5f78ffdfd8bffb37d75700b4761db6c80b20a0
Bart Louwers <bart@emeel.net>
2023-09-22 19:58:56 +0200

    Replace Boost Locale with ICU

--- platform/default/src/mbgl/util/utf.cpp
+++ platform/default/src/mbgl/util/utf.cpp
@@ -1,16 +1,20 @@
 #include <mbgl/util/utf.hpp>
 
-#include <boost/locale/encoding_utf.hpp>
+#include <unicode/unistr.h>
 
 namespace mbgl {
 namespace util {
 
 std::u16string convertUTF8ToUTF16(const std::string& str) {
-    return boost::locale::conv::utf_to_utf<char16_t>(str);
+    auto ustr = icu::UnicodeString::fromUTF8(icu::StringPiece(str));
+    return std::u16string(ustr.getBuffer(), static_cast<size_t>(ustr.length()));
 }
 
 std::string convertUTF16ToUTF8(const std::u16string& str) {
-    return boost::locale::conv::utf_to_utf<char>(str);
+    std::string result;
+    icu::UnicodeString unicode_string(true /* isTerminated */, str.c_str(), static_cast<int32_t>(str.length()));
+    unicode_string.toUTF8String(result);
+    return result;
 }
 
 } // namespace util
