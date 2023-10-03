23d521eb11b06f394e90071603920eb534de622e
Bart Louwers <bart.louwers@gmail.com>
2023-10-03 11:15:57 +0000

    fix overshadowed member

--- src/mbgl/gl/drawable_gl.cpp
+++ src/mbgl/gl/drawable_gl.cpp
@@ -122,10 +122,10 @@ void DrawableGL::bindUniformBuffers() const {
             const auto& uniformBuffer = getUniformBuffers().get(element.first);
             if (!uniformBuffer) {
                 using namespace std::string_literals;
-                const auto tileID = getTileID() ? util::toString(*getTileID()) : "<no tile>";
+                const auto currTileID = getTileID() ? util::toString(*getTileID()) : "<no tile>";
                 Log::Error(Event::General,
                            "bindUniformBuffers: UBO "s + std::string(StringIndexer::get(element.first)) +
-                               " not found for " + util::toString(getID()) + " / " + getName() + " / " + tileID +
+                               " not found for " + util::toString(getID()) + " / " + getName() + " / " + currTileID +
                                ". skipping.");
                 assert(false);
                 continue;
