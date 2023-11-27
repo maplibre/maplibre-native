dd3f200a83d96d71795f0261ba3dd7fc5dae0177
mwilsnd <mwilsnd@gmail.com>
2023-11-27 10:20:21 -0500

    Merge fix

--- src/mbgl/renderer/layers/render_fill_layer.cpp
+++ src/mbgl/renderer/layers/render_fill_layer.cpp
@@ -698,7 +698,7 @@ void RenderFillLayer::update(gfx::ShaderRegistry& shaders,
 
 #if MLN_TRIANGULATE_FILL_OUTLINES
             const auto outlineTriangulatedShader = doOutline && !dataDrivenOutline ? [&]() -> auto {
-                static const std::unordered_set<StringIdentity> outlinePropertiesAsUniforms{
+                static const mbgl::unordered_set<StringIdentity> outlinePropertiesAsUniforms{
                     stringIndexer().get("a_color"),
                     stringIndexer().get("a_opacity"),
                     stringIndexer().get("a_width"),
