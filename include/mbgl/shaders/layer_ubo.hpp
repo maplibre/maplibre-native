18e6802973571ab8442e7a466597f888d2dd18c1
Tim Sylvester <tim@wetdogweather.com>
2023-12-07 08:21:24 -0800

    Metal permutations 2 (#1923)

--- include/mbgl/shaders/layer_ubo.hpp
+++ include/mbgl/shaders/layer_ubo.hpp
@@ -33,18 +33,5 @@ struct Attribute {
 };
 static_assert(sizeof(Attribute) == 8);
 
-struct alignas(16) ExpressionInputsUBO {
-    // These can use uint64_t in later versions of Metal
-    /*  0 */ uint32_t time_lo; /// Current scene time (nanoseconds)
-    /*  4 */ uint32_t time_hi;
-    /*  8 */ uint32_t frame_lo; /// Current frame count
-    /* 12 */ uint32_t frame_hi;
-    /* 16 */ float zoom;      /// Current zoom level
-    /* 20 */ float zoom_frac; /// double precision zoom
-    /* 24 */ float pad1, pad2;
-    /* 32 */
-};
-static_assert(sizeof(ExpressionInputsUBO) == 2 * 16);
-
 } // namespace shaders
 } // namespace mbgl
