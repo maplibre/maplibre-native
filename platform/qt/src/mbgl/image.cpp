5bdfe0b87e923a7feb347ac32b34581ddf06938e
Andrew Calcutt <acalcutt@techidiots.net>
2024-01-16 17:38:06 -0500

    Try to fix qt test "Unsupported image type"
    Add

--- platform/qt/src/mbgl/image.cpp
+++ platform/qt/src/mbgl/image.cpp
@@ -19,7 +19,9 @@ std::string encodePNG(const PremultipliedImage& pre) {
 }
 
 #if !defined(QT_IMAGE_DECODERS)
+PremultipliedImage decodePNG(const uint8_t*, size_t);
 PremultipliedImage decodeJPEG(const uint8_t*, size_t);
+PremultipliedImage decodeWEBP(const uint8_t*, size_t);
 #endif
 
 PremultipliedImage decodeImage(const std::string& string) {
@@ -27,6 +29,28 @@ PremultipliedImage decodeImage(const std::string& string) {
     const size_t size = string.size();
 
 #if !defined(QT_IMAGE_DECODERS)
+    const auto readUInt = [](const uint8_t* imageData, ptrdiff_t offset) -> uint32_t {
+        const auto ptr = imageData + offset;
+        return (static_cast<uint32_t>(ptr[0]) << 24) | (static_cast<uint32_t>(ptr[1]) << 16) |
+               (static_cast<uint32_t>(ptr[2]) << 8) | static_cast<uint32_t>(ptr[3]);
+    };
+
+    if (size >= 16) {
+        uint32_t magic1 = readUInt(data, 0x0);
+        uint32_t magic2 = readUInt(data, 0x8);
+        // RIFF <xxxx = file size> WEBP
+        if (magic1 == 0x52494646 && magic2 == 0x57454250) {
+            return decodeWEBP(data, size);
+        }
+    }
+
+    if (size >= 4) {
+        uint32_t magic = readUInt(data, 0x0);
+        if (magic == 0x89504E47U) {
+            return decodePNG(data, size);
+        }
+    }
+
     if (size >= 2) {
         uint16_t magic = ((data[0] << 8) | data[1]) & 0xffff;
         if (magic == 0xFFD8) {
