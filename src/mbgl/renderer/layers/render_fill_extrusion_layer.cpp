0ba88480b575ad3ed6b3d75fb422e1c860b54320
Tim Sylvester <tim@wetdogweather.com>
2024-01-24 11:13:18 -0800

    Don't re-create extrusion tweakers

--- src/mbgl/renderer/layers/render_fill_extrusion_layer.cpp
+++ src/mbgl/renderer/layers/render_fill_extrusion_layer.cpp
@@ -75,9 +75,8 @@ void RenderFillExtrusionLayer::evaluate(const PropertyEvaluationParameters& para
     evaluatedProperties = std::move(properties);
 
 #if MLN_DRAWABLE_RENDERER
-    if (layerGroup) {
-        auto newTweaker = std::make_shared<FillExtrusionLayerTweaker>(getID(), evaluatedProperties);
-        replaceTweaker(layerTweaker, std::move(newTweaker), {layerGroup});
+    if (layerTweaker) {
+        layerTweaker->updateProperties(evaluatedProperties);
     }
 #endif // MLN_DRAWABLE_RENDERER
 }
