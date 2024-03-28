f4927dfe3b7c1ac0b8b2b3ef4af10b027caa7d18
Bart Louwers <bart@emeel.net>
2024-03-28 15:37:04 +0100

    pre-commit and clang-format

--- platform/glfw/glfw_metal_backend.h
+++ platform/glfw/glfw_metal_backend.h
@@ -10,25 +10,26 @@ struct GLFWwindow;
 @class NSWindow;
 
 class GLFWMetalBackend final : public GLFWBackend {
-public:
-    GLFWMetalBackend(GLFWwindow* window_, const bool capFrameRate);
-    ~GLFWMetalBackend() = default;
-    mbgl::gfx::RendererBackend& getRendererBackend() override;
-    void setSize(mbgl::Size) override;
-    mbgl::Size getSize() const override;
+ public:
+  GLFWMetalBackend(GLFWwindow *window_, const bool capFrameRate);
+  ~GLFWMetalBackend() = default;
+  mbgl::gfx::RendererBackend &getRendererBackend() override;
+  void setSize(mbgl::Size) override;
+  mbgl::Size getSize() const override;
 
-private:
-    NSWindow *window;
-    MetalBackend rendererBackend;
+ private:
+  NSWindow *window;
+  MetalBackend rendererBackend;
 };
 
 namespace mbgl {
 namespace gfx {
 
 template <>
-std::unique_ptr<GLFWBackend> Backend::Create<mbgl::gfx::Backend::Type::Metal>(GLFWwindow* window, bool capFrameRate) {
-    return std::make_unique<GLFWMetalBackend>(window, capFrameRate);
+std::unique_ptr<GLFWBackend> Backend::Create<mbgl::gfx::Backend::Type::Metal>(GLFWwindow *window,
+                                                                              bool capFrameRate) {
+  return std::make_unique<GLFWMetalBackend>(window, capFrameRate);
 }
 
-} // namespace gfx
-} // namespace mbgl
+}  // namespace gfx
+}  // namespace mbgl
