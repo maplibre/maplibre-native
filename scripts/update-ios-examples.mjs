46e17d0ee2ac29c654447d7dee0aa997eefcb90a
Bart Louwers <bart.louwers@gmail.com>
2024-05-12 18:06:43 +0200

    Add some comments

--- scripts/update-ios-examples.mjs
+++ scripts/update-ios-examples.mjs
@@ -1,18 +1,31 @@
+/**
+ * This script updates the iOS examples part of the DocC documentation.
+ * See platform/ios/CONTRIBUTING.md for the details.
+ */
+
 import fs from "fs/promises";
 
+/** documentation files that will be updated */
 const documentationPaths = [
   "platform/ios/MapLibre.docc/GettingStarted.md",
   "platform/ios/MapLibre.docc/LineOnUserTap.md",
 ];
 
-// source files that contain examples
+/** source files that contain examples */
 const examplePaths = [
   "platform/ios/app-swift/Sources/SimpleMapView.swift",
   "platform/ios/app-swift/Sources/LineTapMapView.swift"
 ];
 
 /**
+ * Parses a source file and looks for examples fenced in
+ * ```
+ * // #-example-code(ExampleId)
+ * ...
+ * // #-end-example-code
+ * ```
  * @param {string} examplePath
+ * @returns Mapping from example id to example, e.g. {"ExampleId": "..."}
  */
 async function parseExampleCode(examplePath) {
   const content = await fs.readFile(examplePath, "utf8");
@@ -45,6 +58,9 @@ async function parseExampleCode(examplePath) {
   return exampleCodeMap;
 }
 
+/**
+ * @returns Record of all example id to example code
+ */
 async function loadExamples() {
   /** @type {Record<string, string>} */
   let examples = {};
@@ -59,6 +75,7 @@ async function loadExamples() {
 }
 
 /**
+ * Replaces code block in examples with actual example in source code.
  * 
  * @param {string} documentationPath 
  * @param {Record<string, string>} examples 
@@ -67,21 +84,22 @@ async function updateDocumentation(documentationPath, examples) {
   const content = await fs.readFile(documentationPath, 'utf8');
   const lines = content.split('\n');
 
+  // This status implements little state machine.
   /** @type {{tag: 'find-example'} | {tag: 'find-codeblock', id: string} | {tag: 'skip-to-codeblock-end'}} */
   let status = {tag: 'find-example'};
   let currentId = '';
   let outputLines = [];
 
   for (const line of lines) {
-    if (status.tag === 'find-example') {
+    if (status.tag === 'find-example') {  // look for the start of an example
       const match = line.match(/<!-- include-example\(([^)]+)\) -->/);
-      if (match) {
+      if (match) { // found example
         currentId = match[1];
         status = {tag: 'find-codeblock', id: currentId};
       }
-      outputLines.push(line);
-    } else if (status.tag === 'find-codeblock') {
-      if (line === '```swift') {
+      outputLines.push(line); // otherwise include the line as-is
+    } else if (status.tag === 'find-codeblock') {  // look for a code block to replace
+      if (line === '```swift') {  // found it!
         outputLines.push(line);
         if (!examples[status.id]) throw Error(`Expected example with id='${status.id}'`);
         outputLines.push(examples[status.id].trim());
@@ -92,7 +110,7 @@ async function updateDocumentation(documentationPath, examples) {
     } else if (status.tag === 'skip-to-codeblock-end') {
       if (line === '```') {
         outputLines.push(line);
-        status = {tag: 'find-example'};
+        status = {tag: 'find-example'};  // look for the next example
       } else {
         continue;
       }
