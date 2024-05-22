9455386c6eb009a24fe38e4496f4db48cdbdad6d
Bart Louwers <bart.louwers@gmail.com>
2024-05-22 23:46:47 +0200

    Add comment

--- scripts/vendor/sqlite.sh
+++ scripts/vendor/sqlite.sh
@@ -13,4 +13,8 @@ mv sqlite3.h include
 mv sqlite3.c src
 file_list include src -name "*.h" -o -name "*.c"
 
+# We need this patch for compiling on Android
+# See https://www.sqlite.org/src/info/f18b2524da6bbbcf
+# should be removed when we update SQLite
+# and this is included in the release
 git apply $PWD/../../scripts/vendor/sqlite-ndk.patch
