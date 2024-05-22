7f99ac994a449e468ed30c5dd421d009ae8065f3
Bart Louwers <bart.louwers@gmail.com>
2024-05-22 23:21:42 +0200

    Update SQLite to 3.45.3

--- scripts/vendor/sqlite.sh
+++ scripts/vendor/sqlite.sh
@@ -2,13 +2,15 @@
 source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
 NAME=sqlite
-VERSION=3.24.0
-ROOT=sqlite-amalgamation-3240000
+VERSION=3.45.3
+ROOT=sqlite-amalgamation-3450300
 
-download "https://www.sqlite.org/2018/$ROOT.zip"
+download "https://www.sqlite.org/2024/$ROOT.zip"
 init
 extract_zip "$ROOT/sqlite3.*"
 mkdir -p include src
 mv sqlite3.h include
 mv sqlite3.c src
 file_list include src -name "*.h" -o -name "*.c"
+
+curl $URL | git apply -v --index
