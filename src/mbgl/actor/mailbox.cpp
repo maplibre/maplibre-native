d09197e51fa7a0dbc71a0db58928ee6102f7ce69
Tim Sylvester <tim@wetdogweather.com>
2024-05-29 13:49:52 -0700

    Allow for the weak scheduler reference expiring

--- src/mbgl/actor/mailbox.cpp
+++ src/mbgl/actor/mailbox.cpp
@@ -28,7 +28,9 @@ void Mailbox::open(Scheduler& scheduler_) {
 
     if (!queue.empty()) {
         auto guard = weakScheduler.lock();
-        if (weakScheduler) weakScheduler->schedule(makeClosure(shared_from_this()));
+        if (weakScheduler) {
+            weakScheduler->schedule(makeClosure(shared_from_this()));
+        }
     }
 }
 
@@ -107,9 +109,6 @@ void Mailbox::receive() {
     }};
     std::lock_guard<std::recursive_mutex> receivingLock(receivingMutex);
 
-    auto guard = weakScheduler.lock();
-    assert(weakScheduler);
-
     if (closed) {
         state = State::Abandoned;
         return;
@@ -128,8 +127,13 @@ void Mailbox::receive() {
 
     (*message)();
 
+    // If there are more messages in the queue and the scheduler
+    // is still active, create a new task to handle the next one
     if (!wasEmpty) {
-        weakScheduler->schedule(makeClosure(shared_from_this()));
+        auto guard = weakScheduler.lock();
+        if (weakScheduler) {
+            weakScheduler->schedule(makeClosure(shared_from_this()));
+        }
     }
 }
 
