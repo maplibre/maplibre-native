9df575a007e540e67b67c3275db30693b7b1e56c
Bart Louwers <bart.louwers@gmail.com>
2024-06-11 15:31:21 +0200

    Account for line breaks in declaration

--- platform/scripts/check-public-symbols.js
+++ platform/scripts/check-public-symbols.js
@@ -30,13 +30,13 @@ function hasMissingSymbols(os) {
       _.forEach(structure['key.substructure'], function (substructure) {
         switch (substructure['key.kind']) {
           case 'sourcekitten.source.lang.objc.decl.class':
-            if (!(keyword.test(src[substructure['key.doc.line'] - 1]) || keyword.test(src[substructure['key.doc.line'] - 2]))) {
+            if (!keyword.test(src[substructure['key.doc.line'] - 1]) && !keyword.test(src[substructure['key.doc.line'] - 2]) && !keyword.test(src[substructure['key.doc.line'] - 3]) && !keyword.test(src[substructure['key.doc.line'] - 4])) {
               console.warn(`- missing symbol export for class ${substructure['key.name']} in ${path}:${substructure['key.doc.line']}:${substructure['key.doc.column']}`);
               missing = true;
             }
             break;
           case 'sourcekitten.source.lang.objc.decl.constant':
-            if (!keyword.test(src[substructure['key.doc.line'] - 1])) {
+            if (!keyword.test(src[substructure['key.doc.line'] - 1]) && !keyword.test(src[substructure['key.doc.line'] - 2])) {
               console.warn(`- missing symbol export for constant ${substructure['key.name']} in ${path}:${substructure['key.doc.line']}:${substructure['key.doc.column']}`);
               missing = true;
             }
