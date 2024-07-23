5288c5f2e7c19a33b8b103d926f42df8d7313280
Dilshodbek <jumabaevdilshodbek@gmail.com>
2024-07-23 12:54:37 +0500

    adding activity files

--- platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/options/MapOptionsRuntimeActivity.kt
+++ platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/options/MapOptionsRuntimeActivity.kt
@@ -1,21 +1,100 @@
 package org.maplibre.android.testapp.activity.options
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.maplibre.android.camera.CameraPosition
+import org.maplibre.android.geometry.LatLng
+import org.maplibre.android.maps.MapLibreMap
+import org.maplibre.android.maps.MapLibreMapOptions
+import org.maplibre.android.maps.MapView
+import org.maplibre.android.maps.OnMapReadyCallback
+import org.maplibre.android.maps.Style
 import org.maplibre.android.testapp.R
+import org.maplibre.android.testapp.styles.TestStyles
 
-class MapOptionsRuntimeActivity : AppCompatActivity() {
+/**
+ *  TestActivity demonstrating configuring MapView with MapOptions
+ */
+class MapOptionsRuntimeActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var maplibreMap: MapLibreMap
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_map_options_runtime)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Create map configuration
+        val maplibreMapOptions = MapLibreMapOptions.createFromAttributes(this)
+        maplibreMapOptions.apply {
+            apiBaseUri("https://api.maplibre.org")
+            camera(CameraPosition.Builder()
+                .bearing(34.0)
+                .target(LatLng(52.519003, 13.400972))
+                .zoom(16.0)
+                .tilt(25.0)
+                .build()
+            )
+            maxPitchPreference(90.0)
+            minPitchPreference(0.0)
+            maxZoomPreference(34.0)
+            minZoomPreference(15.0)
+            localIdeographFontFamily("Droid Sans")
+            zoomGesturesEnabled(true)
+            compassEnabled(true)
+            compassFadesWhenFacingNorth(true)
+            scrollGesturesEnabled(true)
+            rotateGesturesEnabled(true)
+            tiltGesturesEnabled(true)
         }
+
+        // Create map programmatically, add to view hierarchy
+        mapView = MapView(this, maplibreMapOptions)
+        mapView.getMapAsync(this)
+        mapView.onCreate(savedInstanceState)
+        (findViewById<View>(R.id.container) as ViewGroup).addView(mapView)
+    }
+
+    override fun onMapReady(maplibreMap: MapLibreMap) {
+        this.maplibreMap = maplibreMap
+        this.maplibreMap.setStyle(
+            Style.Builder().fromUri(TestStyles.getPredefinedStyleWithFallback("Streets"))
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
--- platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/options/MapOptionsXmlActivity.kt
+++ platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/options/MapOptionsXmlActivity.kt
@@ -1,21 +1,69 @@
 package org.maplibre.android.testapp.activity.options
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.maplibre.android.maps.MapLibreMap
+import org.maplibre.android.maps.MapView
+import org.maplibre.android.maps.OnMapReadyCallback
+import org.maplibre.android.maps.Style
 import org.maplibre.android.testapp.R
+import org.maplibre.android.testapp.styles.TestStyles
 
-class MapOptionsXmlActivity : AppCompatActivity() {
+/**
+ *  TestActivity demonstrating configuring MapView with XML
+ */
+
+class MapOptionsXmlActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mapView: MapView
+    private lateinit var maplibreMap: MapLibreMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_map_options_xml)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(maplibreMap: MapLibreMap) {
+        this.maplibreMap = maplibreMap
+        this.maplibreMap.setStyle(
+            Style.Builder().fromUri(TestStyles.getPredefinedStyleWithFallback("Streets"))
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
