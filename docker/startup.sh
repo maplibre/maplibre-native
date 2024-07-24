5832961e0fd844afc852d73c4eea12a561796893
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-07-24 15:23:54 -0400

    Fix help message

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,7 +6,7 @@ if [ ! -d /app/.github ] || [ ! -d /home/user/.cache ]; then
     echo "       From the root of this repo, run the following command."
     echo "       You may add any command to perform in the container at the end of this command."
     echo " "
-    echo '  docker run --rm -it -v "$PWD:/app/" -v "$PWD/.docker_cache:/home/user/.cache" maplibre-native-image'
+    echo '  docker run --rm -it -v "$PWD:/app/" -v "$PWD/docker/.cache:/home/user/.cache" maplibre-native-image'
     exit 1
 fi
 
