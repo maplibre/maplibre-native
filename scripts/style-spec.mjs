d0239629ee70f871cb8d9a39f8b1d8fd152cdbcd
Bart Louwers <bart.louwers@gmail.com>
2024-08-23 15:10:58 +0200

    Update v8.json

--- scripts/style-spec.mjs
+++ scripts/style-spec.mjs
@@ -1,7 +1,22 @@
 import referenceSpec from "../scripts/style-spec-reference/v8.json" with { type: "json" };
 
+// https://github.com/maplibre/maplibre-native/issues/2368
+referenceSpec['layout_symbol']['icon-padding']['type'] = 'number';
+// https://github.com/maplibre/maplibre-native/issues/2754
+referenceSpec['layout_symbol']['icon-padding']['property-type'] = 'data constant';
+
 /** @type {any} */
 let modifiedReferenceSpec = referenceSpec;
+
+// https://github.com/maplibre/maplibre-native/issues/250
+delete modifiedReferenceSpec['layout_symbol']['text-rotation-alignment']["values"]['viewport-glyph']
+
+// https://github.com/maplibre/maplibre-native/issues/2358
+delete modifiedReferenceSpec['layout_symbol']['text-variable-anchor-offset'];
+
+// https://github.com/maplibre/maplibre-native/issues/251
+delete modifiedReferenceSpec['layout_symbol']['icon-overlap'];
+delete modifiedReferenceSpec['layout_symbol']['text-overlap'];
 
 modifiedReferenceSpec.layer.type.values["location-indicator"] = {};
 modifiedReferenceSpec["layout_location-indicator"] = {
