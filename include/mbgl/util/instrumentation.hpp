bc40259ccea614de951e383dd4746b9bb389ce2d
Alex Cristici <alex.cristici@gmail.com>
2024-08-26 16:23:33 +0300

    Update empty states for tracy defines.

--- include/mbgl/util/instrumentation.hpp
+++ include/mbgl/util/instrumentation.hpp
@@ -90,33 +90,33 @@ constexpr const char* tracyConstMemoryLabel = "Constant Buffer Memory";
 
 #else // MLN_RENDER_BACKEND_OPENGL
 
-#define MLN_TRACE_GL_CONTEXT()
-#define MLN_TRACE_GL_ZONE(label)
-#define MLN_TRACE_FUNC_GL()
+#define MLN_TRACE_GL_CONTEXT() ((void)0)
+#define MLN_TRACE_GL_ZONE(label) ((void)0)
+#define MLN_TRACE_FUNC_GL() ((void)0)
 #define MLN_END_FRAME() FrameMark;
 
 #endif // MLN_RENDER_BACKEND_OPENGL
 
 #else // MLN_TRACY_ENABLE
 
-#define MLN_TRACE_GL_CONTEXT()
-#define MLN_TRACE_GL_ZONE(label)
-#define MLN_ZONE_TEXT(label)
-#define MLN_ZONE_STR(str)
-#define MLN_ZONE_VALUE(val)
-#define MLN_TRACE_FUNC_GL()
-#define MLN_END_FRAME()
-#define MLN_TRACE_ALLOC_TEXTURE(id, size)
-#define MLN_TRACE_FREE_TEXTURE(id)
-#define MLN_TRACE_ALLOC_RT(id, size)
-#define MLN_TRACE_FREE_RT(id)
-#define MLN_TRACE_ALLOC_VERTEX_BUFFER(id, size)
-#define MLN_TRACE_FREE_VERTEX_BUFFER(id)
-#define MLN_TRACE_ALLOC_INDEX_BUFFER(id, size)
-#define MLN_TRACE_FREE_INDEX_BUFFER(id)
-#define MLN_TRACE_ALLOC_CONST_BUFFER(id, size)
-#define MLN_TRACE_FREE_CONST_BUFFER(id)
-#define MLN_TRACE_FUNC()
-#define MLN_TRACE_ZONE(label)
+#define MLN_TRACE_GL_CONTEXT() ((void)0)
+#define MLN_TRACE_GL_ZONE(label) ((void)0)
+#define MLN_ZONE_TEXT(label) ((void)0)
+#define MLN_ZONE_STR(str) ((void)0)
+#define MLN_ZONE_VALUE(val) ((void)0)
+#define MLN_TRACE_FUNC_GL() ((void)0)
+#define MLN_END_FRAME() ((void)0)
+#define MLN_TRACE_ALLOC_TEXTURE(id, size) ((void)0)
+#define MLN_TRACE_FREE_TEXTURE(id) ((void)0)
+#define MLN_TRACE_ALLOC_RT(id, size) ((void)0)
+#define MLN_TRACE_FREE_RT(id) ((void)0)
+#define MLN_TRACE_ALLOC_VERTEX_BUFFER(id, size) ((void)0)
+#define MLN_TRACE_FREE_VERTEX_BUFFER(id) ((void)0)
+#define MLN_TRACE_ALLOC_INDEX_BUFFER(id, size) ((void)0)
+#define MLN_TRACE_FREE_INDEX_BUFFER(id) ((void)0)
+#define MLN_TRACE_ALLOC_CONST_BUFFER(id, size) ((void)0)
+#define MLN_TRACE_FREE_CONST_BUFFER(id) ((void)0)
+#define MLN_TRACE_FUNC() ((void)0)
+#define MLN_TRACE_ZONE(label) ((void)0)
 
 #endif // MLN_TRACY_ENABLE
