9cf750ba63b53e65f8982d86aae735bdc2b9c9f5
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2024-09-06 21:05:40 +0300

    Fixed merge conflicts

--- platform/android/MapLibreAndroid/src/vulkan/java/org/maplibre/android/maps/renderer/surfaceview/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the MapLibre Maps Android MapSurfaceView API classes.
- */
-package org.maplibre.android.maps.renderer.surfaceview;
--- platform/android/MapLibreAndroid/src/vulkan/java/org/maplibre/android/maps/renderer/textureview/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the MapLibre Maps Android TextureView API classes.
- */
-package org.maplibre.android.maps.renderer.textureview;
