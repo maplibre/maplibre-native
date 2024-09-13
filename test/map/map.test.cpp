497bb1ccd0ebc8fa15048b018dfa9b4f8ee00db1
mwilsnd <mwilsnd@gmail.com>
2024-09-13 11:42:35 -0400

    Expect loading to possibly start parsing

--- test/map/map.test.cpp
+++ test/map/map.test.cpp
@@ -1808,7 +1808,11 @@ TEST(Map, ObserveTileLifecycle) {
                 case TileOperation::StartParse: {
                     // Parsing is expected to be started early during the request process by a call to `setLayers`
                     EXPECT_THAT(stage,
-                                testing::AnyOf(TileOperation::RequestedFromCache, TileOperation::RequestedFromNetwork));
+                                testing::AnyOf(
+                                    TileOperation::RequestedFromCache,
+                                    TileOperation::RequestedFromNetwork,
+                                    TileOperation::LoadFromCache,
+                                    TileOperation::LoadFromNetwork));
                     EXPECT_FALSE(parsing); // We must not already be parsing when seeing this marker.
                     stage = TileOperation::StartParse;
                     parsing = true;
