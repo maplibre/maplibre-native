88c565266f97e121163c31aedc09637de7a2196f
kaushalbx <kaushalbx@gmail.com>
2024-09-24 20:58:00 +0530

    fixed publish-root.gradle.kts

--- platform/android/gradle/publish-root.gradle.kts
+++ platform/android/gradle/publish-root.gradle.kts
@@ -1,13 +1,6 @@
-val signingKeyId: String? = System.getenv("SIGNING_KEY_ID")
-val signingPassword: String? = System.getenv("SIGNING_PASSWORD")
-val signingSecretKeyRingFile: String = "${projectDir}/../signing-key.gpg"
-val ossrhUsername: String? = System.getenv("OSSRH_USERNAME")
-val ossrhPassword: String? = System.getenv("OSSRH_PASSWORD")
-val sonatypeStagingProfileId: String? = System.getenv("SONATYPE_STAGING_PROFILE_ID")
-
-ext["signing.keyId"] = signingKeyId
-ext["signing.password"] = signingPassword
-ext["signing.secretKeyRingFile"] = signingSecretKeyRingFile
-ext["ossrhUsername"] = ossrhUsername
-ext["ossrhPassword"] = ossrhPassword
-ext["sonatypeStagingProfileId"] = sonatypeStagingProfileId
+extra["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+extra["signing.password"] = System.getenv("SIGNING_PASSWORD")
+extra["signing.secretKeyRingFile"] = "${projectDir}/../signing-key.gpg"
+extra["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+extra["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+extra["sonatypeStagingProfileId"] = System.getenv("SONATYPE_STAGING_PROFILE_ID")
