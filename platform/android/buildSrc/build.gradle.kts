fa556bc9dc5917929fa73020d5282d9d51d790af
kaushalbx <kaushalbx@gmail.com>
2024-10-04 22:50:08 +0530

    checkstyle failed fix

--- platform/android/buildSrc/build.gradle.kts
+++ platform/android/buildSrc/build.gradle.kts
@@ -10,9 +10,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:5.2.1")
-    testImplementation("junit:junit:4.13")
     implementation("com.vanniktech:gradle-dependency-graph-generator-plugin:0.3.0")
     implementation("com.android.tools.build:gradle:8.6.0")
     implementation("com.android.application:com.android.application.gradle.plugin:8.6.0")
+    implementation("commons-collections:commons-collections:3.2.2")
 }
