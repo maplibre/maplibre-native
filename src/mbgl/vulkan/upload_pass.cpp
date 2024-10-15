5f5f8465f18af003e2202b5d2422a5e098462f4e
Alex Cristici <alex.cristici@gmail.com>
2024-10-15 17:27:27 +0300

    Fixed Vulkan.

--- src/mbgl/vulkan/upload_pass.cpp
+++ src/mbgl/vulkan/upload_pass.cpp
@@ -123,7 +123,7 @@ gfx::AttributeBindingArray UploadPass::buildAttributeBindings(
     const gfx::VertexAttributeArray& overrides,
     const gfx::BufferUsageType usage,
     const std::optional<std::chrono::duration<double>> lastUpdate,
-    /*out*/ std::vector<std::unique_ptr<gfx::VertexBufferResource>>&)) {
+    /*out*/ std::vector<std::unique_ptr<gfx::VertexBufferResource>>&) {
     gfx::AttributeBindingArray bindings;
 
     // For each attribute in the program, with the corresponding default and optional override...
