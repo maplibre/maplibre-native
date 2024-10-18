3a2e058d8ffc0ff70291749011b8f3a8bee7c004
Bart Louwers <bart.louwers@gmail.com>
2024-10-18 13:32:16 +0200

    add back explicit deduction guide

--- include/mbgl/util/variant.hpp
+++ include/mbgl/util/variant.hpp
@@ -23,5 +23,8 @@ template <class... Ts>
 struct overloaded : Ts... {
     using Ts::operator()...;
 };
+// explicit deduction guide (not needed as of C++20)
+template <class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
 
 } // namespace mbgl
