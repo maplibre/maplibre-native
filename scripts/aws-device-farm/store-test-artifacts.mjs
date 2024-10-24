af39515af4b48e8a4d17a34e5e36aaea6090c1a9
Bart Louwers <bart.louwers@gmail.com>
2024-10-24 12:14:24 +0200

    Upload Android benchmark results to S3

--- scripts/aws-device-farm/store-test-artifacts.mjs
+++ scripts/aws-device-farm/store-test-artifacts.mjs
@@ -3,28 +3,71 @@ import { Readable } from "node:stream";
 import { finished } from "node:stream/promises";
 import * as path from "node:path";
 import * as crypto from "node:crypto";
+import { parseArgs } from "node:util";
 
-import { ListArtifactsCommand, ListJobsCommand, ListSuitesCommand } from "@aws-sdk/client-device-farm";
+import { ArtifactType, ListArtifactsCommand, ListJobsCommand, ListSuitesCommand } from "@aws-sdk/client-device-farm";
 import { getDeviceFarmClient } from "./device-farm-client.mjs";
+
+function getArgs() {
+  const {
+    values
+  } = parseArgs({
+    options: {
+      outputDir: {
+        type: "string",
+      },
+      runArn: {
+        type: "string"
+      },
+      testsSuite: {
+       type: "boolean" 
+      },
+      customerArtifacts: {
+        type: "boolean"
+      }
+    },
+  });
+  const { outputDir, runArn } = values;
+  if (typeof outputDir !== 'string') usage();
+
+  if (typeof runArn !== 'string') usage();
+
+  function suitesFilter() {
+    const names = new Set();
+    if (values.testsSuite) names.add("Tests Suite");
+
+    if (names.size === 0) return () => true;
+
+    return (/** @type {string} **/ name) => names.has(name);
+  }
+
+  /** @type {() => ArtifactType[]} **/
+  function getArtifactsToDownload() {
+    if (values.customerArtifacts) return ["CUSTOMER_ARTIFACT"];
+    return ['TESTSPEC_OUTPUT', 'CUSTOMER_ARTIFACT', 'CUSTOMER_ARTIFACT_LOG', 'DEVICE_LOG'];
+  }
+
+  return {
+    outputDir,
+    runArn,
+    suitesFilter,
+    artifactsToDownload: getArtifactsToDownload()
+  }
+}
+
+const { outputDir, runArn, suitesFilter, artifactsToDownload } = getArgs();
 
 /**
  * @returns {never}
  */
 function usage() {
   console.error("Stores artifacts from AWS Device Farm run");
-  console.error(`Usage: node ${process.argv.at(1)} RUN_ARN OUTPUT_DIR`);
+  console.error("Usage: node store-test-artifacts.mjs --outputDir OUTPUT_DIR --runArn RUN_ARN");
+  console.error("Arguments:")
+  console.error("--customerArtifacts: only download customer artifacts");
+  console.error("--testsSuite: only download stuff from Tests Suite");
   process.exit(1);
 }
-
-if (process.argv.length !== 4) usage();
-
-const arn = process.argv.at(2);
-const outputDirArg = process.argv.at(3);
-
-if (typeof arn !== 'string') usage();
-if (typeof outputDirArg !== 'string') usage();
-
-const outputDir = outputDirArg;
 
 if (!fs.existsSync(outputDir)) {
   console.error("Output dir does not exist");
@@ -46,14 +89,14 @@ async function getTestSpecOutput(arn) {
   
   await Promise.all((jobs.jobs || []).map(async (job) => {
     const suites = await deviceFarmClient.send(new ListSuitesCommand({arn: job.arn}));
-    await Promise.all((suites.suites || []).map(async (suite) => {
+    await Promise.all((suites.suites || []).filter(suitesFilter).map(async (suite) => {
       const artifacts = await deviceFarmClient.send(new ListArtifactsCommand({
         arn: suite.arn,
         type: 'FILE'
       }));
       await Promise.all((artifacts.artifacts || []).map(async (artifact) => {
         if (!artifact.name || !artifact.url || !artifact.type) return;
-        if (['TESTSPEC_OUTPUT', 'CUSTOMER_ARTIFACT', 'CUSTOMER_ARTIFACT_LOG', 'DEVICE_LOG'].includes(artifact.type)) {
+        if (artifactsToDownload.includes(artifact.type)) {
           const filename = `${artifact.name.replaceAll(' ', '_')}-${crypto.randomBytes(10).toString('hex')}.${artifact.extension}`;
           const res = await fetch(artifact.url);
           if (!res.ok || !res.body) return;
@@ -66,4 +109,4 @@ async function getTestSpecOutput(arn) {
   }));
 }
 
-await getTestSpecOutput(arn);
+await getTestSpecOutput(runArn);
