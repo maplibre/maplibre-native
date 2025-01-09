bdc6fcf7dd4e9d7f40bda4d3ec7612186cc10bcf
Bart Louwers <bart@emeel.net>
2025-01-09 14:14:48 +0100

    Prepare MapLibre iOS 6.10.0 release

--- platform/ios/src/MLNMapView.h
+++ platform/ios/src/MLNMapView.h
@@ -1310,19 +1310,18 @@ vertically on the map.
  the user find his or her bearings even after traversing a great distance.
 
  @param camera The new viewpoint.
+ @param insets The minimum padding (in screen points) that would be visible
+    around the returned camera object if it were set as the receiver's camera.
  @param duration The amount of time, measured in seconds, that the transition
     animation should take. Specify `0` to jump to the new viewpoint
     instantaneously. Specify a negative value to use the default duration, which
     is based on the length of the flight path.
- @param edgePadding The minimum padding (in screen points) that would be visible
- around the returned camera object if it were set as the receiver’s camera.
  @param completion The block to execute after the animation finishes.
  */
 - (void)flyToCamera:(MLNMapCamera *)camera
           edgePadding:(UIEdgeInsets)insets
          withDuration:(NSTimeInterval)duration
     completionHandler:(nullable void (^)(void))completion;
-
 /**
  Returns the camera that best fits the given coordinate bounds.
 
