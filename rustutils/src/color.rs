20bef88e64c6a7ebc7fd9f89e5b7e371f883eca6
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2025-01-24 15:54:14 -0500

    simplify ci, add tests

--- rustutils/src/color.rs
+++ rustutils/src/color.rs
@@ -2,6 +2,9 @@ use csscolorparser::Color;
 
 #[cxx::bridge(namespace = "rustutils")]
 mod ffi {
+    // TODO: Use #[cfg_attr(test, derive(...))] once supported
+    // See https://github.com/dtolnay/cxx/issues/1022
+    #[derive(Debug, PartialEq)]
     struct ParsedColor {
         pub success: bool,
         pub r: f32,
@@ -32,4 +35,43 @@ pub fn parse_css_color(css_str: &str) -> ffi::ParsedColor {
             b: 0.0,
             a: 0.0,
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_css_color() {
+        assert_eq!(
+            parse_css_color("rgb(255, 0, 0)"),
+            ffi::ParsedColor {
+                success: true,
+                r: 1.0,
+                g: 0.0,
+                b: 0.0,
+                a: 1.0,
+            }
+        );
+        assert_eq!(
+            parse_css_color("rgba(255, 0, 0, 0.5)"),
+            ffi::ParsedColor {
+                success: true,
+                r: 1.0,
+                g: 0.0,
+                b: 0.0,
+                a: 0.5,
+            }
+        );
+        assert_eq!(
+            parse_css_color("invalid"),
+            ffi::ParsedColor {
+                success: false,
+                r: 0.0,
+                g: 0.0,
+                b: 0.0,
+                a: 0.0,
+            }
+        );
+    }
 }
