2ae0d902490a2448c5a741567b8e898802dec4ec
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2025-01-27 12:53:37 -0500

    wip

--- platform/rust-cxx/build.rs
+++ platform/rust-cxx/build.rs
@@ -1,10 +1,18 @@
 fn main() {
-    cxx_build::bridge("src/main.rs")
-        .file("src/blobstore.cc")
-        .std("c++20")
-        .compile("cxxbridge-demo");
+    // use src/main.rs to generate a header
+    // use cmake crate with flags to compile that header + rust-ffi.cc + core -> staticlib
 
-    println!("cargo:rerun-if-changed=src/main.rs");
-    println!("cargo:rerun-if-changed=src/blobstore.cc");
-    println!("cargo:rerun-if-changed=include/blobstore.h");
+    // println what to link with
+    println!("cargo:rustc-link-lib=static=rust-ffi");
+
+
+    //
+    // cxx_build::bridge("src/main.rs")
+    //     .file("src/blobstore.cc")
+    //     .std("c++20")
+    //     .compile("cxxbridge-demo");
+    //
+    // println!("cargo:rerun-if-changed=src/main.rs");
+    // println!("cargo:rerun-if-changed=src/blobstore.cc");
+    // println!("cargo:rerun-if-changed=include/blobstore.h");
 }
