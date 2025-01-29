b85ac6f925b316963218272565a4074aed3a4104
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2025-01-29 11:34:09 -0500

    add Rust-specific C++ wrapper, renderer args

---
+++ platform/rust/src/wrapper.cc
@@ -0,0 +1,12 @@
+#include "maplibre-native/include/wrapper.h"
+#include "maplibre-native/src/lib.rs.h"
+
+namespace ml {
+namespace rust {
+
+uint32_t get_42() {
+    return 42;
+}
+
+} // namespace rust
+} // namespace ml
