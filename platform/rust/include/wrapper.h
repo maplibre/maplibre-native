ce96c6281b9f938d96b2fa836369bee5b0f85a5d
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2025-01-30 02:03:10 -0500

    implement TileServerOptions instantiation and access
    BUG: the optional<str> access returns junk

--- platform/rust/include/wrapper.h
+++ platform/rust/include/wrapper.h
@@ -1,11 +1,28 @@
 #pragma once
 #include "rust/cxx.h"
 #include "mbgl/math/log2.hpp"
+#include "mbgl/util/tile_server_options.hpp"
 
 namespace ml {
 namespace rust {
 
 uint32_t get_42();
+
+using namespace mbgl;
+
+// TileServerOptions constructor helpers
+std::unique_ptr<TileServerOptions> TileServerOptions_new();
+std::unique_ptr<TileServerOptions> TileServerOptions_default();
+std::unique_ptr<TileServerOptions> TileServerOptions_mapbox();
+std::unique_ptr<TileServerOptions> TileServerOptions_maplibre();
+std::unique_ptr<TileServerOptions> TileServerOptions_maptiler();
+
+// TileServerOptions Optional<string> helpers (not supported yet by cxx)
+const int8_t * TileServerOptions_sourceVersionPrefix(const TileServerOptions& self);
+const int8_t * TileServerOptions_styleVersionPrefix(const TileServerOptions& self);
+const int8_t * TileServerOptions_spritesVersionPrefix(const TileServerOptions& self);
+const int8_t * TileServerOptions_glyphsVersionPrefix(const TileServerOptions& self);
+const int8_t * TileServerOptions_tileVersionPrefix(const TileServerOptions& self);
 
 } // namespace rust
 } // namespace ml
