d1747c9ee35883ff873eebf5786504499edf514c
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2025-02-06 14:06:32 +0200

    Fix drawable depth options order

---
+++ platform/glfw/example_custom_drawable_style_layer.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <mbgl/style/layers/custom_drawable_layer.hpp>
+
+class ExampleCustomDrawableStyleLayerHost : public mbgl::style::CustomDrawableLayerHost {
+public:
+    using VertexVector = mbgl::gfx::VertexVector<Interface::CommonGeometryVertex>;
+    using TriangleIndexVector = mbgl::gfx::IndexVector<mbgl::gfx::Triangles>;
+
+    ExampleCustomDrawableStyleLayerHost(const std::string& assetsPath);
+    ~ExampleCustomDrawableStyleLayerHost();
+
+    void initialize() override;
+    void deinitialize() override;
+
+    void update(Interface& interface);
+
+protected:
+    mbgl::Point<double> project(const mbgl::LatLng& c, const mbgl::TransformState& s);
+
+    void createDrawables(Interface& interface);
+    void generateCommonGeometry(Interface& interface);
+    void loadCommonGeometry(Interface& interface);
+    void importObj(Interface& interface,
+                   const std::string& filename,
+                   VertexVector& vertices,
+                   TriangleIndexVector& indices,
+                   Interface::CommonGeometryOptions& options);
+
+    mbgl::gfx::Texture2DPtr createCheckerboardTexture(Interface& interface,
+                                                      uint16_t wb,
+                                                      uint16_t hb,
+                                                      uint16_t blockSize,
+                                                      const std::array<uint8_t, 4>& color1,
+                                                      const std::array<uint8_t, 4>& color2);
+
+protected:
+    const std::string assetsPath;
+};
