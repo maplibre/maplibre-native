f490037a761c0ec61c7d1adb5c93d7ea026a13cf
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2025-02-10 16:34:34 -0500

    add sqlite3 dyn link workaround

--- platform/rust/build.rs
+++ platform/rust/build.rs
@@ -107,6 +107,24 @@ fn main() {
     // Link mbgl-core after the bridge - or else `cargo test` won't be able to find the symbols.
     println!("cargo:rustc-link-lib=static=mbgl-core");
 
+    // Link to SQLite in order for `cargo test` to work
+    // TODO: Ensure cmake always uses statically linking to sqlite3
+    // Note that this works, but we do not want to depend on dynamic bindings in Rust
+    println!("cargo:rustc-link-lib=dylib=sqlite3");
+    // Note that linker automatically gets this passed from core, but that seems to not be enough to solve the issues bellow
+    // println!("cargo:rustc-link-lib=static=mbgl-vendor-sqlite");
+    //
+    // FIXME: This is a hack to link the SQLite library on Linux.
+    //        This does NOT work for `cargo run`, but works for `cargo test`.
+    //
+    // cargo run linking errors:
+    // platform/rust/target/debug/build/maplibre-native-d736790a198d5b5a/out/build/libmbgl-core.a(sqlite3.cpp.o): in function `mapbox::sqlite::initialize()':
+    // platform/default/src/mbgl/storage/sqlite3.cpp:117:(.text._ZN6mapbox6sqliteL10initializeEv+0x1f): undefined reference to `sqlite3_libversion_number'
+    // platform/default/src/mbgl/storage/sqlite3.cpp:119:(.text._ZN6mapbox6sqliteL10initializeEv+0x44): undefined reference to `sqlite3_libversion_number'
+    // platform/default/src/mbgl/storage/sqlite3.cpp:130:(.text._ZN6mapbox6sqliteL10initializeEv+0xd5): undefined reference to `sqlite3_config'
+    //
+    // println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+    // println!("cargo:rustc-link-lib=static=sqlite3");
     // ------------------------------------------------------------------------
     // 4. Instruct Cargo when to re-run the build script.
     // ------------------------------------------------------------------------
