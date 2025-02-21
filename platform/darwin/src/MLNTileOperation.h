0b9a8afd1bebffeec7eb7a704240c73c959f5598
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2025-02-21 15:07:14 +0200

    Add macos hooks

---
+++ platform/darwin/src/MLNTileOperation.h
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+
+#ifndef NS_ENUM
+#define NS_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
+#endif
+
+typedef NS_ENUM(NSInteger, MLNTileOperation) {
+    RequestedFromCache,   ///< A read request from the cache
+    RequestedFromNetwork, ///< A read request from the online source
+    LoadFromNetwork,      ///< Tile data from the network has been retrieved
+    LoadFromCache,        ///< Tile data from the cache has been retrieved
+    StartParse,           ///< Background processing of tile data has been initiated
+    EndParse,             ///< Background processing of tile data has been completed
+    Error,                ///< An error occurred while loading the tile
+    Cancelled,            ///< Loading of a tile was cancelled
+    NullOp,               ///< No operation has taken place
+};
