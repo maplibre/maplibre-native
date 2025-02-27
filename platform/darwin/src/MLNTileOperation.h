e0a0e5fc740b66724f225a9f4891fb73d230f4fe
Bart Louwers <bart@emeel.net>
2025-02-27 01:29:26 +0100

    Remove redundant macro def

--- platform/darwin/src/MLNTileOperation.h
+++ platform/darwin/src/MLNTileOperation.h
@@ -1,11 +1,5 @@
 #import <Foundation/Foundation.h>
 
-#ifndef NS_ENUM
-#define NS_ENUM(_type, _name) \
-  enum _name : _type _name;   \
-  enum _name : _type
-#endif
-
 typedef NS_ENUM(NSInteger, MLNTileOperation) {
   MLNTileOperationRequestedFromCache,    ///< A read request from the cache
   MLNTileOperationRequestedFromNetwork,  ///< A read request from the online source
