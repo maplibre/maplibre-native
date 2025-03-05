b3828292bdcba9bb76351fcfd7bb1bb45d890395
Yousif Aldolaijan <yaldolaijan@hudhud.sa>
2025-03-05 21:56:56 +0300

    support android opengl and vulkan release to github
    - disbable android device farm test
    - add support to build and release maplibre-native for android
     with vulkan and opengl builds.
    - Add support to publish to github packages

--- platform/android/build.gradle.kts
+++ platform/android/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 }
 
 
-nexusPublishing {
-    repositories {
-        sonatype {
-            stagingProfileId.set(extra["sonatypeStagingProfileId"] as String?)
-            username.set(extra["ossrhUsername"] as String?)
-            password.set(extra["ossrhPassword"] as String?)
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-        }
-    }
-}
+// nexusPublishing {
+//     repositories {
+//         sonatype {
+//             stagingProfileId.set(extra["sonatypeStagingProfileId"] as String?)
+//             username.set(extra["ossrhUsername"] as String?)
+//             password.set(extra["ossrhPassword"] as String?)
+//             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+//             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+//         }
+//     }
+// }
