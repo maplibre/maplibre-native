e76837cc9db64b4697430e1995b597a071d272d5
Tiago Costa <tdcosta100@gmail.com>
2025-04-28 07:26:44 -0300

    Drop OSMesa since it was phased out from Mesa3D project

--- platform/windows/Get-VendorPackages.ps1
+++ platform/windows/Get-VendorPackages.ps1
@@ -24,7 +24,6 @@ foreach($letter in [byte][char]'Z'..[byte][char]'A')
 switch($Renderer)
 {
     'EGL'    { $renderer_packages = @('egl', 'opengl-registry'); break }
-    'OSMesa' { $renderer_packages = @();                         break }
     'OpenGL' { $renderer_packages = @('opengl-registry');        break }
 	'Vulkan' { $renderer_packages = @();                         break }
 	'All'    { $renderer_packages = @('egl', 'opengl-registry'); break }
@@ -46,12 +45,3 @@ if(-not (Test-Path ('{0}\vcpkg.exe' -f $vcpkg_temp_dir)))
 )
 
 subst $vcpkg_temp_dir /D
-
-if($Renderer -eq 'OSMesa' -and -not (Test-Path ([System.IO.Path]::Combine($PWD.Path, 'vendor', 'mesa3d'))))
-{
-    New-Item -Name temp -Type Directory | Out-Null
-    Invoke-WebRequest https://www.7-zip.org/a/7zr.exe -OutFile ([System.IO.Path]::Combine($PWD.Path, 'temp', '7zr.exe'))
-    (Invoke-WebRequest https://api.github.com/repos/pal1000/mesa-dist-win/releases | ConvertFrom-Json)[0].assets | Where-Object name -match 'mesa3d-.+-(release|devel)-msvc\.7z' | foreach { Invoke-WebRequest $_.browser_download_url -OutFile ([System.IO.Path]::Combine($PWD.Path, 'temp', $_.name)) }
-    Get-ChildItem 'temp\*.7z' | foreach { .\temp\7zr.exe x -ovendor\mesa3d $_.FullName }
-    Remove-Item temp -Recurse
-}
