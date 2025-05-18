30471ac6f9d19f4395beebac1268a3c61171bb97
Bart Louwers <bart.louwers@gmail.com>
2025-05-19 00:03:38 +0200

    Update platform/linux/linux.cmake
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- platform/linux/linux.cmake
+++ platform/linux/linux.cmake
@@ -143,7 +143,7 @@ include(${PROJECT_SOURCE_DIR}/vendor/nunicode.cmake)
 include(${PROJECT_SOURCE_DIR}/vendor/sqlite.cmake)
 
 if(NOT ${ICU_FOUND} OR "${ICU_VERSION}" VERSION_LESS 62.0 OR MLN_USE_BUILTIN_ICU)
-    message(STATUS "ICU not found or too old, using builtin.")
+    message(STATUS "ICU not found, too old or MLN_USE_BUILTIN_ICU requestd, using builtin.")
 
     set(MLN_USE_BUILTIN_ICU TRUE)
     include(${PROJECT_SOURCE_DIR}/vendor/icu.cmake)
