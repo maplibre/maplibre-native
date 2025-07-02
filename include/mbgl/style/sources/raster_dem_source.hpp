cfc21ee3ff659427442d475f5be507868a42bf16
Scott Hoyt <scottrhoyt@gmail.com>
2025-07-02 15:24:25 -0700

    Introduce RasterDEMOptions.

--- include/mbgl/style/sources/raster_dem_source.hpp
+++ include/mbgl/style/sources/raster_dem_source.hpp
@@ -8,10 +8,23 @@ namespace mbgl {
 
 namespace style {
 
+struct RasterDEMOptions {
+    std::optional<Tileset::DEMEncoding> encoding = std::nullopt;
+};
+
 class RasterDEMSource : public RasterSource {
 public:
-    RasterDEMSource(std::string id, variant<std::string, Tileset> urlOrTileset, uint16_t tileSize);
+    RasterDEMSource(std::string id,
+                    variant<std::string, Tileset> urlOrTileset,
+                    uint16_t tileSize,
+                    std::optional<RasterDEMOptions> options = std::nullopt);
     bool supportsLayerType(const mbgl::style::LayerTypeInfo*) const override;
+
+protected:
+    void setTilesetOverrides(Tileset& tileset) override;
+
+private:
+    std::optional<RasterDEMOptions> options;
 };
 
 template <>
