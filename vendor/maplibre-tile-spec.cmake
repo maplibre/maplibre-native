671478f3d722cfaad02b662e901aa66ca7eb78ef
Tim Sylvester <tim@wetdogweather.com>
2025-07-14 14:24:24 -0700

    Disable FastPFOR

--- vendor/maplibre-tile-spec.cmake
+++ vendor/maplibre-tile-spec.cmake
@@ -1,5 +1,6 @@
 set(MLT_WITH_JSON OFF CACHE BOOL "No JSON support" FORCE)
 set(MLT_WITH_PROTOZERO OFF CACHE BOOL "No protozero" FORCE)
+set(MLT_WITH_FASTPFOR OFF CACHE BOOL "disabled for lack of support for ARMv7 in SIMDE and requirement for SSE on x86" FORCE)
 
 # This is set by the platform CMakeLists.txt after vendor libraries.  If it was set earlier, we could just use it.
 set(CMAKE_OSX_DEPLOYMENT_TARGET 14.3)
