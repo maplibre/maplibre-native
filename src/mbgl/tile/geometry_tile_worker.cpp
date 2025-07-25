de3897a98299bc7f1e9a69907aefbb2aa27127f7
“Malcolm <mtoon@atlasprogramming.com>
2025-07-25 10:57:30 -0400

    Reverting some logging

--- src/mbgl/tile/geometry_tile_worker.cpp
+++ src/mbgl/tile/geometry_tile_worker.cpp
@@ -397,14 +397,7 @@ void GeometryTileWorker::parse() {
     groupMap.reserve(layers->size());
 
     for (auto layer : *layers) {
-        auto lk = layoutKey(*layer->baseImpl);
-        //std::cout << "LK: " << lk << "\n";
-        if (lk.find("parking_space1") != std::string::npos) {
-        //    std::cout << "  Found PLugin\n";
-        } else {
-           // continue;
-        }
-        groupMap[lk].push_back(std::move(layer));
+        groupMap[layoutKey(*layer->baseImpl)].push_back(std::move(layer));
     }
 
     for (auto& pair : groupMap) {
