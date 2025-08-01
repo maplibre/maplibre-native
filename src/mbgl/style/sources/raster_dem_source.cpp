ca5f79c12d89005f3f2a582f50adc21512264498
Scott Hoyt <scottrhoyt@gmail.com>
2025-08-01 04:50:09 -0700

    Fix for raster dem encoding override in style.json (#3570)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/mbgl/style/sources/raster_dem_source.cpp
+++ src/mbgl/style/sources/raster_dem_source.cpp
@@ -3,7 +3,7 @@
 #include <mbgl/style/layer.hpp>
 #include <mbgl/style/source_observer.hpp>
 #include <mbgl/style/sources/raster_dem_source.hpp>
-#include <mbgl/style/sources/raster_source_impl.hpp>
+#include <mbgl/style/sources/tile_source_impl.hpp>
 #include <mbgl/tile/tile.hpp>
 #include <mbgl/util/mapbox.hpp>
 #include <utility>
@@ -11,11 +11,23 @@
 namespace mbgl {
 namespace style {
 
-RasterDEMSource::RasterDEMSource(std::string id, variant<std::string, Tileset> urlOrTileset_, uint16_t tileSize)
-    : RasterSource(std::move(id), std::move(urlOrTileset_), tileSize, SourceType::RasterDEM) {}
+RasterDEMSource::RasterDEMSource(std::string id,
+                                 variant<std::string, Tileset> urlOrTileset_,
+                                 uint16_t tileSize,
+                                 std::optional<RasterDEMOptions> options_)
+    : RasterSource(std::move(id), std::move(urlOrTileset_), tileSize, SourceType::RasterDEM),
+      options(std::move(options_)) {}
 
 bool RasterDEMSource::supportsLayerType(const mbgl::style::LayerTypeInfo* info) const {
     return mbgl::underlying_type(Tile::Kind::RasterDEM) == mbgl::underlying_type(info->tileKind);
+}
+
+void RasterDEMSource::setTilesetOverrides(Tileset& tileset) {
+    if (options) {
+        if (std::optional<Tileset::DEMEncoding> encoding = options.value().encoding) {
+            tileset.encoding = encoding.value();
+        }
+    }
 }
 
 } // namespace style
