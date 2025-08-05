05b3af0bd7ae8b172e56024b4066ca3c45796fd0
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2025-08-05 13:40:26 +0300

    Add Android Sentry

--- platform/android/MapLibreAndroidTestApp/build.gradle.kts
+++ platform/android/MapLibreAndroidTestApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     id("maplibre.gradle-config")
     id("maplibre.gradle-checkstyle")
     id("maplibre.gradle-lint")
+    alias(libs.plugins.sentry)
 }
 
 
@@ -118,4 +119,110 @@ dependencies {
     androidTestImplementation(libs.androidxTestExtJUnit)
     androidTestImplementation(libs.androidxTestCoreKtx)
     androidTestImplementation(libs.kotlinxCoroutinesTest)
+}
+
+sentry {
+    // Disables or enables debug log output, e.g. for for sentry-cli.
+    // Default is disabled.
+    debug.set(false)
+
+    // The slug of the Sentry organization to use for uploading proguard mappings/source contexts.
+    org.set(System.getenv("SENTRY_ORG"))
+
+    // The slug of the Sentry project to use for uploading proguard mappings/source contexts.
+    projectName.set(System.getenv("SENTRY_PROJECT"))
+
+    // The authentication token to use for uploading proguard mappings/source contexts.
+    // WARNING: Do not expose this token in your build.gradle files, but rather set an environment
+    // variable and read it into this property.
+    authToken.set(System.getenv("SENTRY_AUTH_TOKEN"))
+
+    // The url of your Sentry instance. If you're using SAAS (not self hosting) you do not have to
+    // set this. If you are self hosting you can set your URL here
+    url = null
+
+    // Disables or enables the handling of Proguard mapping for Sentry.
+    // If enabled the plugin will generate a UUID and will take care of
+    // uploading the mapping to Sentry. If disabled, all the logic
+    // related to proguard mapping will be excluded.
+    // Default is enabled.
+    includeProguardMapping.set(true)
+
+    // Whether the plugin should attempt to auto-upload the mapping file to Sentry or not.
+    // If disabled the plugin will run a dry-run and just generate a UUID.
+    // The mapping file has to be uploaded manually via sentry-cli in this case.
+    // Default is enabled.
+    autoUploadProguardMapping.set(true)
+
+    // Experimental flag to turn on support for GuardSquare's tools integration (Dexguard and External Proguard).
+    // If enabled, the plugin will try to consume and upload the mapping file produced by Dexguard and External Proguard.
+    // Default is disabled.
+    dexguardEnabled.set(false)
+
+    // Disables or enables the automatic configuration of Native Symbols
+    // for Sentry. This executes sentry-cli automatically so
+    // you don't need to do it manually.
+    // Default is disabled.
+    uploadNativeSymbols.set(false)
+
+    // Whether the plugin should attempt to auto-upload the native debug symbols to Sentry or not.
+    // If disabled the plugin will run a dry-run.
+    // Default is enabled.
+    autoUploadNativeSymbols.set(true)
+
+    // Does or doesn't include the source code of native code for Sentry.
+    // This executes sentry-cli with the --include-sources param. automatically so
+    // you don't need to do it manually.
+    // Default is disabled.
+    includeNativeSources.set(false)
+
+    // Generates a JVM (Java, Kotlin, etc.) source bundle and uploads your source code to Sentry.
+    // This enables source context, allowing you to see your source
+    // code as part of your stack traces in Sentry.
+    includeSourceContext.set(true)
+
+    // Configure additional directories to be included in the source bundle which is used for
+    // source context. The directories should be specified relative to the Gradle module/project's
+    // root. For example, if you have a custom source set alongside 'main', the parameter would be
+    // 'src/custom/java'.
+    additionalSourceDirsForSourceContext.set(emptySet())
+
+    // Enable auto-installation of Sentry components (sentry-android SDK and okhttp, timber, fragment and compose integrations).
+    // Default is enabled.
+    // Only available v3.1.0 and above.
+    autoInstallation {
+        enabled.set(true)
+
+        // Specifies a version of the sentry-android SDK and fragment, timber and okhttp integrations.
+        //
+        // This is also useful, when you have the sentry-android SDK already included into a transitive dependency/module and want to
+        // align integration versions with it (if it's a direct dependency, the version will be inferred).
+        //
+        // NOTE: if you have a higher version of the sentry-android SDK or integrations on the classpath, this setting will have no effect
+        // as Gradle will resolve it to the latest version.
+        //
+        // Defaults to the latest published Sentry version.
+        // sentryVersion.set("8.18.0") // use default
+    }
+
+    // Disables or enables dependencies metadata reporting for Sentry.
+    // If enabled, the plugin will collect external dependencies and
+    // upload them to Sentry as part of events. If disabled, all the logic
+    // related to the dependencies metadata report will be excluded.
+    //
+    // Default is enabled.
+    //
+    includeDependenciesReport.set(true)
+
+    // Whether the plugin should send telemetry data to Sentry.
+    // If disabled the plugin won't send telemetry data.
+    // This is auto disabled if running against a self hosted instance of Sentry.
+    // Default is enabled.
+    telemetry.set(true)
+
+    if (System.getenv("USE_MAPLIBRE_SENTRY")?.toBoolean() == true) {
+        ignoredBuildTypes.set(setOf("debug"))
+    } else {
+        ignoredBuildTypes.set(setOf("debug", "release"))
+    }
 }
