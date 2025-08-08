d2e9b99e6a0098cae00dc1711b71744c304ef306
Bart Louwers <bart.louwers@gmail.com>
2025-08-08 15:34:44 +0200

    Apply suggestions from code review

--- include/mbgl/style/style.hpp
+++ include/mbgl/style/style.hpp
@@ -71,7 +71,6 @@ public:
     void addLayer(std::unique_ptr<Layer>, const std::optional<std::string>& beforeLayerID = std::nullopt);
     std::unique_ptr<Layer> removeLayer(const std::string& layerID);
 
-    // Style loading control
     void cancelPendingRequests();
 
     // Private implementation
