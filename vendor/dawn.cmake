4f358e51dee216f404a5c7548e2b5ca8ae474ba7
Birk Skyum <birk.skyum@pm.me>
2025-09-11 02:38:30 +0200

    fix dawn linking

--- vendor/dawn.cmake
+++ vendor/dawn.cmake
@@ -5,34 +5,39 @@ if(MLN_WITH_WEBGPU)
 
     # Dawn library paths
     set(DAWN_DIR "/Users/admin/repos/dawn")
-    set(DAWN_BUILD_DIR "${DAWN_DIR}/out/Release")
+    set(DAWN_BUILD_DIR "${DAWN_DIR}/build")
 
     # Add Dawn include directories
     target_include_directories(mbgl-core
         PUBLIC
             ${DAWN_DIR}/include
-            ${DAWN_DIR}/out/Release/gen/include
+            ${DAWN_BUILD_DIR}/gen/include
     )
+
+    # Find all Dawn libraries we need
+    find_library(DAWN_NATIVE_LIB dawn_native PATHS ${DAWN_BUILD_DIR}/src/dawn/native NO_DEFAULT_PATH)
+    find_library(DAWN_PROC_LIB dawn_proc PATHS ${DAWN_BUILD_DIR}/src/dawn NO_DEFAULT_PATH)
+    find_library(DAWN_PLATFORM_LIB dawn_platform PATHS ${DAWN_BUILD_DIR}/src/dawn/platform NO_DEFAULT_PATH)
+    find_library(DAWN_COMMON_LIB dawn_common PATHS ${DAWN_BUILD_DIR}/src/dawn/common NO_DEFAULT_PATH)
+    find_library(DAWN_WIRE_LIB dawn_wire PATHS ${DAWN_BUILD_DIR}/src/dawn/wire NO_DEFAULT_PATH)
 
     # Link Dawn libraries
     target_link_libraries(mbgl-core
         PUBLIC
-            ${DAWN_BUILD_DIR}/src/dawn/native/libwebgpu_dawn.a
             ${DAWN_BUILD_DIR}/src/dawn/native/libdawn_native.a
             ${DAWN_BUILD_DIR}/src/dawn/libdawn_proc.a
             ${DAWN_BUILD_DIR}/src/dawn/platform/libdawn_platform.a
             ${DAWN_BUILD_DIR}/src/dawn/common/libdawn_common.a
-            ${DAWN_BUILD_DIR}/src/tint/libtint_api.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/base/libabsl_base.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/base/libabsl_raw_logging_internal.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/base/libabsl_log_severity.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/container/libabsl_raw_hash_set.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/container/libabsl_hashtablez_sampler.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/numeric/libabsl_int128.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/strings/libabsl_strings.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/strings/libabsl_strings_internal.a
-            ${DAWN_BUILD_DIR}/third_party/abseil-cpp/absl/types/libabsl_bad_optional_access.a
+            ${DAWN_BUILD_DIR}/src/dawn/wire/libdawn_wire.a
     )
+
+    # Link Tint libraries
+    file(GLOB TINT_LIBS "${DAWN_BUILD_DIR}/src/tint/*.a")
+    target_link_libraries(mbgl-core PUBLIC ${TINT_LIBS})
+
+    # Link abseil libraries
+    file(GLOB_RECURSE ABSEIL_LIBS "${DAWN_BUILD_DIR}/third_party/abseil/*.a")
+    target_link_libraries(mbgl-core PUBLIC ${ABSEIL_LIBS})
 
     # Platform-specific libraries
     if(APPLE)
