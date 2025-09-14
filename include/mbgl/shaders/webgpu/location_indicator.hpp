0899fa17a516599eb977c5591c99865131079135
Birk Skyum <birk.skyum@pm.me>
2025-09-14 15:46:57 +0200

    fix missing types of shaders

--- include/mbgl/shaders/webgpu/location_indicator.hpp
+++ include/mbgl/shaders/webgpu/location_indicator.hpp
@@ -12,6 +12,44 @@ struct ShaderSource<BuiltIn::LocationIndicatorShader, gfx::Backend::Type::WebGPU
     static const std::array<AttributeInfo, 1> attributes;
     static constexpr std::array<AttributeInfo, 0> instanceAttributes{};
     static const std::array<TextureInfo, 0> textures;
+
+    static constexpr auto vertex = R"(
+struct VertexInput {
+    @location(0) position: vec2<f32>,
+};
+
+struct VertexOutput {
+    @builtin(position) position: vec4<f32>,
+};
+
+struct LocationIndicatorUBO {
+    matrix: mat4x4<f32>,
+    color: vec4<f32>,
+};
+
+@group(0) @binding(0) var<uniform> ubo: LocationIndicatorUBO;
+
+@vertex
+fn main(in: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+    out.position = ubo.matrix * vec4<f32>(in.position, 0.0, 1.0);
+    return out;
+}
+)";
+
+    static constexpr auto fragment = R"(
+struct LocationIndicatorUBO {
+    matrix: mat4x4<f32>,
+    color: vec4<f32>,
+};
+
+@group(0) @binding(0) var<uniform> ubo: LocationIndicatorUBO;
+
+@fragment
+fn main(@builtin(position) position: vec4<f32>) -> @location(0) vec4<f32> {
+    return ubo.color;
+}
+)";
 };
 
 template <>
@@ -20,6 +58,50 @@ struct ShaderSource<BuiltIn::LocationIndicatorTexturedShader, gfx::Backend::Type
     static const std::array<AttributeInfo, 2> attributes;
     static constexpr std::array<AttributeInfo, 0> instanceAttributes{};
     static const std::array<TextureInfo, 1> textures;
+
+    static constexpr auto vertex = R"(
+struct VertexInput {
+    @location(0) position: vec2<f32>,
+    @location(1) uv: vec2<f32>,
+};
+
+struct VertexOutput {
+    @builtin(position) position: vec4<f32>,
+    @location(0) uv: vec2<f32>,
+};
+
+struct LocationIndicatorUBO {
+    matrix: mat4x4<f32>,
+    color: vec4<f32>,
+};
+
+@group(0) @binding(0) var<uniform> ubo: LocationIndicatorUBO;
+
+@vertex
+fn main(in: VertexInput) -> VertexOutput {
+    var out: VertexOutput;
+    out.position = ubo.matrix * vec4<f32>(in.position, 0.0, 1.0);
+    out.uv = in.uv;
+    return out;
+}
+)";
+
+    static constexpr auto fragment = R"(
+struct LocationIndicatorUBO {
+    matrix: mat4x4<f32>,
+    color: vec4<f32>,
+};
+
+@group(0) @binding(0) var<uniform> ubo: LocationIndicatorUBO;
+@group(0) @binding(1) var colorTexture: texture_2d<f32>;
+@group(0) @binding(2) var colorSampler: sampler;
+
+@fragment
+fn main(@location(0) uv: vec2<f32>) -> @location(0) vec4<f32> {
+    let textureColor = textureSample(colorTexture, colorSampler, uv);
+    return textureColor * ubo.color;
+}
+)";
 };
 
 } // namespace shaders
