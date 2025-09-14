77a2abc05d5f8f58b462299e3c0523ac595b4136
Birk Skyum <birk.skyum@pm.me>
2025-09-14 21:16:37 +0200

    update shaders

--- src/mbgl/shaders/webgpu/line.cpp
+++ src/mbgl/shaders/webgpu/line.cpp
@@ -7,18 +7,29 @@ namespace shaders {
 
 using LineShaderSource = ShaderSource<BuiltIn::LineShader, gfx::Backend::Type::WebGPU>;
 
-const std::array<AttributeInfo, 2> LineShaderSource::attributes = {
-    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Float4, idLinePosNormalVertexAttribute},
-    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::Float4, idLineDataVertexAttribute},
+const std::array<AttributeInfo, 8> LineShaderSource::attributes = {
+    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Short2, idLinePosNormalVertexAttribute},
+    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::UByte4, idLineDataVertexAttribute},
+    AttributeInfo{lineUBOCount + 2, gfx::AttributeDataType::Float4, idLineColorVertexAttribute},
+    AttributeInfo{lineUBOCount + 3, gfx::AttributeDataType::Float2, idLineBlurVertexAttribute},
+    AttributeInfo{lineUBOCount + 4, gfx::AttributeDataType::Float2, idLineOpacityVertexAttribute},
+    AttributeInfo{lineUBOCount + 5, gfx::AttributeDataType::Float2, idLineGapWidthVertexAttribute},
+    AttributeInfo{lineUBOCount + 6, gfx::AttributeDataType::Float2, idLineOffsetVertexAttribute},
+    AttributeInfo{lineUBOCount + 7, gfx::AttributeDataType::Float2, idLineWidthVertexAttribute},
 };
 const std::array<TextureInfo, 0> LineShaderSource::textures = {};
 
 // Line Gradient
 using LineGradientShaderSource = ShaderSource<BuiltIn::LineGradientShader, gfx::Backend::Type::WebGPU>;
 
-const std::array<AttributeInfo, 2> LineGradientShaderSource::attributes = {
-    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Float4, idLinePosNormalVertexAttribute},
-    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::Float4, idLineDataVertexAttribute},
+const std::array<AttributeInfo, 7> LineGradientShaderSource::attributes = {
+    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Short2, idLinePosNormalVertexAttribute},
+    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::UByte4, idLineDataVertexAttribute},
+    AttributeInfo{lineUBOCount + 2, gfx::AttributeDataType::Float2, idLineBlurVertexAttribute},
+    AttributeInfo{lineUBOCount + 3, gfx::AttributeDataType::Float2, idLineOpacityVertexAttribute},
+    AttributeInfo{lineUBOCount + 4, gfx::AttributeDataType::Float2, idLineGapWidthVertexAttribute},
+    AttributeInfo{lineUBOCount + 5, gfx::AttributeDataType::Float2, idLineOffsetVertexAttribute},
+    AttributeInfo{lineUBOCount + 6, gfx::AttributeDataType::Float2, idLineWidthVertexAttribute},
 };
 const std::array<TextureInfo, 1> LineGradientShaderSource::textures = {
     TextureInfo{0, idLineImageTexture}
@@ -27,9 +38,16 @@ const std::array<TextureInfo, 1> LineGradientShaderSource::textures = {
 // Line Pattern
 using LinePatternShaderSource = ShaderSource<BuiltIn::LinePatternShader, gfx::Backend::Type::WebGPU>;
 
-const std::array<AttributeInfo, 2> LinePatternShaderSource::attributes = {
-    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Float4, idLinePosNormalVertexAttribute},
-    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::Float4, idLineDataVertexAttribute},
+const std::array<AttributeInfo, 9> LinePatternShaderSource::attributes = {
+    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Short2, idLinePosNormalVertexAttribute},
+    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::UByte4, idLineDataVertexAttribute},
+    AttributeInfo{lineUBOCount + 2, gfx::AttributeDataType::Float2, idLineBlurVertexAttribute},
+    AttributeInfo{lineUBOCount + 3, gfx::AttributeDataType::Float2, idLineOpacityVertexAttribute},
+    AttributeInfo{lineUBOCount + 4, gfx::AttributeDataType::Float2, idLineGapWidthVertexAttribute},
+    AttributeInfo{lineUBOCount + 5, gfx::AttributeDataType::Float2, idLineOffsetVertexAttribute},
+    AttributeInfo{lineUBOCount + 6, gfx::AttributeDataType::Float2, idLineWidthVertexAttribute},
+    AttributeInfo{lineUBOCount + 7, gfx::AttributeDataType::UShort4, idLinePatternFromVertexAttribute},
+    AttributeInfo{lineUBOCount + 8, gfx::AttributeDataType::UShort4, idLinePatternToVertexAttribute},
 };
 const std::array<TextureInfo, 1> LinePatternShaderSource::textures = {
     TextureInfo{0, idLineImageTexture}
@@ -38,9 +56,16 @@ const std::array<TextureInfo, 1> LinePatternShaderSource::textures = {
 // Line SDF
 using LineSDFShaderSource = ShaderSource<BuiltIn::LineSDFShader, gfx::Backend::Type::WebGPU>;
 
-const std::array<AttributeInfo, 2> LineSDFShaderSource::attributes = {
-    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Float4, idLinePosNormalVertexAttribute},
-    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::Float4, idLineDataVertexAttribute},
+const std::array<AttributeInfo, 9> LineSDFShaderSource::attributes = {
+    AttributeInfo{lineUBOCount + 0, gfx::AttributeDataType::Short2, idLinePosNormalVertexAttribute},
+    AttributeInfo{lineUBOCount + 1, gfx::AttributeDataType::UByte4, idLineDataVertexAttribute},
+    AttributeInfo{lineUBOCount + 2, gfx::AttributeDataType::Float4, idLineColorVertexAttribute},
+    AttributeInfo{lineUBOCount + 3, gfx::AttributeDataType::Float2, idLineBlurVertexAttribute},
+    AttributeInfo{lineUBOCount + 4, gfx::AttributeDataType::Float2, idLineOpacityVertexAttribute},
+    AttributeInfo{lineUBOCount + 5, gfx::AttributeDataType::Float2, idLineGapWidthVertexAttribute},
+    AttributeInfo{lineUBOCount + 6, gfx::AttributeDataType::Float2, idLineOffsetVertexAttribute},
+    AttributeInfo{lineUBOCount + 7, gfx::AttributeDataType::Float2, idLineWidthVertexAttribute},
+    AttributeInfo{lineUBOCount + 8, gfx::AttributeDataType::Float2, idLineFloorWidthVertexAttribute},
 };
 const std::array<TextureInfo, 1> LineSDFShaderSource::textures = {
     TextureInfo{0, idLineImageTexture}
