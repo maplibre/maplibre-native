c07165580ca7f70c048f92253b22b3e473aeb30e
“Malcolm <mtoon@atlasprogramming.com>
2025-09-14 09:33:29 -0400

    Updates

--- platform/darwin/include/mbgl/interface/native_apple_interface.h
+++ platform/darwin/include/mbgl/interface/native_apple_interface.h
@@ -8,7 +8,7 @@ NS_ASSUME_NONNULL_BEGIN
     
 @property (retain, nullable) NSError *error;
 @property (retain, nullable) NSData *data;
-@property NSURLResponse *response;
+@property (retain, nullable) NSURLResponse *response;
 
 +(NetworkResponse *)responseWithData:(NSData *)data
                          urlResponse:(NSURLResponse *)response
