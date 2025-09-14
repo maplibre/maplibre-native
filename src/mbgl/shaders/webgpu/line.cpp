cb23e0255f139de06e9393bf6db0c717181dd87b
Birk Skyum <birk.skyum@pm.me>
2025-09-14 15:46:57 +0200

    fix missing types of shaders

--- src/mbgl/shaders/webgpu/line.cpp
+++ src/mbgl/shaders/webgpu/line.cpp
@@ -13,5 +13,38 @@ const std::array<AttributeInfo, 2> LineShaderSource::attributes = {
 };
 const std::array<TextureInfo, 0> LineShaderSource::textures = {};
 
+// Line Gradient
+using LineGradientShaderSource = ShaderSource<BuiltIn::LineGradientShader, gfx::Backend::Type::WebGPU>;
+
+const std::array<AttributeInfo, 2> LineGradientShaderSource::attributes = {
+    AttributeInfo{0, gfx::AttributeDataType::Float4, idLinePosNormalVertexAttribute},
+    AttributeInfo{1, gfx::AttributeDataType::Float4, idLineDataVertexAttribute},
+};
+const std::array<TextureInfo, 1> LineGradientShaderSource::textures = {
+    TextureInfo{0, idLineImageTexture}
+};
+
+// Line Pattern
+using LinePatternShaderSource = ShaderSource<BuiltIn::LinePatternShader, gfx::Backend::Type::WebGPU>;
+
+const std::array<AttributeInfo, 2> LinePatternShaderSource::attributes = {
+    AttributeInfo{0, gfx::AttributeDataType::Float4, idLinePosNormalVertexAttribute},
+    AttributeInfo{1, gfx::AttributeDataType::Float4, idLineDataVertexAttribute},
+};
+const std::array<TextureInfo, 1> LinePatternShaderSource::textures = {
+    TextureInfo{0, idLineImageTexture}
+};
+
+// Line SDF
+using LineSDFShaderSource = ShaderSource<BuiltIn::LineSDFShader, gfx::Backend::Type::WebGPU>;
+
+const std::array<AttributeInfo, 2> LineSDFShaderSource::attributes = {
+    AttributeInfo{0, gfx::AttributeDataType::Float4, idLinePosNormalVertexAttribute},
+    AttributeInfo{1, gfx::AttributeDataType::Float4, idLineDataVertexAttribute},
+};
+const std::array<TextureInfo, 1> LineSDFShaderSource::textures = {
+    TextureInfo{0, idLineImageTexture}
+};
+
 } // namespace shaders
 } // namespace mbgl
