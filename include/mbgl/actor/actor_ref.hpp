1053740731b666f44001f5b4bd3014f65c753343
Birk Skyum <birk.skyum@pm.me>
2025-09-15 09:10:34 +0200

    reset

--- include/mbgl/actor/actor_ref.hpp
+++ include/mbgl/actor/actor_ref.hpp
@@ -32,9 +32,6 @@ public:
     void invoke(Fn fn, Args&&... args) const {
         if (auto mailbox = weakMailbox.lock()) {
             mailbox->push(actor::makeMessage(*object, fn, std::forward<Args>(args)...));
-        } else {
-            // Log when mailbox is not available
-            // Note: Can't use Log here as it would create circular dependency
         }
     }
 
