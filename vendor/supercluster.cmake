2ec1d77ffac26c271bcd50486b342ccd4f51b156
Bart Louwers <bart@emeel.net>
2025-09-23 13:16:46 +0200

    rename mbgl-vendor-supercluster

--- vendor/supercluster.cmake
+++ vendor/supercluster.cmake
@@ -1,16 +1,16 @@
-if(TARGET mbgl-vendor-supercluster.hpp)
+if(TARGET mbgl-vendor-supercluster)
     return()
 endif()
 
-add_library(mbgl-vendor-supercluster.hpp INTERFACE)
+add_library(mbgl-vendor-supercluster INTERFACE)
 
 target_include_directories(
-    mbgl-vendor-supercluster.hpp SYSTEM
+    mbgl-vendor-supercluster SYSTEM
     INTERFACE ${CMAKE_CURRENT_LIST_DIR}/supercluster/include
 )
 
 set_target_properties(
-    mbgl-vendor-supercluster.hpp
+    mbgl-vendor-supercluster
     PROPERTIES
         INTERFACE_MAPLIBRE_NAME "supercluster.hpp"
         INTERFACE_MAPLIBRE_URL "https://github.com/mapbox/supercluster.hpp"
