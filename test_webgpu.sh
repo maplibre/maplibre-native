083a5695230f8d063e314bd83a45f53776a1c255
Birk Skyum <birk.skyum@pm.me>
2025-09-24 02:32:48 +0200

    remove test files

--- test_webgpu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run WebGPU for 3 seconds and capture output
-timeout 3 ./build/platform/glfw/mbgl-glfw --style https://demotiles.maplibre.org/style.json --benchmark --zoom 2 2>&1 | tee /tmp/webgpu_output.log
