9d5f0e42eab181f75eb489b3119b19f649f1a832
Bart Louwers <bart@emeel.net>
2025-10-14 23:58:55 +0200

    Improve CMake config Harfbuzz and Freetype

--- vendor/harfbuzz.cmake
+++ vendor/harfbuzz.cmake
@@ -1,22 +1,26 @@
-if(TARGET harfbuzz)
+if(TARGET mbgl-harfbuzz)
     return()
 endif()
 if (MLN_TEXT_SHAPING_HARFBUZZ)
-    add_subdirectory(vendor/harfbuzz SYSTEM)
+    add_library(mbgl-harfbuzz STATIC
+        ${CMAKE_CURRENT_LIST_DIR}/harfbuzz/src/harfbuzz.cc
+    )
 
-    target_compile_definitions(harfbuzz PRIVATE -DHAVE_FREETYPE)
+    target_include_directories(mbgl-harfbuzz
+        PUBLIC
+            ${CMAKE_CURRENT_LIST_DIR}/harfbuzz/src
+    )
+
+    target_link_libraries(mbgl-harfbuzz PRIVATE mbgl-freetype)
+    target_include_directories(mbgl-harfbuzz PRIVATE ${CMAKE_CURRENT_LIST_DIR}/freetype/include)
+    target_compile_definitions(mbgl-harfbuzz PRIVATE -DHAVE_FREETYPE)
 
     set_target_properties(
-        harfbuzz
+        mbgl-harfbuzz
         PROPERTIES
             INTERFACE_MAPLIBRE_NAME "harfbuzz"
             INTERFACE_MAPLIBRE_URL "https://github.com/harfbuzz/harfbuzz"
-            INTERFACE_MAPLIBRE_AUTHOR ${PROJECT_SOURCE_DIR}/vendor/harfbuzz/AUTHORS
-            INTERFACE_MAPLIBRE_LICENSE ${PROJECT_SOURCE_DIR}/vendor/harfbuzz/COPYING
-    )
-
-    target_include_directories(
-        mbgl-core SYSTEM
-        PUBLIC vendor/harfbuzz/src
+            INTERFACE_MAPLIBRE_AUTHOR ${CMAKE_CURRENT_LIST_DIR}/harfbuzz/AUTHORS
+            INTERFACE_MAPLIBRE_LICENSE ${CMAKE_CURRENT_LIST_DIR}/harfbuzz/COPYING
     )
 endif()
