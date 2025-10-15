82a6a7e0b6ad2ab448baf221c75c92997e2cf8e1
Birk Skyum <birk.skyum@pm.me>
2025-10-15 23:48:27 +0200

    move inline shader_group functions to cpp file

--- include/mbgl/shaders/webgpu/shader_group.hpp
+++ include/mbgl/shaders/webgpu/shader_group.hpp
@@ -7,11 +7,8 @@
 #include <mbgl/shaders/shader_source.hpp>
 #include <mbgl/util/hash.hpp>
 #include <mbgl/util/containers.hpp>
+#include <mbgl/webgpu/context.hpp>
 
-#include <cctype>
-#include <numeric>
-#include <sstream>
-#include <stack>
 #include <string>
 #include <type_traits>
 #include <unordered_map>
@@ -21,106 +18,9 @@ namespace webgpu {
 
 namespace detail {
 
-inline bool isDirective(const std::string& line, const std::string& directive) {
-    std::size_t pos = 0;
-    while (pos < line.size() && std::isspace(static_cast<unsigned char>(line[pos]))) {
-        ++pos;
-    }
-    if (pos >= line.size() || line[pos] != '#') {
-        return false;
-    }
-    ++pos;
-    while (pos < line.size() && std::isspace(static_cast<unsigned char>(line[pos]))) {
-        ++pos;
-    }
-    if (line.compare(pos, directive.size(), directive) != 0) {
-        return false;
-    }
-    pos += directive.size();
-    return pos == line.size() || std::isspace(static_cast<unsigned char>(line[pos]));
-}
-
-inline std::string getDirectiveArgument(const std::string& line) {
-    auto hash = line.find('#');
-    if (hash == std::string::npos) {
-        return {};
-    }
-    auto pos = line.find_first_not_of(" \t", hash + 1);
-    pos = line.find_first_of(" \t", pos);
-    if (pos == std::string::npos) {
-        return {};
-    }
-    pos = line.find_first_not_of(" \t", pos);
-    if (pos == std::string::npos) {
-        return {};
-    }
-    const auto end = line.find_first_of(" \t\r\n", pos);
-    return line.substr(pos, end == std::string::npos ? std::string::npos : end - pos);
-}
-
-inline std::string preprocessWGSL(const std::string& source, const std::unordered_map<std::string, bool>& defines) {
-    std::stringstream input(source);
-    std::string line;
-    std::string output;
-    output.reserve(source.size());
-
-    struct ConditionalFrame {
-        bool parentActive;
-        bool condition;
-        bool elseSeen;
-    };
-
-    std::stack<ConditionalFrame> stack;
-    bool currentActive = true;
-
-    while (std::getline(input, line)) {
-        if (isDirective(line, "ifdef")) {
-            const auto symbol = getDirectiveArgument(line);
-            const bool condition = defines.contains(symbol);
-            stack.push(ConditionalFrame{currentActive, condition, false});
-            currentActive = currentActive && condition;
-            continue;
-        }
-
-        if (isDirective(line, "ifndef")) {
-            const auto symbol = getDirectiveArgument(line);
-            const bool condition = !defines.contains(symbol);
-            stack.push(ConditionalFrame{currentActive, condition, false});
-            currentActive = currentActive && condition;
-            continue;
-        }
-
-        if (isDirective(line, "else")) {
-            if (stack.empty()) {
-                continue;
-            }
-            auto& frame = stack.top();
-            if (!frame.elseSeen) {
-                frame.elseSeen = true;
-                currentActive = frame.parentActive && !frame.condition;
-            } else {
-                currentActive = false;
-            }
-            continue;
-        }
-
-        if (isDirective(line, "endif")) {
-            if (stack.empty()) {
-                continue;
-            }
-            currentActive = stack.top().parentActive;
-            stack.pop();
-            continue;
-        }
-
-        if (currentActive) {
-            output.append(line);
-            output.push_back('\n');
-        }
-    }
-
-    return output;
-}
+bool isDirective(const std::string& line, const std::string& directive);
+std::string getDirectiveArgument(const std::string& line);
+std::string preprocessWGSL(const std::string& source, const std::unordered_map<std::string, bool>& defines);
 
 } // namespace detail
 
