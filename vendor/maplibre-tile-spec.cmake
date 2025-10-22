163cdd85a35341073d01a7f4a40c08f77c6b0ada
Tim Sylvester <tim@wetdogweather.com>
2025-10-22 13:41:29 -0700

    Disable tests in MLT to avoid conflicts with Dawn

--- vendor/maplibre-tile-spec.cmake
+++ vendor/maplibre-tile-spec.cmake
@@ -1,6 +1,8 @@
 set(MLT_WITH_JSON OFF CACHE BOOL "No JSON support" FORCE)
 set(MLT_WITH_PROTOZERO OFF CACHE BOOL "No protozero" FORCE)
 set(MLT_WITH_FASTPFOR OFF CACHE BOOL "disabled for lack of support for ARMv7 in SIMDE and requirement for SSE on x86" FORCE)
+set(MLT_WITH_TESTS OFF CACHE BOOL "Google Test conflicts with Dawn")
+set(MLT_WITH_TOOLS OFF CACHE BOOL "Not used")
 
 # This is set by the platform CMakeLists.txt after vendor libraries.  If it was set earlier, we could just use it.
 set(CMAKE_OSX_DEPLOYMENT_TARGET 14.3)
