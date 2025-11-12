951ac20175335905876b45f88ebee37d7d184ee8
“Malcolm <mtoon@atlasprogramming.com>
2025-11-12 09:12:50 -0500

    Fix for android build issue

--- platform/android/MapLibreAndroid/src/cpp/native_map_view.cpp
+++ platform/android/MapLibreAndroid/src/cpp/native_map_view.cpp
@@ -1550,7 +1550,7 @@ void NativeMapView::registerNative(jni::JNIEnv& env) {
         METHOD(&NativeMapView::setFrustumOffset, "nativeSetFrustumOffset"),
         METHOD(&NativeMapView::isRenderingStatsViewEnabled, "nativeIsRenderingStatsViewEnabled"),
         METHOD(&NativeMapView::enableRenderingStatsView, "nativeEnableRenderingStatsView"),
-        METHOD(&NativeMapView::addPluginFileSource, "nativeAddPluginFileSource")
+        METHOD(&NativeMapView::addPluginFileSource, "nativeAddPluginFileSource"));
 }
 
 void NativeMapView::onRegisterShaders(gfx::ShaderRegistry&) {};
