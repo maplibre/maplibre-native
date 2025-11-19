c5b3d681c2f62bcd4eb4318420a88017330010d4
“Malcolm <mtoon@atlasprogramming.com>
2025-11-19 13:04:57 -0500

    Initial Style Preprocess Implementation

---
+++ src/mbgl/plugin/plugin_style_filter.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <functional>
+
+
+namespace mbgl {
+
+namespace style {
+
+class PluginStyleFilter {
+public:
+    
+    using OnFilterStyle = std::function<const std::string(const std::string&)>;
+
+    OnFilterStyle _filterStyleFunction;
+    
+    // This method
+    const std::string filterResponse(const std::string&);
+};
+
+
+}
+}
