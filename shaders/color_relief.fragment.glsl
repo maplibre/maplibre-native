72c4ee5a29ac6f883c142c63c6d7b264ff5f6cf0
acalcutt <acalcutt@techidiots.net>
2025-11-22 13:44:55 -0500

    start adding color-relief

---
+++ shaders/color_relief.fragment.glsl
@@ -0,0 +1,65 @@
+layout(std140) uniform ColorReliefDrawableUBO {
+    highp mat4 u_matrix;
+};
+
+layout(std140) uniform ColorReliefTilePropsUBO {
+    highp vec4 u_unpack;
+    highp vec2 u_dimension;
+    int u_color_ramp_size;
+    float pad0;
+};
+
+layout(std140) uniform ColorReliefEvaluatedPropsUBO {
+    float u_opacity;
+    float pad0;
+    float pad1;
+    float pad2;
+};
+
+uniform sampler2D u_image;
+uniform sampler2D u_elevation_stops;
+uniform sampler2D u_color_stops;
+
+in vec2 v_pos;
+
+float getElevation(vec2 coord) {
+    vec4 data = texture(u_image, coord) * 255.0;
+    data.a = -1.0;
+    return dot(data, u_unpack);
+}
+
+float getElevationStop(int stop) {
+    float x = (float(stop) + 0.5) / float(u_color_ramp_size);
+    vec4 data = texture(u_elevation_stops, vec2(x, 0.0)) * 255.0;
+    data.a = -1.0;
+    return dot(data, u_unpack);
+}
+
+void main() {
+    float el = getElevation(v_pos);
+    
+    // Binary search for color stops
+    int r = (u_color_ramp_size - 1);
+    int l = 0;
+    float el_l = getElevationStop(l);
+    float el_r = getElevationStop(r);
+    
+    while(r - l > 1) {
+        int m = (r + l) / 2;
+        float el_m = getElevationStop(m);
+        if(el < el_m) {
+            r = m;
+            el_r = el_m;
+        } else {
+            l = m;
+            el_l = el_m;
+        }
+    }
+    
+    float x = (float(l) + (el - el_l) / (el_r - el_l) + 0.5) / float(u_color_ramp_size);
+    fragColor = u_opacity * texture(u_color_stops, vec2(x, 0.0));
+    
+#ifdef OVERDRAW_INSPECTOR
+    fragColor = vec4(1.0);
+#endif
+}
