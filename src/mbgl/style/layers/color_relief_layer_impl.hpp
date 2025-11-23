5825a8cecc94e22fe452577f017e39568b7a40b5
acalcutt <acalcutt@techidiots.net>
2025-11-23 02:20:25 -0500

    Update color_relief_layer_impl.hpp

--- src/mbgl/style/layers/color_relief_layer_impl.hpp
+++ src/mbgl/style/layers/color_relief_layer_impl.hpp
@@ -1,23 +1,15 @@
-#pragma once
-
-#include <mbgl/style/layer_impl.hpp>
-#include <mbgl/style/layers/color_relief_layer.hpp>
-#include <mbgl/style/layers/color_relief_layer_properties.hpp>
+#include <mbgl/style/layers/color_relief_layer_impl.hpp>
 
 namespace mbgl {
 namespace style {
 
-class ColorReliefLayer::Impl : public Layer::Impl {
-public:
-    using Layer::Impl::Impl;
+bool ColorReliefLayer::Impl::hasLayoutDifference(const Layer::Impl&) const {
+    return false;
+}
 
-    bool hasLayoutDifference(const Layer::Impl&) const override;
-    void stringifyLayout(rapidjson::Writer<rapidjson::StringBuffer>&) const override;
-
-    ColorReliefPaintProperties::Transitionable paint;
-    
-    DECLARE_LAYER_TYPE_INFO;
-};
+void ColorReliefLayer::Impl::stringifyLayout(rapidjson::Writer<rapidjson::StringBuffer>&) const {
+    // No layout properties for color-relief
+}
 
 } // namespace style
 } // namespace mbgl
