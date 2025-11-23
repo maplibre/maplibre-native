a00c93b24771e3b3cd1bbac6edc64c63ec910f1a
acalcutt <acalcutt@techidiots.net>
2025-11-23 01:48:51 -0500

    Update color_relief_layer_impl.cpp

--- src/mbgl/style/layers/color_relief_layer_impl.cpp
+++ src/mbgl/style/layers/color_relief_layer_impl.cpp
@@ -3,6 +3,10 @@
 namespace mbgl {
 namespace style {
 
+const LayerTypeInfo* ColorReliefLayer::Impl::getTypeInfo() const noexcept {
+    return ColorReliefLayer::Impl::staticTypeInfo();
+}
+
 bool ColorReliefLayer::Impl::hasLayoutDifference(const Layer::Impl&) const {
     return false;
 }
