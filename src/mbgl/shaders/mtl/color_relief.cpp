12c9b394b74a8bf5a74019323bdb6b55f5ec4c48
acalcutt <acalcutt@techidiots.net>
2025-12-08 00:30:01 -0500

    Update color_relief.cpp

--- src/mbgl/shaders/mtl/color_relief.cpp
+++ src/mbgl/shaders/mtl/color_relief.cpp
@@ -6,6 +6,9 @@ namespace shaders {
 
 using ColorReliefShaderSource = ShaderSource<BuiltIn::ColorReliefShader, gfx::Backend::Type::Metal>;
 
+// Note: Attribute indices must match the Metal shader prelude enum values.
+// In Metal, colorReliefUBOCount = 5 (based on common.hpp base values), so attributes are at 5 and 6.
+// This differs from shader_defines.hpp where colorReliefUBOCount = 4.
 const std::array<AttributeInfo, 2> ColorReliefShaderSource::attributes = {
     AttributeInfo{5, gfx::AttributeDataType::Short2, idColorReliefPosVertexAttribute},
     AttributeInfo{6, gfx::AttributeDataType::Short2, idColorReliefTexturePosVertexAttribute},
