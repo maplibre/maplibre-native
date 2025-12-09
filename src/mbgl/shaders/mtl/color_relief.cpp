a6c7cfe16d0d8bb32715b642b225b9895dd682c9
acalcutt <acalcutt@techidiots.net>
2025-12-08 19:03:57 -0500

    Update color_relief.cpp

--- src/mbgl/shaders/mtl/color_relief.cpp
+++ src/mbgl/shaders/mtl/color_relief.cpp
@@ -1,18 +1,16 @@
 #include <mbgl/shaders/mtl/color_relief.hpp>
 #include <mbgl/shaders/shader_defines.hpp>
+#include <mbgl/shaders/mtl/shader_program.hpp>
 
 namespace mbgl {
 namespace shaders {
 
 using ColorReliefShaderSource = ShaderSource<BuiltIn::ColorReliefShader, gfx::Backend::Type::Metal>;
 
-// Note: Attribute indices must match the Metal shader prelude enum values.
-// In Metal, colorReliefUBOCount = 6 (based on common.hpp base values), so attributes are at 6 and 7.
 const std::array<AttributeInfo, 2> ColorReliefShaderSource::attributes = {
-    AttributeInfo{6, gfx::AttributeDataType::Short2, idColorReliefPosVertexAttribute},
-    AttributeInfo{7, gfx::AttributeDataType::Short2, idColorReliefTexturePosVertexAttribute},
+    AttributeInfo{colorReliefUBOCount + 0, gfx::AttributeDataType::Short2, idColorReliefPosVertexAttribute},
+    AttributeInfo{colorReliefUBOCount + 1, gfx::AttributeDataType::Short2, idColorReliefTexturePosVertexAttribute},
 };
-
 const std::array<TextureInfo, 3> ColorReliefShaderSource::textures = {
     TextureInfo{0, idColorReliefImageTexture},
     TextureInfo{1, idColorReliefElevationStopsTexture},
