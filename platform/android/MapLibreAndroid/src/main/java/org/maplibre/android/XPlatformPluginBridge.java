4f22f6ad4ffd8ec4e884b0b0168e97750b6f5ecc
Bart Louwers <bart@emeel.net>
2025-12-10 13:10:53 +0100

    revert accidentely pushed file

--- platform/android/MapLibreAndroid/src/main/java/org/maplibre/android/XPlatformPluginBridge.java
+++ platform/android/MapLibreAndroid/src/main/java/org/maplibre/android/XPlatformPluginBridge.java
@@ -1,8 +1,8 @@
 package org.maplibre.android;
 
 public interface XPlatformPluginBridge {
-    /**
-     * Returns a pointer to a native XPlatformPlugin instance.
-     */
-    long getNativePlugin();
+  /**
+  * Returns a pointer to a native XPlatformPlugin instance.
+  */
+  long getNativePlugin();
 }
