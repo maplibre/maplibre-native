8c2b603355dc9ad65bee39690988b6db13b10d90
Bart Louwers <bart@emeel.net>
2026-01-28 18:07:16 +0100

    add [[maybe_unused]]

--- src/mbgl/util/instrumentation.cpp
+++ src/mbgl/util/instrumentation.cpp
@@ -2,7 +2,7 @@
 
 namespace mbgl::instrumentation {
 
-void setThreadName(const std::string &name) {
+void setThreadName([[maybe_unused]] const std::string &name) {
 #ifdef MLN_TRACY_ENABLE
     tracy::SetThreadName(name.c_str());
 #endif
