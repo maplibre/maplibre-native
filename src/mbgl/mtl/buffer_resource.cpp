091f49907b13fde5f0cb2d63dfa2f51abd0648a6
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2026-01-29 13:48:05 +0200

    Cleanup

--- src/mbgl/mtl/buffer_resource.cpp
+++ src/mbgl/mtl/buffer_resource.cpp
@@ -77,10 +77,6 @@ BufferResource BufferResource::clone() const {
 
 BufferResource& BufferResource::operator=(BufferResource&& other) noexcept {
     assert(&context == &other.context);
-    if (isValid()) {
-        context.renderingStats().numBuffers--;
-        context.renderingStats().memBuffers -= size;
-    }
 
     buffer = std::move(other.buffer);
     raw = std::move(other.raw);
