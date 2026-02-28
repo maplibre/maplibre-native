acba061e394c9fe9905a47f48d409bff15874024
birkskyum <74932975+birkskyum@users.noreply.github.com>
2026-02-28 15:25:41 +0000

    Deploying to gh-pages from @ maplibre/maplibre-native@57a14cf110170fa8ec8abfa48a33022aecfcee04 🚀

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-245f40a6.js',
+                path_to_root + 'searchindex-4c017c9e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
