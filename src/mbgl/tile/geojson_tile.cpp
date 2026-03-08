d506960e39a89803caafbd39d2b20886060fb9a0
buldogin-cloud <buldogin@gmail.com>
2026-03-08 10:03:40 +0200

    Fix: Missing data for single tiles when using GeoJsonSource

--- src/mbgl/tile/geojson_tile.cpp
+++ src/mbgl/tile/geojson_tile.cpp
@@ -23,8 +23,14 @@ void GeoJSONTile::updateData(std::shared_ptr<style::GeoJSONData> data_, bool nee
     MLN_TRACE_FUNC();
 
     assert(data_);
+    
+    // Reset state if relayout is needed or if this is the first data update
+    if (needsRelayout || !data) {
+        reset();
+    }
+    
     data = std::move(data_);
-    if (needsRelayout) reset();
+    
     data->getTile(
         id.canonical,
         [this, self = weakFactory.makeWeakPtr(), capturedData = data.get()](TileFeatures features) {
