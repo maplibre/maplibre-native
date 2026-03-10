d117b802ef6084772be91b8c7774047ea52446ab
nitrag <nitrag@users.noreply.github.com>
2026-03-10 13:32:36 -0400

    fix variable name

--- platform/darwin/test/MLNOfflinePackTests.mm
+++ platform/darwin/test/MLNOfflinePackTests.mm
@@ -16,7 +16,9 @@
       invalidPack.state, MLNOfflinePackStateInvalid,
       @"Offline pack should be invalid when initialized independently of MLNOfflineStorage.");
 
-  XCTAssertNil(invalidPack.regionID, @"Invalid offline pack should return nil regionID.");
+  XCTAssertNil(
+      invalidPack.regionId,
+      @"Invalid offline pack should return nil regionID.");
   XCTAssertThrowsSpecificNamed(
       invalidPack.region, NSException, MLNInvalidOfflinePackException,
       @"Invalid offline pack should raise an exception when accessing its region.");
