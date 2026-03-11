2c46089047bda45323d1cb735008bc3b4ff8962d
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2026-03-11 15:26:02 +0200

    Pin tinyobjloader commit

--- vendor/tinyobjloader.cmake
+++ vendor/tinyobjloader.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     tinyobjloader
     GIT_REPOSITORY "https://github.com/tinyobjloader/tinyobjloader.git"
-    GIT_TAG release
+    GIT_TAG 5a2342f3b8ed1035bdd1822e5d71e363f1f825ea #release
     GIT_SHALLOW TRUE
     GIT_PROGRESS TRUE
 )
