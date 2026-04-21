13c3f6f9e14326498a53ddcdc9f97b71823147d2
Bart Louwers <bart@emeel.net>
2026-04-21 16:50:06 +0200

    update AGP

--- platform/android/buildSrc/build.gradle.kts
+++ platform/android/buildSrc/build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 dependencies {
     implementation("com.vanniktech:gradle-dependency-graph-generator-plugin:0.3.0")
     implementation("com.vanniktech:gradle-maven-publish-plugin:0.34.0")
-    implementation("com.android.tools.build:gradle:8.6.0")
-    implementation("com.android.application:com.android.application.gradle.plugin:8.6.0")
+    implementation("com.android.tools.build:gradle:8.13.2")
+    implementation("com.android.application:com.android.application.gradle.plugin:8.13.2")
     implementation("commons-collections:commons-collections:3.2.2")
     implementation("io.sentry:sentry-android-gradle-plugin:5.9.0")
 }
