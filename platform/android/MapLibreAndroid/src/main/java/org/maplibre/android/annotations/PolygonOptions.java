a4072d746775776259a9f30d96e5d9ac31c36073
Bart Louwers <bart@emeel.net>
2026-05-29 14:29:43 +0200

    Replace link with MapLibre Plugins Android (#4313)

--- platform/android/MapLibreAndroid/src/main/java/org/maplibre/android/annotations/PolygonOptions.java
+++ platform/android/MapLibreAndroid/src/main/java/org/maplibre/android/annotations/PolygonOptions.java
@@ -15,7 +15,7 @@ import java.util.List;
 /**
  * Builder for composing {@link Polygon} objects.
  * @deprecated As of 7.0.0,
- * use <a href="https://github.com/mapbox/mapbox-plugins-android/tree/master/plugin-annotation">
+ * use <a href="https://github.com/maplibre/maplibre-plugins-android">
  *   MapLibre Annotation Plugin</a> instead
  */
 @Deprecated
