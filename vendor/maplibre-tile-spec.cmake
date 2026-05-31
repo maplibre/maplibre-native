74ba3237daa13f18fa0941e19c4dbb8d11f63a7a
Sargun Vohra <sargunv@users.noreply.github.com>
2026-05-31 00:03:10 -0700

    Centralize OHOS compiler workarounds

--- vendor/maplibre-tile-spec.cmake
+++ vendor/maplibre-tile-spec.cmake
@@ -13,5 +13,3 @@ if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.25")
 else()
     add_subdirectory(${PROJECT_SOURCE_DIR}/vendor/maplibre-tile-spec/cpp)
 endif()
-
-mbgl_enable_ohos_libcxx_experimental(mlt-cpp)
