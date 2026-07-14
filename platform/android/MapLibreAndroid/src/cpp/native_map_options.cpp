247c776d2658b408c5a9138ee6dbed5bcd9f704f
Tim Sylvester <tim@wetdogweather.com>
2026-07-14 12:31:41 -0700

    Enable FastPFOR encodings in MLT (#4146)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bart Louwers <bart@emeel.net>

--- platform/android/MapLibreAndroid/src/cpp/native_map_options.cpp
+++ platform/android/MapLibreAndroid/src/cpp/native_map_options.cpp
@@ -47,5 +47,11 @@ bool NativeMapOptions::asyncRendererCleanup(jni::JNIEnv &env, const jni::Object<
     return obj.Get(env, asyncRendererCleanupField);
 }
 
+bool NativeMapOptions::fastPFOREnabled(jni::JNIEnv &env, const jni::Object<NativeMapOptions> &obj) {
+    auto &javaClass = jni::Class<NativeMapOptions>::Singleton(env);
+    auto field = javaClass.GetField<jni::jboolean>(env, "fastPFOREnabled");
+    return obj.Get(env, field);
+}
+
 } // namespace android
 } // namespace mbgl
