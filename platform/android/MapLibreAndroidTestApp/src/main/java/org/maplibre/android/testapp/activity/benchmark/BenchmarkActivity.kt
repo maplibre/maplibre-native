46d03cf548542e838a489416864510d1e7619f47
Bart Louwers <bart@emeel.net>
2026-07-17 23:42:10 +0200

    Remove AWS style using exposed key

--- platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/benchmark/BenchmarkActivity.kt
+++ platform/android/MapLibreAndroidTestApp/src/main/java/org/maplibre/android/testapp/activity/benchmark/BenchmarkActivity.kt
@@ -116,7 +116,6 @@ class BenchmarkActivity : AppCompatActivity() {
         // return default
         return BenchmarkInputData(
             styleNames = listOf(
-//                "AWS Open Data Standard Light",
                 "Facebook Light",
                 "Americana",
 //                "Protomaps Light",
@@ -125,7 +124,6 @@ class BenchmarkActivity : AppCompatActivity() {
             ),
             styleURLs = listOf(
 //                {"message":"User is not authorized to access this resource with an explicit deny in an identity-based policy"}
-//                "https://maps.geo.us-east-2.amazonaws.com/maps/v0/maps/OpenDataStyle/style-descriptor?key=v1.public.eyJqdGkiOiI1NjY5ZTU4My0yNWQwLTQ5MjctODhkMS03OGUxOTY4Y2RhMzgifR_7GLT66TNRXhZJ4KyJ-GK1TPYD9DaWuc5o6YyVmlikVwMaLvEs_iqkCIydspe_vjmgUVsIQstkGoInXV_nd5CcmqRMMa-_wb66SxDdbeRDvmmkpy2Ow_LX9GJDgL2bbiCws0wupJPFDwWCWFLwpK9ICmzGvNcrPbX5uczOQL0N8V9iUvziA52a1WWkZucIf6MUViFRf3XoFkyAT15Ll0NDypAzY63Bnj8_zS8bOaCvJaQqcXM9lrbTusy8Ftq8cEbbK5aMFapXRjug7qcrzUiQ5sr0g23qdMvnKJQFfo7JuQn8vwAksxrQm6A0ByceEXSfyaBoVpFcTzEclxUomhY.NjAyMWJkZWUtMGMyOS00NmRkLThjZTMtODEyOTkzZTUyMTBi",
                 "https://external.xx.fbcdn.net/maps/vt/style/canterbury_1_0/?locale=en_US",
                 "https://americanamap.org/style.json",
 //                "https://api.protomaps.com/styles/v2/light.json?key=e761cc7daedf832a",
