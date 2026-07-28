fd7cf22884e08798b8d8297f473e709870e25f99
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2026-07-28 19:46:24 +0300

    Add exception handling

--- src/mbgl/util/action_journal_impl.cpp
+++ src/mbgl/util/action_journal_impl.cpp
@@ -148,11 +148,21 @@ ActionJournal::Impl::Impl(const Map& map_, const ActionJournalOptions& options_)
 
     previousFrameTime = util::MonotonicTimer::now().count();
 
-    options.withPath((mbgl::filesystem::canonical(options.path()) / ACTION_JOURNAL_DIRECTORY_NAME).generic_string());
+    try {
+        options.withPath(
+            (mbgl::filesystem::canonical(options.path()) / ACTION_JOURNAL_DIRECTORY_NAME).generic_string());
 
-    if (!openFile(detectFiles(), false)) {
-        Log::Error(Event::General, "Failed to open Action Journal file");
+        if (!openFile(detectFiles(), false)) {
+            Log::Error(Event::General, "Failed to open Action Journal file");
+        }
+    } catch (const mbgl::filesystem::filesystem_error& e) {
+        Log::Error(Event::General, std::string("Action Journal file exception: ") + e.what());
+    } catch (...) {
+        Log::Error(Event::General, std::string("Action Journal exception: ") + toString(std::current_exception()));
     }
+
+    scheduler->setExceptionHandler(
+        [](const std::exception_ptr e) { Log::Error(Event::General, "Action Journal log exception: " + toString(e)); });
 }
 
 ActionJournal::Impl::~Impl() {
