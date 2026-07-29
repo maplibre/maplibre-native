c54d9975ce3226ff43d9b6cd1cbc875df9c5de77
Adrian Cojocaru <adrian.cojocaru92@yahoo.com>
2026-07-29 17:31:57 +0300

    Add comment

--- include/mbgl/shaders/vulkan/location_indicator.hpp
+++ include/mbgl/shaders/vulkan/location_indicator.hpp
@@ -80,7 +80,9 @@ layout(location = 0) out vec4 out_color;
 layout(set = DRAWABLE_IMAGE_SET_INDEX, binding = 0) uniform sampler2D image_sampler;
 
 void main() {
+    // using lod bias variant for sampling as a workaround for Adreno 600 series
     out_color = texture(image_sampler, frag_uv, 0.0);
+    // working alternatives
     //out_color = textureGrad(image_sampler, frag_uv, dFdx(frag_uv), dFdy(frag_uv));
     //out_color = texelFetch(image_sampler, ivec2(frag_uv * vec2(textureSize(image_sampler, 0))), 0);
 }
