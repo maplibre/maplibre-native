40be634126c95fa1f592e9e66ff9a831b2ed7160
Richard Fairhurst <richard@systemeD.net>
2026-08-04 21:00:50 +0100

    Android: setOverrideSynchronousUpdate for existing sources (#4366)
    Co-authored-by: Bart Louwers <bart@emeel.net>

--- src/mbgl/style/sources/geojson_source.cpp
+++ src/mbgl/style/sources/geojson_source.cpp
@@ -132,5 +132,9 @@ bool GeoJSONSource::isUpdateSynchronous() const noexcept {
     return baseImpl->isUpdateSynchronous();
 }
 
+void GeoJSONSource::setOverrideSynchronousUpdate(bool value) noexcept {
+    impl().setOverrideSynchronousUpdate(value);
+}
+
 } // namespace style
 } // namespace mbgl
