4ecadb3fd1e1199073507675d451b5d5effa9e63
louwers <bart.louwers@gmail.com>
2026-08-22 18:52:05 +0000

    Deploying to gh-pages from @ maplibre/maplibre-native@2a8ebc4906df6037e756534e583a3b8e55283ed7 🚀

--- docs/book/searcher-09f2665d.js
+++ docs/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-07167a6c.js',
+                path_to_root + 'searchindex-917cba74.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
