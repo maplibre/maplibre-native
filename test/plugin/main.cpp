1817e053b73554d734071609003d7dc7b050a28e
Bart Louwers <bart@emeel.net>
2026-09-11 00:09:21 +0200

    Run plugin API tests through the core test runner

--- test/plugin/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <mln/util/run_loop.hpp>
-#include <gtest/gtest.h>
-
-int main(int argc, char** argv) {
-    mln::util::RunLoop loop;
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
