0cfade362672fea3da76b57f7b5ea92dae2ad59b
Sargun Vohra <sargunv@users.noreply.github.com>
2026-09-12 12:36:33 -0700

    fix(core): work around SwiftShader uniform block ordering

--- include/mln/shaders/gl/symbol_text_and_icon.hpp
+++ include/mln/shaders/gl/symbol_text_and_icon.hpp
@@ -208,13 +208,6 @@ lowp float halo_blur = u_halo_blur;
 #define SDF 1.0
 #define ICON 0.0
 
-layout (std140) uniform SymbolTilePropsUBO {
-    bool u_is_text;
-    bool u_is_halo;
-    highp float u_gamma_scale;
-    lowp float tileprops_pad1;
-};
-
 layout (std140) uniform SymbolEvaluatedPropsUBO {
     highp vec4 u_text_fill_color;
     highp vec4 u_text_halo_color;
@@ -228,6 +221,13 @@ layout (std140) uniform SymbolEvaluatedPropsUBO {
     highp float u_icon_halo_width;
     highp float u_icon_halo_blur;
     lowp float props_pad2;
+};
+
+layout (std140) uniform SymbolTilePropsUBO {
+    bool u_is_text;
+    bool u_is_halo;
+    highp float u_gamma_scale;
+    lowp float tileprops_pad1;
 };
 
 uniform sampler2D u_texture;
