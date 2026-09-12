13ccec1878397ee15c75682488be8962aca18dbd
Sargun Vohra <sargunv@users.noreply.github.com>
2026-09-12 12:36:33 -0700

    fix(core): work around SwiftShader uniform block ordering

--- shaders/line_sdf.fragment.glsl
+++ shaders/line_sdf.fragment.glsl
@@ -1,10 +1,3 @@
-layout (std140) uniform LineSDFTilePropsUBO {
-    highp float u_sdfgamma;
-    highp float u_mix;
-    lowp float tileprops_pad1;
-    lowp float tileprops_pad2;
-};
-
 layout (std140) uniform LineEvaluatedPropsUBO {
     highp vec4 u_color;
     lowp float u_blur;
@@ -15,6 +8,13 @@ layout (std140) uniform LineEvaluatedPropsUBO {
     lowp float u_floorwidth;
     lowp float props_pad1;
     lowp float props_pad2;
+};
+
+layout (std140) uniform LineSDFTilePropsUBO {
+    highp float u_sdfgamma;
+    highp float u_mix;
+    lowp float tileprops_pad1;
+    lowp float tileprops_pad2;
 };
 
 uniform sampler2D u_image;
