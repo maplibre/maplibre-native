0f08b2ab65e4aab715d8da6e66e4c0032d9c01ed
Sargun Vohra <sargunv@users.noreply.github.com>
2026-09-17 00:45:57 -0700

    fix(core): guard location indicator tweaker update for OpenGL

--- src/mln/renderer/layers/render_location_indicator_layer.cpp
+++ src/mln/renderer/layers/render_location_indicator_layer.cpp
@@ -984,9 +984,11 @@ void RenderLocationIndicatorLayer::evaluate(const PropertyEvaluationParameters& 
 
     evaluatedProperties = std::move(properties);
 
+#ifdef MLN_DRAWABLE_LOCATION_INDICATOR
     if (layerTweaker) {
         layerTweaker->updateProperties(evaluatedProperties);
     }
+#endif
 }
 
 bool RenderLocationIndicatorLayer::hasTransition() const {
